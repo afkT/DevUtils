2bd3c9aa4d802512c74a719c46e08b19dac2174c
Ttt <13798405957@163.com>
2018-09-15 01:51:22 +0800

    1.增加播放器管理类, 快捷使用处理 2.增加写入权限、网络权限
    Former-commit-id: 2fab78bff7961289a9afd2f0f26acbce40a02f0b

--- app/src/main/java/com/dev/MainActivity.java
+++ app/src/main/java/com/dev/MainActivity.java
@@ -1,7 +1,12 @@
 package com.dev;
 
+import android.Manifest;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import dev.utils.app.SDCardUtils;
+import dev.utils.app.player.DevMediaManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 申请权限
+        if (Build.VERSION.SDK_INT >=  Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+
+//        DevMediaManager.getInstance().setMeidaListener(new DevMediaManager.MediaListener() {
+//            @Override
+//            public void onPrepared() {
+//                DevMediaManager.getInstance().getMediaPlayer().start();
+//            }
+//
+//            @Override
+//            public void onCompletion() {
+//
+//            }
+//
+//            @Override
+//            public void onBufferingUpdate(int percent) {
+//
+//            }
+//
+//            @Override
+//            public void onSeekComplete() {
+//
+//            }
+//
+//            @Override
+//            public void onError(int what, int extra) {
+//
+//            }
+//
+//            @Override
+//            public void onVideoSizeChanged(int width, int height) {
+//
+//            }
+//        });
+//        DevMediaManager.getInstance().playPrepare(SDCardUtils.getSDCardPath() + "/video_3.mp4");
+//        DevMediaManager.getInstance().playPrepareAssets("a.mp3");
+//        DevMediaManager.getInstance().playPrepare(SDCardUtils.getSDCardPath() + "/a.mp3");
     }
 }
