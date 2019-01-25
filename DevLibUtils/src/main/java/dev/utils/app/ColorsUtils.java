0a237ff3c3851d12a5a07102f7f8d960768037a4
Ttt <13798405957@163.com>
2019-01-25 12:28:36 +0800

    1.更新 ColorUtils 代码
    Former-commit-id: 9c5765be68a865af20e8e5bd453ef6b1d4f9cda5

--- DevLibUtils/src/main/java/dev/utils/app/ColorsUtils.java
+++ DevLibUtils/src/main/java/dev/utils/app/ColorsUtils.java
@@ -1,5 +1,11 @@
 package dev.utils.app;
 
+import android.graphics.Color;
+import android.support.v4.graphics.ColorUtils;
+
+import dev.DevUtils;
+import dev.utils.LogPrintUtils;
+
 /**
  * detail: 颜色工具类 包括常用的色值
  * Created by Ttt
@@ -20,6 +26,8 @@ public final class ColorsUtils {
      * 00、19、33、4C、66、7F、99、B2、CC、E5、FF
      */
 
+    /** 透明 */
+    public static final int TRANSPARENT = 0x00000000;
     /** 白色 */
     public static final int WHITE = 0xffffffff;
     /** 白色 - 半透明 */
@@ -28,135 +36,100 @@ public final class ColorsUtils {
     public static final int BLACK = 0xff000000;
     /** 黑色 - 半透明 */
     public static final int BLACK_TRANSLUCENT = 0x80000000;
-    /** 透明 */
-    public static final int TRANSPARENT = 0x00000000;
     /** 红色 */
     public static final int RED = 0xffff0000;
     /** 红色 - 半透明 */
     public static final int RED_TRANSLUCENT = 0x80ff0000;
-    /** 红色 - 深的 */
-    public static final int RED_DARK = 0xff8b0000;
-    /** 红色 - 深的 - 半透明 */
-    public static final int RED_DARK_TRANSLUCENT = 0x808b0000;
     /** 绿色 */
     public static final int GREEN = 0xff00ff00;
     /** 绿色 - 半透明 */
     public static final int GREEN_TRANSLUCENT = 0x8000ff00;
-    /** 绿色 - 深的 */
-    public static final int GREEN_DARK = 0xff003300;
-    /** 绿色 - 深的 - 半透明 */
-    public static final int GREEN_DARK_TRANSLUCENT = 0x80003300;
-    /** 绿色 - 浅的 */
-    public static final int GREEN_LIGHT = 0xffccffcc;
-    /** 绿色 - 浅的 - 半透明 */
-    public static final int GREEN_LIGHT_TRANSLUCENT = 0x80ccffcc;
     /** 蓝色 */
     public static final int BLUE = 0xff0000ff;
     /** 蓝色 - 半透明 */
     public static final int BLUE_TRANSLUCENT = 0x800000ff;
-    /** 蓝色 - 深的 */
-    public static final int BLUE_DARK = 0xff00008b;
-    /** 蓝色 - 深的 - 半透明 */
-    public static final int BLUE_DARK_TRANSLUCENT = 0x8000008b;
-    /** 蓝色 - 浅的 */
-    public static final int BLUE_LIGHT = 0xff36a5E3;
-    /** 蓝色 - 浅的 - 半透明 */
-    public static final int BLUE_LIGHT_TRANSLUCENT = 0x8036a5E3;
-    /** 天蓝 */
-    public static final int SKYBLUE = 0xff87ceeb;
-    /** 天蓝 - 半透明 */
-    public static final int SKYBLUE_TRANSLUCENT = 0x8087ceeb;
-    /** 天蓝 - 深的 */
-    public static final int SKYBLUE_DARK = 0xff00bfff;
-    /** 天蓝 - 深的 - 半透明 */
-    public static final int SKYBLUE_DARK_TRANSLUCENT = 0x8000bfff;
-    /** 天蓝 - 浅的 */
-    public static final int SKYBLUE_LIGHT = 0xff87cefa;
-    /** 天蓝 - 浅的 - 半透明 */
-    public static final int SKYBLUE_LIGHT_TRANSLUCENT = 0x8087cefa;
     /** 灰色 */
     public static final int GRAY = 0xff969696;
-    /** 灰色 - 半透明 */
-    public static final int GRAY_TRANSLUCENT = 0x80969696;
     /** 灰色 - 深的 */
     public static final int GRAY_DARK = 0xffa9a9a9;
-    /** 灰色 - 深的 - 半透明 */
-    public static final int GRAY_DARK_TRANSLUCENT = 0x80a9a9a9;
-    /** 灰色 - 暗的 */
-    public static final int GRAY_DIM = 0xff696969;
-    /** 灰色 - 暗的 - 半透明 */
-    public static final int GRAY_DIM_TRANSLUCENT = 0x80696969;
-    /** 灰色 - 浅的 */
-    public static final int GRAY_LIGHT = 0xffd3d3d3;
-    /** 灰色 - 浅的 - 半透明 */
-    public static final int GRAY_LIGHT_TRANSLUCENT = 0x80d3d3d3;
     /** 橙色 */
     public static final int ORANGE = 0xffffa500;
-    /** 橙色 - 半透明 */
-    public static final int ORANGE_TRANSLUCENT = 0x80ffa500;
-    /** 橙色 - 深的 */
-    public static final int ORANGE_DARK = 0xffff8800;
-    /** 橙色 - 深的 - 半透明 */
-    public static final int ORANGE_DARK_TRANSLUCENT = 0x80ff8800;
-    /** 橙色 - 浅的 */
-    public static final int ORANGE_LIGHT = 0xffffbb33;
-    /** 橙色 - 浅的 - 半透明 */
-    public static final int ORANGE_LIGHT_TRANSLUCENT = 0x80ffbb33;
     /** 金色 */
     public static final int GOLD = 0xffffd700;
-    /** 金色 - 半透明 */
-    public static final int GOLD_TRANSLUCENT = 0x80ffd700;
     /** 粉色 */
     public static final int PINK = 0xffffc0cb;
-    /** 粉色 - 半透明 */
-    public static final int PINK_TRANSLUCENT = 0x80ffc0cb;
-    /** 紫红色 */
-    public static final int FUCHSIA = 0xffff00ff;
-    /** 紫红色 - 半透明 */
-    public static final int FUCHSIA_TRANSLUCENT = 0x80ff00ff;
-    /** 灰白色 */
-    public static final int GRAYWHITE = 0xfff2f2f2;
-    /** 灰白色 - 半透明 */
-    public static final int GRAYWHITE_TRANSLUCENT = 0x80f2f2f2;
     /** 紫色 */
     public static final int PURPLE = 0xff800080;
-    /** 紫色 - 半透明 */
-    public static final int PURPLE_TRANSLUCENT = 0x80800080;
-    /** 青色 */
-    public static final int CYAN = 0xff00ffff;
-    /** 青色 - 半透明 */
-    public static final int CYAN_TRANSLUCENT = 0x8000ffff;
-    /** 青色 - 深的 */
-    public static final int CYAN_DARK = 0xff008b8b;
-    /** 青色 - 深的 - 半透明 */
-    public static final int CYAN_DARK_TRANSLUCENT = 0x80008b8b;
     /** 黄色 */
     public static final int YELLOW = 0xffffff00;
-    /** 黄色 - 半透明 */
-    public static final int YELLOW_TRANSLUCENT = 0x80ffff00;
-    /** 黄色 - 浅的 */
-    public static final int YELLOW_LIGHT = 0xffffffe0;
-    /** 黄色 - 浅的 - 半透明 */
-    public static final int YELLOW_LIGHT_TRANSLUCENT = 0x80ffffe0;
-    /** 巧克力色 */
-    public static final int CHOCOLATE = 0xffd2691e;
-    /** 巧克力色 - 半透明 */
-    public static final int CHOCOLATE_TRANSLUCENT = 0x80d2691e;
+    /** 青色 */
+    public static final int CYAN = 0xff00ffff;
     /** 番茄色 */
     public static final int TOMATO = 0xffff6347;
-    /** 番茄色 - 半透明 */
-    public static final int TOMATO_TRANSLUCENT = 0x80ff6347;
-    /** 橙红色 */
-    public static final int ORANGERED = 0xffff4500;
-    /** 橙红色 - 半透明 */
-    public static final int ORANGERED_TRANSLUCENT = 0x80ff4500;
-    /** 银白色 */
-    public static final int SILVER = 0xffc0c0c0;
-    /** 银白色 - 半透明 */
-    public static final int SILVER_TRANSLUCENT = 0x80c0c0c0;
     /** 高光 */
     public static final int HIGHLIGHT = 0x33ffffff;
     /** 低光 */
     public static final int LOWLIGHT = 0x33000000;
 
+    /**
+     * 根据颜色资源Id，获取颜色
+     * @param colorId
+     * @return
+     */
+    public static int getResourcesColor(int colorId) {
+        try {
+            return DevUtils.getContext().getResources().getColor(colorId);
+        } catch (Exception e) {
+            LogPrintUtils.eTag(TAG, e, "getResourcesColor");
+        }
+        return -1;
+    }
+
+    /**
+     * 返回一个颜色中红色的色值(返回10进制)
+     * @param color
+     * @return
+     */
+    public static int red(int color) {
+        return (color >> 16) & 0xFF;
+    }
+
+    /**
+     * 返回一个颜色中绿色的色值(返回10进制)
+     * @param color
+     * @return
+     */
+    public static int green(int color) {
+        return (color >> 8) & 0xFF;
+    }
+
+    /**
+     * 返回一个颜色中蓝色的色值(返回10进制)
+     * @param color
+     * @return
+     */
+    public static int blue(int color) {
+        return color & 0xFF;
+    }
+
+    /**
+     * 根据对应的 red、green、blue 生成一个颜色值
+     * @param red [0-255]
+     * @param green [0-255]
+     * @param blue [0-255]
+     */
+    public static int rgb(int red, int green, int blue) {
+        return 0xff000000 | (red << 16) | (green << 8) | blue;
+    }
+
+    /**
+     * 根据对应的 alpha, red、green、blue 生成一个颜色值 (含透明度)
+     * @param alpha [0-255]
+     * @param red [0-255]
+     * @param green [0-255]
+     * @param blue [0-255]
+     */
+    public static int argb(int alpha, int red, int green, int blue) {
+        return (alpha << 24) | (red << 16) | (green << 8) | blue;
+    }
 }
