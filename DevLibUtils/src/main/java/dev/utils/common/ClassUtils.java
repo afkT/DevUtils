e6de2c8bffc4f84d37e24397b10d0ee1dd093846
Ttt <13798405957@163.com>
2019-04-04 11:05:42 +0800

    1.增加 ClassUtils 工具类 两个获取泛型类型方法（父类、接口）
    Former-commit-id: cc6f2e7f8886ca31f0a5fb043c0930109f233b10

--- DevLibUtils/src/main/java/dev/utils/common/ClassUtils.java
+++ DevLibUtils/src/main/java/dev/utils/common/ClassUtils.java
@@ -1,6 +1,8 @@
 package dev.utils.common;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.Collection;
 import java.util.Date;
 
@@ -88,5 +90,136 @@ public final class ClassUtils {
      */
     public static boolean isArray(Class clazz) {
         return (clazz != null && clazz.isArray());
+    }
+
+    // =
+
+    /**
+     * 获取父类 泛型类型
+     * @param object
+     * @return
+     */
+    public static Class<?> getGenericSuperclass(Object object) {
+        return getGenericSuperclass(object, 0);
+    }
+
+    /**
+     * 获取父类 泛型类型
+     * @param object
+     * @param pos 泛型参数位置
+     * @return
+     */
+    public static Class<?> getGenericSuperclass(Object object, int pos) {
+        if (object != null) {
+            return getGenericSuperclass(object.getClass(), pos);
+        }
+        return null;
+    }
+
+    // =
+
+    /**
+     * 获取父类 泛型类型
+     * @param clazz
+     * @return
+     */
+    public static Class<?> getGenericSuperclass(Class clazz) {
+        return getGenericSuperclass(clazz, 0);
+    }
+
+    /**
+     * 获取父类 泛型类型
+     * @param clazz
+     * @param pos 泛型参数位置
+     * @return
+     */
+    public static Class<?> getGenericSuperclass(Class clazz, int pos) {
+        if (clazz != null && pos >= 0) {
+            try {
+                return (Class<?>) ((ParameterizedType) clazz.getGenericSuperclass()).getActualTypeArguments()[pos];
+            } catch (Exception e) {
+                JCLogUtils.eTag(TAG, e, "getGenericSuperclass");
+            }
+        }
+        return null;
+    }
+
+    // =
+
+    /**
+     * 获取接口泛型类型
+     * @param object
+     * @param interfaceClazz 接口 Class
+     * @return
+     */
+    public static Class<?> getGenericInterfaces(Object object, Class interfaceClazz) {
+        return getGenericInterfaces(object, interfaceClazz, 0);
+    }
+
+
+    /**
+     * 获取接口泛型类型
+     * @param object
+     * @param interfaceClazz 接口 Class
+     * @param pos 泛型参数位置
+     * @return
+     */
+    public static Class<?> getGenericInterfaces(Object object, Class interfaceClazz, int pos) {
+        if (object != null) {
+            return getGenericInterfaces(object.getClass(), interfaceClazz, pos);
+        }
+        return null;
+    }
+
+    // =
+
+    /**
+     * 获取接口泛型类型
+     * @param clazz
+     * @param interfaceClazz 接口 Class
+     * @return
+     */
+    public static Class<?> getGenericInterfaces(Class clazz, Class interfaceClazz) {
+        return getGenericInterfaces(clazz, interfaceClazz, 0);
+    }
+
+    /**
+     * 获取接口泛型类型
+     * @param clazz
+     * @param interfaceClazz 接口 Class
+     * @param pos 泛型参数位置
+     * @return
+     */
+    public static Class<?> getGenericInterfaces(Class clazz, Class interfaceClazz, int pos) {
+        if (clazz != null && interfaceClazz != null && pos >= 0) {
+            try {
+                // 获取接口类名
+                String iName = interfaceClazz.getName();
+                if (iName.equals("")) return null;
+                // 获取接口泛型类型数组
+                Type[] types = clazz.getGenericInterfaces();
+                // 循环类型
+                for (Type type : types) {
+                    if (type instanceof ParameterizedType) {
+                        ParameterizedType parameterizedType = (ParameterizedType) type;
+                        // 判断是否属于该接口
+                        String rawType = parameterizedType.getRawType().toString();
+                        // 判断接口包名是否一致
+                        if (rawType.startsWith("interface ")) {
+                            if (rawType.equals("interface " + iName)) {
+                                return (Class<?>) parameterizedType.getActualTypeArguments()[pos];
+                            }
+                        } else {
+                            if (rawType.equals(iName)) {
+                                return (Class<?>) parameterizedType.getActualTypeArguments()[pos];
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                JCLogUtils.eTag(TAG, e, "getGenericInterfaces");
+            }
+        }
+        return null;
     }
 }
