b3e37b2c6a1a9519528676bc2e7a44a697e3b636
Ttt <13798405957@163.com>
2019-05-08 11:38:00 +0800

    1.更新 ClickUtils，增加内部辅助类实现，方便控制某个 Activity/功能模块 双击处理
    Former-commit-id: 5b1d082f11ade517faa5c542354cb4d7bf9f9fce

--- DevLibUtils/src/main/java/dev/utils/app/ClickUtils.java
+++ DevLibUtils/src/main/java/dev/utils/app/ClickUtils.java
@@ -20,194 +20,15 @@ public final class ClickUtils {
 
     // 日志 TAG
     private static final String TAG = ClickUtils.class.getSimpleName();
-    // 上一次点击的标识id = viewId 等
-    private static int sLastTagId = -1;
-    // 上次点击时间
-    private static long sLastClickTime = 0l; // 局限性是, 全局统一事件，如果上次点击后，立刻点击其他就无法点
-    // 默认间隔时间
-    private static long DF_DIFF = 1000l; // 点击间隔1秒内
-    // 配置数据
-    private static final Map<String, Long> sConfigs = new HashMap<>();
-    // 点击记录数据
-    private static final Map<String, Long> sRecords = new HashMap<>();
-
-    // =
-
-    /**
-     * 判断两次点击的间隔 小于默认间隔时间(1秒), 则认为是多次无效点击
-     * @return
-     */
-    public static boolean isFastDoubleClick() {
-        return isFastDoubleClick(-1, DF_DIFF);
-    }
-
-    /**
-     * 判断两次点击的间隔 小于默认间隔时间(1秒), 则认为是多次无效点击
-     * @param tagId
-     * @return
-     */
-    public static boolean isFastDoubleClick(final int tagId) {
-        return isFastDoubleClick(tagId, DF_DIFF);
-    }
-
-    /**
-     * 判断两次点击的间隔 小于间隔时间(diff), 则认为是多次无效点击
-     * @param tagId
-     * @param diff
-     * @return
-     */
-    public static boolean isFastDoubleClick(final int tagId, final long diff) {
-        long cTime = System.currentTimeMillis();
-        long dTime = cTime - sLastClickTime;
-        // 判断时间是否超过
-        if (sLastTagId == tagId && sLastClickTime > 0 && dTime < diff) {
-            LogPrintUtils.dTag(TAG, "isFastDoubleClick 无效点击 => tagId: " + tagId + ", diff: " + diff);
-            return true;
-        }
-        LogPrintUtils.dTag(TAG, "isFastDoubleClick 有效点击 => tagId: " + tagId + ", diff: " + diff);
-        sLastTagId = tagId;
-        sLastClickTime = cTime;
-        return false;
-    }
-
-    // =
-
-    /**
-     * 判断两次点击的间隔(根据默认Tag判断) 小于指定间隔时间, 则认为是多次无效点击
-     * @param tag
-     * @return
-     */
-    public static boolean isFastDoubleClick(final String tag) {
-        // 获取配置时间
-        Long config_time = sConfigs.get(tag);
-        // 如果等于null, 则传入默认时间
-        if (config_time == null) {
-            return isFastDoubleClick(tag, DF_DIFF);
-        }
-        return isFastDoubleClick(tag, config_time);
-    }
-
-    /**
-     * 判断两次点击的间隔 小于间隔时间(diff), 则认为是多次无效点击
-     * @param tag
-     * @param diff
-     * @return
-     */
-    public static boolean isFastDoubleClick(final String tag, final long diff) {
-        // 获取上次点击的时间
-        Long lastTime = sRecords.get(tag);
-        if (lastTime == null) {
-            lastTime = 0l;
-        }
-        long cTime = System.currentTimeMillis();
-        long dTime = cTime - lastTime;
-        // 判断时间是否超过
-        if (lastTime > 0 && dTime < diff) {
-            LogPrintUtils.dTag(TAG, "isFastDoubleClick 无效点击 => tag: " + tag + ", diff: " + diff);
-            return true;
-        }
-        LogPrintUtils.dTag(TAG, "isFastDoubleClick 有效点击 => tag: " + tag + ", diff: " + diff);
-        // 保存上次点击时间
-        sRecords.put(tag, cTime);
-        return false;
-    }
-
-    // =
-
-    /**
-     * 判断两次点击的间隔(根据默认Tag判断) 小于指定间隔时间, 则认为是多次无效点击
-     * @param object
-     * @return
-     */
-    public static boolean isFastDoubleClick(final Object object) {
-        // 获取TAG
-        String tag = ((object != null) ? ("obj_" + object.hashCode()) : "obj_null");
-        // 获取配置时间
-        Long config_time = sConfigs.get(tag);
-        // 如果等于null, 则传入默认时间
-        if (config_time == null) {
-            return isFastDoubleClick(tag, DF_DIFF);
-        }
-        return isFastDoubleClick(tag, config_time);
-    }
-
-    /**
-     * 判断两次点击的间隔 小于间隔时间(diff), 则认为是多次无效点击
-     * @param object
-     * @param diff
-     * @return
-     */
-    public static boolean isFastDoubleClick(final Object object, final long diff) {
-        // 获取TAG
-        String tag = ((object != null) ? ("obj_" + object.hashCode()) : "obj_null");
-        // 获取上次点击的时间
-        Long lastTime = sRecords.get(tag);
-        if (lastTime == null) {
-            lastTime = 0l;
-        }
-        long cTime = System.currentTimeMillis();
-        long dTime = cTime - lastTime;
-        // 判断时间是否超过
-        if (lastTime > 0 && dTime < diff) {
-            LogPrintUtils.dTag(TAG, "isFastDoubleClick 无效点击 => obj: " + object + ", diff: " + diff);
-            return true;
-        }
-        LogPrintUtils.dTag(TAG, "isFastDoubleClick 有效点击 => obj: " + object + ", diff: " + diff);
-        // 保存上次点击时间
-        sRecords.put(tag, cTime);
-        return false;
-    }
-
-    // =
-
-    /**
-     * 初始化配置信息
-     * @param mapConfig
-     */
-    public static void initConfig(final Map<String, Object> mapConfig) {
-        if (mapConfig != null) {
-            mapConfig.putAll(mapConfig);
-        }
-    }
-
-    /**
-     * 添加配置信息
-     * @param key
-     * @param val
-     */
-    public static void putConfig(final String key, final Long val) {
-        sConfigs.put(key, val);
-    }
-
-    /**
-     * 移除配置信息
-     * @param key
-     */
-    public static void removeConfig(final String key) {
-        sConfigs.remove(key);
-    }
-
-    // =
-
-    /**
-     * 移除点击记录
-     * @param key
-     */
-    public static void removeRecord(final String key) {
-        sRecords.remove(key);
-    }
-
-    /**
-     * 清空点击记录(全部)
-     */
-    public static void clearRecord() {
-        sRecords.clear();
-    }
+    // 全局共用的点击辅助类
+    private static final ClickAssist sGlobalClickAssist = new ClickAssist();
+    // 功能模块 ClickAssist Maps
+    private static final Map<Object, ClickAssist> sMapClickAssists = new HashMap<>();
 
     /**
      * 增加控件的触摸范围，最大范围只能是父布局所包含的的区域
-     * @param view
-     * @param range
+     * @param view  待添加点击范围 View
+     * @param range 点击范围
      */
     public static void addTouchArea(final View view, final int range) {
         addTouchArea(view, range, range, range, range);
@@ -215,11 +36,11 @@ public final class ClickUtils {
 
     /**
      * 增加控件的触摸范围，最大范围只能是父布局所包含的的区域
-     * @param view
-     * @param top
-     * @param bottom
-     * @param left
-     * @param right
+     * @param view   待添加点击范围 View
+     * @param top    top range
+     * @param bottom bottom range
+     * @param left   left range
+     * @param right  right range
      */
     public static void addTouchArea(final View view, final int top, final int bottom, final int left, final int right) {
         if (view != null) {
@@ -249,6 +70,355 @@ public final class ClickUtils {
             } catch (Exception e) {
                 LogPrintUtils.eTag(TAG, e, "addTouchArea");
             }
+        }
+    }
+
+    // ======================
+    // = 功能模块辅助类操作 =
+    // ======================
+
+    /**
+     * 获取对应功能模块点击辅助类
+     * @param object key by Object
+     * @return {@link ClickAssist}
+     */
+    public static ClickAssist get(final Object object) {
+        // 获取对应模块点击辅助类
+        ClickAssist clickAssist = sMapClickAssists.get(object);
+        if (clickAssist != null) {
+            return clickAssist;
+        }
+        clickAssist = new ClickAssist();
+        sMapClickAssists.put(object, clickAssist);
+        return clickAssist;
+    }
+
+    /**
+     * 移除对应功能模块点击辅助类
+     * @param object key by Object
+     */
+    public static void remove(final Object object) {
+        sMapClickAssists.remove(object);
+    }
+
+    // ======================
+    // = 全局点击辅助类操作 =
+    // ======================
+
+    /**
+     * 判断是否双击(无效点击 - 短时间内多次点击)
+     * @return {@code true} yes, {@code false} no
+     */
+    public boolean isFastDoubleClick() {
+        return sGlobalClickAssist.isFastDoubleClick();
+    }
+
+    /**
+     * 判断是否双击(无效点击 - 短时间内多次点击)
+     * @param tagId id
+     * @return {@code true} yes, {@code false} no
+     */
+    public boolean isFastDoubleClick(final int tagId) {
+        return sGlobalClickAssist.isFastDoubleClick(tagId);
+    }
+
+    /**
+     * 判断是否双击(无效点击 - 短时间内多次点击)
+     * @param tagId        id
+     * @param intervalTime 时间间隔
+     * @return {@code true} yes, {@code false} no
+     */
+    public boolean isFastDoubleClick(final int tagId, final long intervalTime) {
+        return sGlobalClickAssist.isFastDoubleClick(tagId, intervalTime);
+    }
+
+    // =
+
+    /**
+     * 判断是否双击(无效点击 - 短时间内多次点击)
+     * @param object    key by Object
+     * @param configKey 时间间隔配置 Key
+     * @return {@code true} yes, {@code false} no
+     */
+    public boolean isFastDoubleClick(final Object object, final String configKey) {
+        return sGlobalClickAssist.isFastDoubleClick(object, configKey);
+    }
+
+    /**
+     * 判断是否双击(无效点击 - 短时间内多次点击)
+     * @param object       key by Object
+     * @param intervalTime 时间间隔
+     * @return {@code true} yes, {@code false} no
+     */
+    public boolean isFastDoubleClick(final Object object, final long intervalTime) {
+        return sGlobalClickAssist.isFastDoubleClick(object, intervalTime);
+    }
+
+    // =
+
+    /**
+     * 初始化配置信息
+     * @param mapConfigs config Maps
+     */
+    public void initConfig(final Map<String, Long> mapConfigs) {
+        sGlobalClickAssist.initConfig(mapConfigs);
+    }
+
+    /**
+     * 添加配置信息
+     * @param key   config Key
+     * @param value config Value
+     */
+    public void putConfig(final String key, final Long value) {
+        sGlobalClickAssist.putConfig(key, value);
+    }
+
+    /**
+     * 移除配置信息
+     * @param key config Key
+     */
+    public void removeConfig(final String key) {
+        sGlobalClickAssist.removeConfig(key);
+    }
+
+    /**
+     * 获取配置时间
+     * @param key config Key
+     * @return 配置时间
+     */
+    public Long getConfigTime(final String key) {
+        return sGlobalClickAssist.getConfigTime(key);
+    }
+
+    // =
+
+    /**
+     * 移除点击记录
+     * @param key tag Key
+     */
+    public void removeRecord(final String key) {
+        sGlobalClickAssist.removeRecord(key);
+    }
+
+    /**
+     * 清空全部点击记录
+     */
+    public void clearRecord() {
+        sGlobalClickAssist.clearRecord();
+    }
+
+    // =
+
+    /**
+     * 设置默认点击时间间隔
+     * @param intervalTime 时间间隔
+     */
+    public void setIntervalTime(final long intervalTime) {
+        sGlobalClickAssist.setIntervalTime(intervalTime);
+    }
+
+    /**
+     * 重置处理
+     */
+    public void reset() {
+        sGlobalClickAssist.reset();
+    }
+
+    // =
+
+    /**
+     * detail: 点击(双击) 辅助类
+     * @author Ttt
+     * <pre>
+     *      ps: 该辅助类, 主要避免全局共用一个双击控制类, 容易出现冲突, 方便控制某个 Activity/功能模块 双击处理
+     *      使用 Key(Tag-Object) 获取指定的 {@link ClickAssist}, 能够实现不同 Activity/功能模块 单独使用 {@link ClickAssist}
+     *      并且可以进行销毁处理
+     * </pre>
+     */
+    public static class ClickAssist {
+
+        // 最后一次点击的标识id
+        private int mLastTagId = -1;
+        // 最后一次点击时间
+        private long mLastClickTime = 0l;
+        // 双击间隔时间
+        private long mIntervalTime = 1000L;
+        // 配置数据
+        private final Map<String, Long> mMapConfigs = new HashMap<>();
+        // 点击记录数据
+        private final Map<String, Long> mMapRecords = new HashMap<>();
+
+        public ClickAssist() {
+        }
+
+        /**
+         * 构造函数
+         * @param intervalTime 时间间隔
+         */
+        public ClickAssist(final long intervalTime) {
+            this.mIntervalTime = intervalTime;
+        }
+
+        // =
+
+        /**
+         * 判断是否双击(无效点击 - 短时间内多次点击)
+         * @return {@code true} yes, {@code false} no
+         */
+        public boolean isFastDoubleClick() {
+            return isFastDoubleClick(-1, mIntervalTime);
+        }
+
+        /**
+         * 判断是否双击(无效点击 - 短时间内多次点击)
+         * @param tagId id
+         * @return {@code true} yes, {@code false} no
+         */
+        public boolean isFastDoubleClick(final int tagId) {
+            return isFastDoubleClick(tagId, mIntervalTime);
+        }
+
+        /**
+         * 判断是否双击(无效点击 - 短时间内多次点击)
+         * @param tagId        id
+         * @param intervalTime 时间间隔
+         * @return {@code true} yes, {@code false} no
+         */
+        public boolean isFastDoubleClick(final int tagId, final long intervalTime) {
+            long curTime = System.currentTimeMillis();
+            long diffTime = curTime - mLastClickTime;
+            // 判断时间是否超过
+            if (mLastTagId == tagId && mLastClickTime > 0 && diffTime < intervalTime) {
+                LogPrintUtils.dTag(TAG, "isFastDoubleClick 无效点击 => tagId: " + tagId + ", intervalTime: " + intervalTime);
+                return true;
+            }
+            LogPrintUtils.dTag(TAG, "isFastDoubleClick 有效点击 => tagId: " + tagId + ", intervalTime: " + intervalTime);
+            mLastTagId = tagId;
+            mLastClickTime = curTime;
+            return false;
+        }
+
+        // =
+
+        /**
+         * 判断是否双击(无效点击 - 短时间内多次点击)
+         * @param object    key by Object
+         * @param configKey 时间间隔配置 Key
+         * @return {@code true} yes, {@code false} no
+         */
+        public boolean isFastDoubleClick(final Object object, final String configKey) {
+            return isFastDoubleClick(object, getConfigTime(configKey));
+        }
+
+        /**
+         * 判断是否双击(无效点击 - 短时间内多次点击)
+         * @param object       key by Object
+         * @param intervalTime 时间间隔
+         * @return {@code true} yes, {@code false} no
+         */
+        public boolean isFastDoubleClick(final Object object, final long intervalTime) {
+            // 获取TAG
+            String tag = ((object != null) ? ("obj_" + object.hashCode()) : "obj_null");
+            // 获取上次点击的时间
+            Long lastTime = mMapRecords.get(tag);
+            if (lastTime == null) {
+                lastTime = 0l;
+            }
+            long curTime = System.currentTimeMillis();
+            long diffTime = curTime - lastTime;
+            // 判断时间是否超过
+            if (lastTime > 0 && diffTime < intervalTime) {
+                LogPrintUtils.dTag(TAG, "isFastDoubleClick 无效点击 => obj: " + object + ", intervalTime: " + intervalTime);
+                return true;
+            }
+            LogPrintUtils.dTag(TAG, "isFastDoubleClick 有效点击 => obj: " + object + ", intervalTime: " + intervalTime);
+            // 保存上次点击时间
+            mMapRecords.put(tag, curTime);
+            return false;
+        }
+
+        // =
+
+        /**
+         * 初始化配置信息
+         * @param mapConfigs config Maps
+         */
+        public void initConfig(final Map<String, Long> mapConfigs) {
+            if (mapConfigs != null) {
+                mMapConfigs.putAll(mapConfigs);
+            }
+        }
+
+        /**
+         * 添加配置信息
+         * @param key   config Key
+         * @param value config Value
+         */
+        public void putConfig(final String key, final Long value) {
+            mMapConfigs.put(key, value);
+        }
+
+        /**
+         * 移除配置信息
+         * @param key config Key
+         */
+        public void removeConfig(final String key) {
+            mMapConfigs.remove(key);
+        }
+
+        /**
+         * 获取配置时间
+         * @param key config Key
+         * @return 配置时间
+         */
+        public Long getConfigTime(final String key) {
+            // 获取配置时间
+            Long configTime = mMapConfigs.get(key);
+            // 判断是否为null
+            return (configTime != null) ? configTime : mIntervalTime;
+        }
+
+        // =
+
+        /**
+         * 移除点击记录
+         * @param key tag Key
+         */
+        public void removeRecord(final String key) {
+            mMapRecords.remove(key);
+        }
+
+        /**
+         * 清空全部点击记录
+         */
+        public void clearRecord() {
+            mMapRecords.clear();
+        }
+
+        // =
+
+        /**
+         * 设置默认点击时间间隔
+         * @param intervalTime 时间间隔
+         */
+        public void setIntervalTime(final long intervalTime) {
+            this.mIntervalTime = intervalTime;
+        }
+
+        /**
+         * 重置处理
+         */
+        public void reset() {
+            // 重置最后一次点击的标识id
+            mLastTagId = -1;
+            // 重置最后一次点击时间
+            mLastClickTime = 0l;
+            // 重置双击间隔时间
+            mIntervalTime = 1000L;
+            // 清空配置信息
+            mMapConfigs.clear();
+            // 清空点击记录
+            mMapRecords.clear();
         }
     }
 }
