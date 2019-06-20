89d3bbe46ab3e28212e688181711c2e2e2ed3e8c
Ttt <13798405957@163.com>
2019-06-20 11:02:37 +0800

    1.规范化 app.SDCardUtils javadoc 注释代码, 以及遗漏的 @return、@param 注释代码等
    Former-commit-id: 46802dbcf13431aafb8f6fe3c15f91145a625392

--- DevLibUtils/src/main/java/dev/utils/app/SDCardUtils.java
+++ DevLibUtils/src/main/java/dev/utils/app/SDCardUtils.java
@@ -5,7 +5,6 @@ import android.os.Build;
 import android.os.Environment;
 import android.os.StatFs;
 import android.os.storage.StorageManager;
-import android.support.annotation.RequiresApi;
 import android.text.format.Formatter;
 
 import java.io.File;
@@ -17,7 +16,6 @@ import java.util.List;
 
 import dev.DevUtils;
 import dev.utils.LogPrintUtils;
-import dev.utils.common.FileUtils;
 
 /**
  * detail: SDCard 工具类
@@ -56,6 +54,17 @@ public final class SDCardUtils {
      */
     public static String getSDCardPath() {
         return Environment.getExternalStorageDirectory().getAbsolutePath();
+    }
+
+    /**
+     * 获取内置 SDCard 绝对路径
+     * <pre>
+     *     结尾添加 File.separator
+     * </pre>
+     * @return 内置 SDCard 绝对路径
+     */
+    public static String getSDCardPathSeparator() {
+        return Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator;
     }
 
     // =
@@ -117,6 +126,109 @@ public final class SDCardUtils {
         return listPaths;
     }
 
+    // ============
+    // = 空间信息 =
+    // ============
+
+    /**
+     * 获取内置 SDCard 空间总大小
+     * @return 内置 SDCard 空间总大小
+     */
+    public static String getAllBlockSizeFormat() {
+        try {
+            long size = getAllBlockSize(Environment.getExternalStorageDirectory().getPath());
+            // 格式化
+            return Formatter.formatFileSize(DevUtils.getContext(), size);
+        } catch (Exception e) {
+            LogPrintUtils.eTag(TAG, e, "getAllBlockSizeFormat");
+            return null;
+        }
+    }
+
+    /**
+     * 获取内置 SDCard 空闲空间大小
+     * @return 内置 SDCard 空闲空间大小
+     */
+    public static String getAvailableBlocksFormat() {
+        try {
+            long size = getAvailableBlocks(Environment.getExternalStorageDirectory().getPath());
+            // 格式化
+            return Formatter.formatFileSize(DevUtils.getContext(), size);
+        } catch (Exception e) {
+            LogPrintUtils.eTag(TAG, e, "getAvailableBlocksFormat");
+            return null;
+        }
+    }
+
+    /**
+     * 获取内置 SDCard 已使用空间大小
+     * @return 内置 SDCard 已使用空间大小
+     */
+    public static String getUsedBlocksFormat() {
+        try {
+            long size = getUsedBlocks(Environment.getExternalStorageDirectory().getPath());
+            // 格式化
+            return Formatter.formatFileSize(DevUtils.getContext(), size);
+        } catch (Exception e) {
+            LogPrintUtils.eTag(TAG, e, "getUsedBlocksFormat");
+            return null;
+        }
+    }
+
+    // =
+
+    /**
+     * 获取内置 SDCard 空间总大小
+     * @return 内置 SDCard 空间总大小
+     */
+    public static long getAllBlockSize() {
+        try {
+            return getAllBlockSize(Environment.getExternalStorageDirectory().getPath());
+        } catch (Exception e) {
+            LogPrintUtils.eTag(TAG, e, "getAllBlockSize");
+            return 0L;
+        }
+    }
+
+    /**
+     * 获取内置 SDCard 空闲空间大小
+     * @return 内置 SDCard 空闲空间大小
+     */
+    public static long getAvailableBlocks() {
+        try {
+            return getAvailableBlocks(Environment.getExternalStorageDirectory().getPath());
+        } catch (Exception e) {
+            LogPrintUtils.eTag(TAG, e, "getAvailableBlocks");
+            return 0L;
+        }
+    }
+
+    /**
+     * 获取内置 SDCard 已使用空间大小
+     * @return 内置 SDCard 已使用空间大小
+     */
+    public static long getUsedBlocks() {
+        try {
+            return getUsedBlocks(Environment.getExternalStorageDirectory().getPath());
+        } catch (Exception e) {
+            LogPrintUtils.eTag(TAG, e, "getUsedBlocks");
+            return 0L;
+        }
+    }
+
+    /**
+     * 返回内置 SDCard 空间大小信息
+     * @return long[], 0 = 总空间大小, 1 = 空闲空间大小, 2 = 已使用空间大小
+     */
+    public static long[] getBlockSizeInfos() {
+        try {
+            return getBlockSizeInfos(Environment.getExternalStorageDirectory().getPath());
+        } catch (Exception e) {
+            LogPrintUtils.eTag(TAG, e, "getBlockSizeInfos");
+            return null;
+        }
+    }
+
     // =
 
     /**
@@ -128,16 +240,22 @@ public final class SDCardUtils {
         try {
             // 获取路径的存储空间信息
             StatFs statFs = new StatFs(path);
-            // 获取单个数据块的大小(Byte)
-            long blockSize = statFs.getBlockSize();
-            // 获取数据块的数量
-            long blockCount = statFs.getBlockCount();
+            // 单个数据块的大小、数据块数量
+            long blockSize, blockCount;
+            // 版本兼容
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                blockSize = statFs.getBlockSizeLong();
+                blockCount = statFs.getBlockCountLong();
+            } else {
+                blockSize = statFs.getBlockSize();
+                blockCount = statFs.getBlockCount();
+            }
             // 返回空间总大小
             return (blockCount * blockSize);
         } catch (Exception e) {
             LogPrintUtils.eTag(TAG, e, "getAllBlockSize");
         }
-        return -1L;
+        return 0L;
     }
 
     /**
@@ -149,39 +267,51 @@ public final class SDCardUtils {
         try {
             // 获取路径的存储空间信息
             StatFs statFs = new StatFs(path);
-            // 获取单个数据块的大小(Byte)
-            long blockSize = statFs.getBlockSize();
-            // 空闲的数据块的数量
-            long freeBlocks = statFs.getAvailableBlocks();
+            // 单个数据块的大小、空闲的数据块数量
+            long blockSize, availableBlocks;
+            // 版本兼容
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                blockSize = statFs.getBlockSizeLong();
+                availableBlocks = statFs.getAvailableBlocksLong();
+            } else {
+                blockSize = statFs.getBlockSize();
+                availableBlocks = statFs.getAvailableBlocks();
+            }
             // 返回空闲空间
-            return (freeBlocks * blockSize);
+            return (availableBlocks * blockSize);
         } catch (Exception e) {
             LogPrintUtils.eTag(TAG, e, "getAvailableBlocks");
         }
-        return -1L;
+        return 0L;
     }
 
     /**
-     * 获取对应路径已使用的空间大小
+     * 获取对应路径已使用空间大小
      * @param path 路径
-     * @return 对应路径已使用的空间大小
+     * @return 对应路径已使用空间大小
      */
-    public static long getAlreadyBlock(final String path) {
+    public static long getUsedBlocks(final String path) {
         try {
             // 获取路径的存储空间信息
             StatFs statFs = new StatFs(path);
-            // 获取单个数据块的大小(Byte)
-            long blockSize = statFs.getBlockSize();
-            // 获取数据块的数量
-            long blockCount = statFs.getBlockCount();
-            // 空闲的数据块的数量
-            long freeBlocks = statFs.getAvailableBlocks();
-            // 返回空间总大小
-            return ((blockCount - freeBlocks) * blockSize);
+            // 单个数据块的大小、数据块数量、空闲的数据块数量
+            long blockSize, blockCount, availableBlocks;
+            // 版本兼容
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                blockSize = statFs.getBlockSizeLong();
+                blockCount = statFs.getBlockCountLong();
+                availableBlocks = statFs.getAvailableBlocksLong();
+            } else {
+                blockSize = statFs.getBlockSize();
+                blockCount = statFs.getBlockCount();
+                availableBlocks = statFs.getAvailableBlocks();
+            }
+            // 返回已使用空间大小
+            return ((blockCount - availableBlocks) * blockSize);
         } catch (Exception e) {
-            LogPrintUtils.eTag(TAG, e, "getAlreadyBlock");
+            LogPrintUtils.eTag(TAG, e, "getUsedBlocks");
         }
-        return -1L;
+        return 0L;
     }
 
     /**
@@ -193,17 +323,23 @@ public final class SDCardUtils {
         try {
             // 获取路径的存储空间信息
             StatFs statFs = new StatFs(path);
-            // 获取单个数据块的大小(Byte)
-            long blockSize = statFs.getBlockSize();
-            // 获取数据块的数量
-            long blockCount = statFs.getBlockCount();
-            // 空闲的数据块的数量
-            long freeBlocks = statFs.getAvailableBlocks();
+            // 单个数据块的大小、数据块数量、空闲的数据块数量
+            long blockSize, blockCount, availableBlocks;
+            // 版本兼容
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                blockSize = statFs.getBlockSizeLong();
+                blockCount = statFs.getBlockCountLong();
+                availableBlocks = statFs.getAvailableBlocksLong();
+            } else {
+                blockSize = statFs.getBlockSize();
+                blockCount = statFs.getBlockCount();
+                availableBlocks = statFs.getAvailableBlocks();
+            }
             // 计算空间信息
             long[] blocks = new long[3];
             blocks[0] = blockSize * blockCount;
-            blocks[1] = blockSize * freeBlocks;
-            blocks[2] = ((blockCount - freeBlocks) * blockSize);
+            blocks[1] = blockSize * availableBlocks;
+            blocks[2] = ((blockCount - availableBlocks) * blockSize);
             // 返回空间信息
             return blocks;
         } catch (Exception e) {
@@ -212,83 +348,11 @@ public final class SDCardUtils {
         return null;
     }
 
-    /**
-     * 获取 SDCard 总大小
-     * @return
-     */
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
-    public static String getSDTotalSize() {
-        try {
-            File path = Environment.getExternalStorageDirectory();
-            StatFs stat = new StatFs(path.getPath());
-            long blockSize = stat.getBlockSizeLong();
-            long totalBlocks = stat.getBlockCountLong();
-            return Formatter.formatFileSize(DevUtils.getContext(), blockSize * totalBlocks);
-        } catch (Exception e) {
-            LogPrintUtils.eTag(TAG, e, "getSDTotalSize");
-        }
-        return "unknown";
-    }
-
-    /**
-     * 获取 SDCard 剩余容量, 即可用大小
-     * @return
-     */
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
-    public static String getSDAvailableSize() {
-        try {
-            File path = Environment.getExternalStorageDirectory();
-            StatFs stat = new StatFs(path.getPath());
-            long blockSize = stat.getBlockSizeLong();
-            long availableBlocks = stat.getAvailableBlocksLong();
-            return Formatter.formatFileSize(DevUtils.getContext(), blockSize * availableBlocks);
-        } catch (Exception e) {
-            LogPrintUtils.eTag(TAG, e, "getSDAvailableSize");
-        }
-        return "unknown";
-    }
-
-    /**
-     * 获取机身内存总大小
-     * @return
-     */
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
-    public static String getRomTotalSize() {
-        try {
-            File path = Environment.getDataDirectory();
-            StatFs stat = new StatFs(path.getPath());
-            long blockSize = stat.getBlockSizeLong();
-            long totalBlocks = stat.getBlockCountLong();
-            return Formatter.formatFileSize(DevUtils.getContext(), blockSize * totalBlocks);
-        } catch (Exception e) {
-            LogPrintUtils.eTag(TAG, e, "getRomTotalSize");
-        }
-        return "unknown";
-    }
-
-    /**
-     * 获取机身可用内存
-     * @return
-     */
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
-    public static String getRomAvailableSize() {
-        try {
-            File path = Environment.getDataDirectory();
-            StatFs stat = new StatFs(path.getPath());
-            long blockSize = stat.getBlockSizeLong();
-            long availableBlocks = stat.getAvailableBlocksLong();
-            return Formatter.formatFileSize(DevUtils.getContext(), blockSize * availableBlocks);
-        } catch (Exception e) {
-            LogPrintUtils.eTag(TAG, e, "getRomAvailableSize");
-        }
-        return "unknown";
-    }
-
     // =
 
     /**
-     * 获取缓存地址
-     * @return
+     * 获取 App Cache 文件夹地址
+     * @return App Cache 文件夹地址
      */
     public static String getDiskCacheDir() {
         String cachePath;
@@ -298,33 +362,91 @@ public final class SDCardUtils {
             cachePath = DevUtils.getContext().getCacheDir().getPath();
         }
         // 防止不存在目录文件, 自动创建
-        FileUtils.createFolder(cachePath);
+        createFolder(cachePath);
         // 返回文件存储地址
         return cachePath;
     }
 
     /**
-     * 获取缓存资源地址
+     * 获取 App Cache 路径 File
      * @param filePath 文件路径
-     * @return
+     * @return App Cache 路径 File
      */
     public static File getCacheFile(final String filePath) {
-        return FileUtils.getFile(getCachePath(filePath));
+        return getFile(getCachePath(filePath));
     }
 
     /**
-     * 获取缓存资源地址
+     * 获取 App Cache 路径
      * @param filePath 文件路径
-     * @return
+     * @return App Cache 路径
      */
     public static String getCachePath(final String filePath) {
         if (filePath == null) return null;
         // 获取缓存地址
         String cachePath = new File(getDiskCacheDir(), filePath).getAbsolutePath();
         // 防止不存在目录文件, 自动创建
-        FileUtils.createFolder(cachePath);
+        createFolder(cachePath);
         // 返回缓存地址
         return cachePath;
+    }
+
+    // ======================
+    // = 其他工具类实现代码 =
+    // ======================
+
+    // =============
+    // = FileUtils =
+    // =============
+
+    /**
+     * 获取文件
+     * @param filePath 文件路径
+     * @return 文件 {@link File}
+     */
+    private static File getFile(final String filePath) {
+        return getFileByPath(filePath);
+    }
+
+    /**
+     * 获取文件
+     * @param filePath 文件路径
+     * @return 文件 {@link File}
+     */
+    private static File getFileByPath(final String filePath) {
+        return filePath != null ? new File(filePath) : null;
+    }
+
+    /**
+     * 判断某个文件夹是否创建, 未创建则创建(纯路径 - 无文件名)
+     * @param dirPath 文件夹路径(无文件名字.后缀)
+     * @return {@code true} success, {@code false} fail
+     */
+    private static boolean createFolder(final String dirPath) {
+        return createFolder(getFileByPath(dirPath));
+    }
+
+    /**
+     * 判断某个文件夹是否创建, 未创建则创建(纯路径 - 无文件名)
+     * @param file 文件夹路径(无文件名字.后缀)
+     * @return {@code true} success, {@code false} fail
+     */
+    private static boolean createFolder(final File file) {
+        if (file != null) {
+            try {
+                // 当这个文件夹不存在的时候则创建文件夹
+                if (!file.exists()) {
+                    // 允许创建多级目录
+                    return file.mkdirs();
+                    // 这个无法创建多级目录
+                    // rootFile.mkdir();
+                }
+                return true;
+            } catch (Exception e) {
+                LogPrintUtils.eTag(TAG, e, "createFolder");
+            }
+        }
+        return false;
     }
 }
 
