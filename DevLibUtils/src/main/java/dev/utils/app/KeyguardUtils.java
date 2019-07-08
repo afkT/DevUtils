1d165d9ad45919f2df41e206316bedc7f3298c8d
Ttt <13798405957@163.com>
2019-07-08 15:16:51 +0800

    1.规范化 app.KeyguardUtils javadoc 注释代码, 以及遗漏的 @return、@param 注释代码等
    Former-commit-id: 472b631e9fd3cf632cb61c58e615e523c49d75fa

--- DevLibUtils/src/main/java/dev/utils/app/KeyguardUtils.java
+++ DevLibUtils/src/main/java/dev/utils/app/KeyguardUtils.java
@@ -7,9 +7,10 @@ import android.support.annotation.RequiresApi;
 import android.support.annotation.RequiresPermission;
 
 import dev.DevUtils;
+import dev.utils.LogPrintUtils;
 
 /**
- * detail: 锁屏管理工具类(锁屏、禁用锁屏, 判断是否锁屏)
+ * detail: 锁屏管理工具类 ( 锁屏、禁用锁屏, 判断是否锁屏 )
  * @author Ttt
  * <pre>
  *     需要的权限:
@@ -19,6 +20,8 @@ import dev.DevUtils;
 @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
 public final class KeyguardUtils {
 
+    // 日志 TAG
+    private static final String TAG = KeyguardUtils.class.getSimpleName();
     // KeyguardUtils 实例
     private static KeyguardUtils sInstance;
 
@@ -35,54 +38,57 @@ public final class KeyguardUtils {
 
     // 锁屏管理类
     private KeyguardManager mKeyguardManager;
-    // android-26 开始过时
+    // android 26 开始过时
     private KeyguardManager.KeyguardLock mKeyguardLock;
 
     /**
      * 构造函数
      */
     private KeyguardUtils() {
-        // 获取系统服务
-        mKeyguardManager = (KeyguardManager) DevUtils.getContext().getSystemService(Context.KEYGUARD_SERVICE);
-        // 初始化锁
-        mKeyguardLock = mKeyguardManager.newKeyguardLock("KeyguardUtils");
+        try {
+            // 获取系统服务
+            mKeyguardManager = (KeyguardManager) DevUtils.getContext().getSystemService(Context.KEYGUARD_SERVICE);
+            // 初始化锁
+            mKeyguardLock = mKeyguardManager.newKeyguardLock(TAG);
+        } catch (Exception e) {
+            LogPrintUtils.eTag(TAG, e, "KeyguardUtils");
+        }
     }
 
     /**
-     * 是否锁屏 - android 4.1以上支持
-     * @return
+     * 是否锁屏 - android 4.1 以上支持
+     * @return {@code true} yes, {@code false} no
      */
     public boolean isKeyguardLocked() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
-            return false;
-        } else {
-            return mKeyguardManager.isKeyguardLocked();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            if (mKeyguardManager != null) return mKeyguardManager.isKeyguardLocked();
         }
+        return false;
     }
 
     /**
-     * 是否有锁屏密码 - android 4.1以上支持
-     * @return
+     * 是否有锁屏密码 - android 4.1 以上支持
+     * @return {@code true} yes, {@code false} no
      */
     public boolean isKeyguardSecure() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
-            return false;
-        } else {
-            return mKeyguardManager.isKeyguardSecure();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            if (mKeyguardManager != null) return mKeyguardManager.isKeyguardSecure();
         }
+        return false;
     }
 
     /**
-     * 是否锁屏 - android 4.1 之前
-     * @return
+     * 是否锁屏
+     * @return {@code true} yes, {@code false} no
      */
     public boolean inKeyguardRestrictedInputMode() {
-        return mKeyguardManager.inKeyguardRestrictedInputMode();
+        if (mKeyguardManager != null) return mKeyguardManager.inKeyguardRestrictedInputMode();
+        return false;
     }
 
     /**
      * 获取 KeyguardManager
-     * @return
+     * @return {@link KeyguardManager}
      */
     public KeyguardManager getKeyguardManager() {
         return mKeyguardManager;
@@ -90,7 +96,7 @@ public final class KeyguardUtils {
 
     /**
      * 设置 KeyguardManager
-     * @param keyguardManager
+     * @param keyguardManager {@link KeyguardManager}
      */
     public void setKeyguardManager(final KeyguardManager keyguardManager) {
         this.mKeyguardManager = keyguardManager;
@@ -105,16 +111,20 @@ public final class KeyguardUtils {
      */
     @RequiresPermission(android.Manifest.permission.DISABLE_KEYGUARD)
     public void disableKeyguard() {
-        mKeyguardLock.disableKeyguard();
+        if (mKeyguardLock != null) {
+            mKeyguardLock.disableKeyguard();
+        }
     }
 
     /**
-     * 使能显示锁屏界面, 如果你之前调用了disableKeyguard()方法取消锁屏界面, 那么会马上显示锁屏界面
+     * 使能显示锁屏界面, 如果你之前调用了 disableKeyguard() 方法取消锁屏界面, 那么会马上显示锁屏界面
      * <uses-permission android:name="android.permission.DISABLE_KEYGUARD"/>
      */
     @RequiresPermission(android.Manifest.permission.DISABLE_KEYGUARD)
     public void reenableKeyguard() {
-        mKeyguardLock.reenableKeyguard();
+        if (mKeyguardLock != null) {
+            mKeyguardLock.reenableKeyguard();
+        }
     }
 
     /**
@@ -131,15 +141,17 @@ public final class KeyguardUtils {
 
     /**
      * 生成一个新的 KeyguardLock, 并且设置 TAG
-     * @param tag
+     * @param tag TAG
      */
     public void newKeyguardLock(final String tag) {
-        mKeyguardLock = mKeyguardManager.newKeyguardLock(tag);
+        if (mKeyguardManager != null && tag != null) {
+            this.mKeyguardLock = mKeyguardManager.newKeyguardLock(tag);
+        }
     }
 
     /**
      * 获取 KeyguardManager.KeyguardLock
-     * @return
+     * @return {@link KeyguardManager.KeyguardLock}
      */
     public KeyguardManager.KeyguardLock getKeyguardLock() {
         return mKeyguardLock;
@@ -147,7 +159,7 @@ public final class KeyguardUtils {
 
     /**
      * 设置 KeyguardManager.KeyguardLock
-     * @param keyguardLock
+     * @param keyguardLock {@link KeyguardManager.KeyguardLock}
      */
     public void setKeyguardLock(final KeyguardManager.KeyguardLock keyguardLock) {
         this.mKeyguardLock = keyguardLock;
