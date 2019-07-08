d4b82495cbc1e2eb71ac8033315edccd8af3df6a
Ttt <13798405957@163.com>
2019-07-08 11:52:25 +0800

    1.规范化 app.LocationUtils javadoc 注释代码, 以及遗漏的 @return、@param 注释代码等
    Former-commit-id: b9c852e7f178ace33a5f5032ba124771256e0c40

--- DevLibUtils/src/main/java/dev/utils/app/LocationUtils.java
+++ DevLibUtils/src/main/java/dev/utils/app/LocationUtils.java
@@ -1,6 +1,7 @@
 package dev.utils.app;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.location.Address;
 import android.location.Criteria;
@@ -12,14 +13,11 @@ import android.location.LocationProvider;
 import android.os.Bundle;
 import android.provider.Settings;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
 import dev.DevUtils;
 import dev.utils.LogPrintUtils;
-
-import static android.content.Context.LOCATION_SERVICE;
 
 /**
  * detail: 定位相关工具类
@@ -32,7 +30,7 @@ public final class LocationUtils {
 
     // 日志 TAG
     private static final String TAG = LocationUtils.class.getSimpleName();
-    // 时间常量 = 2分钟
+    // 时间常量 - 2 分钟
     private static final int MINUTES_TWO = 1000 * 60 * 2;
     // 定位改变通知事件
     private static OnLocationChangeListener sListener;
@@ -44,16 +42,16 @@ public final class LocationUtils {
     /**
      * 获取位置, 需要先判断是否开启了定位
      * <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
-     * @param listener
+     * @param listener {@link LocationListener}
      * @param time     间隔时间
      * @param distance 间隔距离
-     * @return
+     * @return {@link Location}
      */
     @SuppressLint("MissingPermission")
     public static Location getLocation(final LocationListener listener, final long time, final float distance) {
         Location location = null;
         try {
-            sLocationManager = (LocationManager) DevUtils.getContext().getSystemService(LOCATION_SERVICE);
+            sLocationManager = (LocationManager) DevUtils.getContext().getSystemService(Context.LOCATION_SERVICE);
             if (isLocationEnabled()) {
                 sLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, time, distance, listener);
                 if (sLocationManager != null) {
@@ -84,13 +82,13 @@ public final class LocationUtils {
     }
 
     /**
-     * 判断Gps是否可用
+     * 判断 GPS 是否可用
      * @return {@code true} yes, {@code false} no
      */
     public static boolean isGpsEnabled() {
         try {
-            LocationManager lm = (LocationManager) DevUtils.getContext().getSystemService(LOCATION_SERVICE);
-            return lm != null && lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            LocationManager locationManager = (LocationManager) DevUtils.getContext().getSystemService(Context.LOCATION_SERVICE);
+            return locationManager != null && locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
         } catch (Exception e) {
             LogPrintUtils.eTag(TAG, e, "isGpsEnabled");
         }
@@ -103,8 +101,8 @@ public final class LocationUtils {
      */
     public static boolean isLocationEnabled() {
         try {
-            LocationManager lm = (LocationManager) DevUtils.getContext().getSystemService(LOCATION_SERVICE);
-            return lm != null && (lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER) || lm.isProviderEnabled(LocationManager.GPS_PROVIDER));
+            LocationManager locationManager = (LocationManager) DevUtils.getContext().getSystemService(Context.LOCATION_SERVICE);
+            return locationManager != null && (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) || locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER));
         } catch (Exception e) {
             LogPrintUtils.eTag(TAG, e, "isLocationEnabled");
         }
@@ -124,13 +122,13 @@ public final class LocationUtils {
     }
 
     /**
-     * 注册 - 使用完记得调用{@link #unregister()}
+     * 注册 - 使用完记得调用 {@link #unregister()}
      * <uses-permission android:name="android.permission.INTERNET" />
      * <uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION" />
      * <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
-     * 如果 minDistance 为 0, 则通过 minTime 来定时更新, minDistance 不为 0, 则以 minDistance 为准, 两者都为0, 则随时刷新
-     * @param minTime     位置信息更新周期(单位: 毫秒)
-     * @param minDistance 位置变化最小距离: 当位置距离变化超过此值时, 将更新位置信息(单位: 米)
+     * 如果 minDistance 为 0, 则通过 minTime 来定时更新, minDistance 不为 0, 则以 minDistance 为准, 两者都为 0, 则随时刷新
+     * @param minTime     位置信息更新周期 ( 单位: 毫秒 )
+     * @param minDistance 位置变化最小距离: 当位置距离变化超过此值时, 将更新位置信息 ( 单位: 米 )
      * @param listener    位置刷新的回调接口
      * @return {@code true} 初始化成功, {@code false} 初始化失败
      */
@@ -138,7 +136,7 @@ public final class LocationUtils {
     public static boolean register(final long minTime, final long minDistance, final OnLocationChangeListener listener) {
         if (listener == null) return false;
         try {
-            sLocationManager = (LocationManager) DevUtils.getContext().getSystemService(LOCATION_SERVICE);
+            sLocationManager = (LocationManager) DevUtils.getContext().getSystemService(Context.LOCATION_SERVICE);
             if (sLocationManager == null || (!sLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
                     && !sLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))) {
                 return false;
@@ -179,12 +177,12 @@ public final class LocationUtils {
     }
 
     /**
-     * 设置定位参数
+     * 获取定位参数对象
      * @return {@link Criteria}
      */
     private static Criteria getCriteria() {
         Criteria criteria = new Criteria();
-        // 设置定位精确度 Criteria.ACCURACY_COARSE比较粗略, Criteria.ACCURACY_FINE则比较精细
+        // 设置定位精确度 Criteria.ACCURACY_COARSE 比较粗略, Criteria.ACCURACY_FINE 则比较精细
         criteria.setAccuracy(Criteria.ACCURACY_FINE);
         // 设置是否要求速度
         criteria.setSpeedRequired(false);
@@ -210,7 +208,7 @@ public final class LocationUtils {
             Geocoder geocoder = new Geocoder(DevUtils.getContext(), Locale.getDefault());
             List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
             if (addresses.size() > 0) return addresses.get(0);
-        } catch (IOException e) {
+        } catch (Exception e) {
             LogPrintUtils.eTag(TAG, e, "getAddress");
         }
         return null;
@@ -246,27 +244,29 @@ public final class LocationUtils {
      */
     public static String getStreet(final double latitude, final double longitude) {
         Address address = getAddress(latitude, longitude);
-        return address == null ? "unknown" : address.getAddressLine(0);
+        try {
+            return address == null ? "unknown" : address.getAddressLine(0);
+        } catch (Exception e) {
+            LogPrintUtils.eTag(TAG, e, "getStreet");
+        }
+        return "unknown";
     }
 
     /**
-     * 是否更好的位置
-     * @param newLocation
-     * @param currentBestLocation
+     * 判断是否更好的位置
+     * @param newLocation         新的位置
+     * @param currentBestLocation 当前最佳位置
      * @return {@code true} yes, {@code false} no
      */
     public static boolean isBetterLocation(final Location newLocation, final Location currentBestLocation) {
-        if (newLocation == null || currentBestLocation == null) {
-            // A new location is always better than no location
-            return true;
-        }
+        if (newLocation == null || currentBestLocation == null) return true;
         // 检查位置信息的时间间隔
         long timeDelta = newLocation.getTime() - currentBestLocation.getTime();
         boolean isSignificantlyNewer = timeDelta > MINUTES_TWO;
         boolean isSignificantlyOlder = timeDelta < -MINUTES_TWO;
         boolean isNewer = timeDelta > 0;
 
-        // 如果时间超过2分钟, 则使用新的位置
+        // 如果时间超过 2 分钟, 则使用新的位置
         if (isSignificantlyNewer) {
             return true;
         } else if (isSignificantlyOlder) { // 时间超过两分钟
@@ -292,8 +292,8 @@ public final class LocationUtils {
 
     /**
      * 是否相同的提供者
-     * @param provider0 提供者1
-     * @param provider1 提供者2
+     * @param provider0 提供者 1
+     * @param provider1 提供者 2
      * @return {@code true} yes, {@code false} no
      */
     public static boolean isSameProvider(final String provider0, final String provider1) {
@@ -304,11 +304,13 @@ public final class LocationUtils {
     }
 
     /**
-     * 自定义定位监听事件
+     * detail: 自定义定位监听事件
+     * @author Ttt
      */
     private static class CustomLocationListener implements LocationListener {
+
         /**
-         * 当坐标改变时触发此函数, 如果Provider传进相同的坐标, 它就不会被触发
+         * 当坐标改变时触发此函数, 如果 Provider 传进相同的坐标, 它就不会被触发
          * @param location 坐标
          */
         @Override
@@ -319,7 +321,7 @@ public final class LocationUtils {
         }
 
         /**
-         * provider的在可用、暂时不可用和无服务三个状态直接切换时触发此函数
+         * provider 的在可用、暂时不可用和无服务三个状态直接切换时触发此函数
          * @param provider 提供者
          * @param status   状态
          * @param extras   provider 可选包
@@ -331,20 +333,20 @@ public final class LocationUtils {
             }
             switch (status) {
                 case LocationProvider.AVAILABLE:
-                    LogPrintUtils.dTag(TAG, "当前GPS状态为可见状态");
+                    LogPrintUtils.dTag(TAG, "当前 GPS 状态为可见状态");
                     break;
                 case LocationProvider.OUT_OF_SERVICE:
-                    LogPrintUtils.dTag(TAG, "当前GPS状态为服务区外状态");
+                    LogPrintUtils.dTag(TAG, "当前 GPS 状态为服务区外状态");
                     break;
                 case LocationProvider.TEMPORARILY_UNAVAILABLE:
-                    LogPrintUtils.dTag(TAG, "当前GPS状态为暂停服务状态");
+                    LogPrintUtils.dTag(TAG, "当前 GPS 状态为暂停服务状态");
                     break;
             }
         }
 
         /**
          * provider 被 enable 时触发此函数, 比如 GPS 被打开
-         * @param provider
+         * @param provider 提供者
          */
         @Override
         public void onProviderEnabled(String provider) {
@@ -352,7 +354,7 @@ public final class LocationUtils {
 
         /**
          * provider 被 disable 时触发此函数, 比如 GPS 被关闭
-         * @param provider
+         * @param provider 提供者
          */
         @Override
         public void onProviderDisabled(String provider) {
@@ -372,16 +374,16 @@ public final class LocationUtils {
         void getLastKnownLocation(Location location);
 
         /**
-         * 当坐标改变时触发此函数, 如果Provider传进相同的坐标, 它就不会被触发
+         * 当坐标改变时触发此函数, 如果 Provider 传进相同的坐标, 它就不会被触发
          * @param location 坐标
          */
         void onLocationChanged(Location location);
 
         /**
-         * provider的在可用、暂时不可用和无服务三个状态直接切换时触发此函数
+         * provider 的在可用、暂时不可用和无服务三个状态直接切换时触发此函数
          * @param provider 提供者
          * @param status   状态
-         * @param extras   provider可选包
+         * @param extras   provider 可选包
          */
         void onStatusChanged(String provider, int status, Bundle extras); // 位置状态发生改变
     }
