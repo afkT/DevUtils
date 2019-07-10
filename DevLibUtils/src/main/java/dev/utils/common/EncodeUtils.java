dfa7fbb69b4cec51aa3c3ecf4c89ad7892188d08
Ttt <13798405957@163.com>
2019-07-10 16:20:31 +0800

    1.规范化 common.EncodeUtils javadoc 注释代码, 以及遗漏的 @return、@param 注释代码等
    Former-commit-id: 45d18b101b0674dc57c2ad9e495cdc6ab2735b50

--- DevLibUtils/src/main/java/dev/utils/common/EncodeUtils.java
+++ DevLibUtils/src/main/java/dev/utils/common/EncodeUtils.java
@@ -1,17 +1,20 @@
 package dev.utils.common;
 
-import android.os.Build;
-import android.text.Html;
-import android.util.Base64;
-
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-
-import dev.utils.LogPrintUtils;
+import dev.utils.JCLogUtils;
+import dev.utils.common.cipher.Base64;
 
 /**
  * detail: 编码工具类
  * @author Ttt
+ * <pre>
+ *     Base64 flags:
+ *     <p></p>
+ *     CRLF 这个参数看起来比较眼熟, 它就是 Win 风格的换行符, 意思就是使用 CR LF 这一对作为一行的结尾而不是 Unix 风格的 LF
+ *     DEFAULT 这个参数是默认, 使用默认的方法来加密
+ *     NO_PADDING 这个参数是略去加密字符串最后的 =
+ *     NO_WRAP 这个参数意思是略去所有的换行符 ( 设置后 CRLF 就没用了 )
+ *     URL_SAFE 这个参数意思是加密时不使用对 URL 和文件名有特殊意义的字符来作为加密字符, 具体就是以 - 和 _ 取代 + 和 /
+ * </pre>
  */
 public final class EncodeUtils {
 
@@ -21,162 +24,189 @@ public final class EncodeUtils {
     // 日志 TAG
     private static final String TAG = EncodeUtils.class.getSimpleName();
 
-    /**
-     * url 编码 - UTF-8
-     * @param input
-     * @return
-     */
-    public static String urlEncode(final String input) {
-        return urlEncode(input, "UTF-8");
-    }
+    // ==========
+    // = Base64 =
+    // ==========
+
+    // ===============
+    // = Base64 编码 =
+    // ===============
 
     /**
-     * url 编码
-     * @param input
-     * @param charsetName
-     * @return
-     */
-    public static String urlEncode(final String input, final String charsetName) {
-        try {
-            return URLEncoder.encode(input, charsetName);
-        } catch (Exception e) {
-            LogPrintUtils.eTag(TAG, e, "urlEncode");
-        }
-        return null;
-    }
-
-    /**
-     * url 解码 - UTF-8
-     * @param input
-     * @return
-     */
-    public static String urlDecode(final String input) {
-        return urlDecode(input, "UTF-8");
-    }
-
-    /**
-     * url 解码
-     * @param input
-     * @param charsetName
-     * @return
-     */
-    public static String urlDecode(final String input, final String charsetName) {
-        try {
-            return URLDecoder.decode(input, charsetName);
-        } catch (Exception e) {
-            LogPrintUtils.eTag(TAG, e, "urlDecode");
-        }
-        return null;
-    }
-
-//    CRLF 这个参数看起来比较眼熟, 它就是Win风格的换行符, 意思就是使用CR LF这一对作为一行的结尾而不是Unix风格的LF
-//
-//    DEFAULT 这个参数是默认, 使用默认的方法来加密
-//
-//    NO_PADDING 这个参数是略去加密字符串最后的 =
-//
-//    NO_WRAP 这个参数意思是略去所有的换行符(设置后 CRLF 就没用了)
-//
-//    URL_SAFE 这个参数意思是加密时不使用对URL和文件名有特殊意义的字符来作为加密字符, 具体就是以-和_取代+和/
-
-    /**
-     * base64 编码 => 去除\n 替换 + 和 - 号
-     * @param input
-     * @return
+     * Base64 编码
+     * @param input 待处理数据
+     * @return Base64 编码后的 byte[]
      */
     public static byte[] base64Encode(final String input) {
-        if (input == null) return null;
-        try {
-            return base64Encode(input.getBytes());
-        } catch (Exception e) {
-            LogPrintUtils.eTag(TAG, e, "base64Encode");
-        }
-        return null;
+        return base64Encode(input, Base64.NO_WRAP);
     }
 
     /**
-     * base64 编码
-     * @param input
-     * @return
+     * Base64 编码
+     * @param input 待处理数据
+     * @param flags Base64 编解码 flags
+     * @return Base64 编码后的 byte[]
+     */
+    public static byte[] base64Encode(final String input, final int flags) {
+        return base64Encode(toBytes(input), flags);
+    }
+
+    /**
+     * Base64 编码
+     * @param input 待处理数据
+     * @return Base64 编码后的 byte[]
      */
     public static byte[] base64Encode(final byte[] input) {
-        if (input == null) return null;
-        try {
-            return Base64.encode(input, Base64.NO_WRAP);
-        } catch (Exception e) {
-            LogPrintUtils.eTag(TAG, e, "base64Encode");
-        }
-        return null;
+        return base64Encode(input, Base64.NO_WRAP);
     }
 
     /**
-     * base64 编码
-     * @param input
-     * @return
+     * Base64 编码
+     * @param input 待处理数据
+     * @param flags Base64 编解码 flags
+     * @return Base64 编码后的 byte[]
+     */
+    public static byte[] base64Encode(final byte[] input, final int flags) {
+        if (input == null) return null;
+        return Base64.encode(input, flags);
+    }
+
+    // =
+
+    /**
+     * Base64 编码
+     * @param input 待处理数据
+     * @return Base64 编码后的 byte[] 转 String
+     */
+    public static String base64EncodeToString(final String input) {
+        return base64EncodeToString(input, Base64.NO_WRAP);
+    }
+
+    /**
+     * Base64 编码
+     * @param input 待处理数据
+     * @param flags Base64 编解码 flags
+     * @return Base64 编码后的 byte[] 转 String
+     */
+    public static String base64EncodeToString(final String input, final int flags) {
+        return base64EncodeToString(toBytes(input), flags);
+    }
+
+    /**
+     * Base64 编码
+     * @param input 待处理数据
+     * @return Base64 编码后的 byte[] 转 String
      */
     public static String base64EncodeToString(final byte[] input) {
-        if (input == null) return null;
-        try {
-            return Base64.encodeToString(input, Base64.NO_WRAP);
-        } catch (Exception e) {
-            LogPrintUtils.eTag(TAG, e, "base64EncodeToString");
-        }
-        return null;
+        return base64EncodeToString(input, Base64.NO_WRAP);
     }
 
-    // =
+    /**
+     * Base64 编码
+     * @param input 待处理数据
+     * @param flags Base64 编解码 flags
+     * @return Base64 编码后的 byte[] 转 String
+     */
+    public static String base64EncodeToString(final byte[] input, final int flags) {
+        if (input == null) return null;
+        return toString(Base64.encode(input, flags));
+    }
+
+    // ===============
+    // = Base64 解码 =
+    // ===============
 
     /**
-     * base64 解码
-     * @param input
-     * @return
+     * Base64 解码
+     * @param input 待处理数据
+     * @return Base64 解码后的 byte[]
      */
     public static byte[] base64Decode(final String input) {
-        if (input == null) return null;
-        try {
-            return Base64.decode(input, Base64.NO_WRAP);
-        } catch (Exception e) {
-            LogPrintUtils.eTag(TAG, e, "base64Decode");
-        }
-        return null;
+        return base64Decode(input, Base64.NO_WRAP);
     }
 
     /**
-     * base64 解码
-     * @param input
-     * @return
+     * Base64 解码
+     * @param input 待处理数据
+     * @param flags Base64 编解码 flags
+     * @return Base64 解码后的 byte[]
+     */
+    public static byte[] base64Decode(final String input, final int flags) {
+        return base64Decode(toBytes(input), flags);
+    }
+
+    /**
+     * Base64 解码
+     * @param input 待处理数据
+     * @return Base64 解码后的 byte[]
      */
     public static byte[] base64Decode(final byte[] input) {
-        if (input == null) return null;
-        try {
-            return Base64.decode(input, Base64.NO_WRAP);
-        } catch (Exception e) {
-            LogPrintUtils.eTag(TAG, e, "base64Decode");
-        }
-        return null;
+        return base64Decode(input, Base64.NO_WRAP);
     }
 
     /**
-     * base64 解码
-     * @param input
-     * @return
+     * Base64 解码
+     * @param input 待处理数据
+     * @param flags Base64 编解码 flags
+     * @return Base64 解码后的 byte[]
      */
-    public static String base64DecodeToString(final byte[] input) {
+    public static byte[] base64Decode(final byte[] input, final int flags) {
         if (input == null) return null;
-        try {
-            return new String(base64Decode(input));
-        } catch (Exception e) {
-            LogPrintUtils.eTag(TAG, e, "base64DecodeToString");
-        }
-        return null;
+        return Base64.decode(input, flags);
     }
 
     // =
 
     /**
-     * html 编码
-     * @param input
-     * @return
+     * Base64 解码
+     * @param input 待处理数据
+     * @return Base64 解码后的 byte[] 转 String
+     */
+    public static String base64DecodeToString(final String input) {
+        return base64DecodeToString(input, Base64.NO_WRAP);
+    }
+
+    /**
+     * Base64 解码
+     * @param input 待处理数据
+     * @param flags Base64 编解码 flags
+     * @return Base64 解码后的 byte[] 转 String
+     */
+    public static String base64DecodeToString(final String input, final int flags) {
+        return base64DecodeToString(toBytes(input), flags);
+    }
+
+    /**
+     * Base64 解码
+     * @param input 待处理数据
+     * @return Base64 解码后的 byte[] 转 String
+     */
+    public static String base64DecodeToString(final byte[] input) {
+        return base64DecodeToString(input, Base64.NO_WRAP);
+    }
+
+    /**
+     * Base64 解码
+     * @param input 待处理数据
+     * @param flags Base64 编解码 flags
+     * @return Base64 解码后的 byte[] 转 String
+     */
+    public static String base64DecodeToString(final byte[] input, final int flags) {
+        if (input == null) return null;
+        return toString(Base64.decode(input, flags));
+    }
+
+    // ========
+    // = Html =
+    // ========
+
+    /**
+     * Html 字符串编码
+     * <pre>
+     *     TextUtils.htmlEncode()
+     * </pre>
+     * @param input 待处理数据
+     * @return Html 编码后的数据
      */
     public static String htmlEncode(final CharSequence input) {
         if (input == null) return null;
@@ -195,11 +225,10 @@ public final class EncodeUtils {
                     builder.append("&amp;"); //$NON-NLS-1$
                     break;
                 case '\'':
-                    // http://www.w3.org/TR/xhtml1
-                    // The named character reference &apos; (the apostrophe, U+0027) was
-                    // introduced in XML 1.0 but does not appear in HTML. Authors should
-                    // therefore use &#39; instead of &apos; to work as expected in HTML 4
-                    // user agents.
+                    //http://www.w3.org/TR/xhtml1
+                    // The named character reference &apos; (the apostrophe, U+0027) was introduced in
+                    // XML 1.0 but does not appear in HTML. Authors should therefore use &#39; instead
+                    // of &apos; to work as expected in HTML 4 user agents.
                     builder.append("&#39;"); //$NON-NLS-1$
                     break;
                 case '"':
@@ -212,22 +241,52 @@ public final class EncodeUtils {
         return builder.toString();
     }
 
+    // ======================
+    // = 其他工具类实现代码 =
+    // ======================
+
+    // ================
+    // = ConvertUtils =
+    // ================
+
     /**
-     * html 解码
-     * @param input
-     * @return
+     * byte[] 转 String
+     * @param data byte[]
+     * @return {@link String}
      */
-    @SuppressWarnings("deprecation")
-    public static CharSequence htmlDecode(final String input) {
-        if (input == null) return null;
-        try {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                return Html.fromHtml(input, Html.FROM_HTML_MODE_LEGACY);
-            } else {
-                return Html.fromHtml(input);
+    private static String toString(final byte[] data) {
+        return toString(data, null);
+    }
+
+    /**
+     * byte[] 转 String
+     * @param data       byte[]
+     * @param defaultStr 默认字符串
+     * @return {@link String} 如果转换失败, 则返回 defaultStr
+     */
+    private static String toString(final byte[] data, final String defaultStr) {
+        if (data != null) {
+            try {
+                return new String(data);
+            } catch (Exception e) {
+                JCLogUtils.eTag(TAG, e, "toString");
             }
-        } catch (Exception e) {
-            LogPrintUtils.eTag(TAG, e, "htmlDecode");
+        }
+        return defaultStr;
+    }
+
+    /**
+     * 字符串 获取 byte[]
+     * @param str String
+     * @return byte[]
+     */
+    private static byte[] toBytes(final String str) {
+        if (str != null) {
+            try {
+                return str.getBytes();
+            } catch (Exception e) {
+                JCLogUtils.eTag(TAG, e, "toBytes");
+            }
         }
         return null;
     }
