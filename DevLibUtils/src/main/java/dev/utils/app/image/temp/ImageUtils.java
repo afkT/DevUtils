c8d2401ddfc1ab0e771c975c7367b3886e990a2c
Ttt <13798405957@163.com>
2019-07-21 02:27:59 +0800

    1.增加 ImageUtils (temp) 整理中
    Former-commit-id: f5fdba6dd3d2fdcccbf1cb241fa17534b875a818

---
+++ DevLibUtils/src/main/java/dev/utils/app/image/temp/ImageUtils.java
@@ -0,0 +1,59 @@
+package dev.utils.app.image.temp;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+
+/**
+ * detail: Image ( Bitmap、Drawable 等 ) 工具类
+ * @author Ttt
+ */
+public final class ImageUtils {
+
+    private ImageUtils() {
+    }
+
+    // 日志 TAG
+    private static final String TAG = ImageUtils.class.getSimpleName();
+
+    /**
+     * 判断 Bitmap 对象是否为 null
+     * @param bitmap {@link Bitmap}
+     * @return {@code true} yes, {@code false} no
+     */
+    public static boolean isEmpty(final Bitmap bitmap) {
+        return bitmap == null || bitmap.getWidth() == 0 || bitmap.getHeight() == 0;
+    }
+
+    /**
+     * 判断 Drawable 对象是否为 null
+     * @param drawable {@link Drawable}
+     * @return {@code true} yes, {@code false} no
+     */
+    public static boolean isEmpty(final Drawable drawable) {
+        return drawable == null;
+    }
+
+    /**
+     * 判断 Bitmap 对象是否不为 null
+     * @param bitmap {@link Bitmap}
+     * @return {@code true} yes, {@code false} no
+     */
+    public static boolean isNotEmpty(final Bitmap bitmap) {
+        return bitmap != null && bitmap.getWidth() != 0 && bitmap.getHeight() != 0;
+    }
+
+    /**
+     * 判断 Drawable 对象是否不为 null
+     * @param drawable {@link Drawable}
+     * @return {@code true} yes, {@code false} no
+     */
+    public static boolean isNotEmpty(final Drawable drawable) {
+        return drawable != null;
+    }
+
+    // ===========================
+    // = Bitmap、Drawable 等获取 =
+    // ===========================
+
+
+}
