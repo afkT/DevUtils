41c2d807843cb9b9a9cb59957a346b639fde5e5e
Ttt <13798405957@163.com>
2019-07-23 15:25:34 +0800

    1.增加 ImageViewUtils 部分设置方法
    Former-commit-id: d336ed3dfc63db3d9c17b1aa14447aa6639fea14

--- DevLibUtils/src/main/java/dev/utils/app/ImageViewUtils.java
+++ DevLibUtils/src/main/java/dev/utils/app/ImageViewUtils.java
@@ -1,7 +1,16 @@
 package dev.utils.app;
 
 import android.app.Activity;
+import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.ColorInt;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.IdRes;
+import android.support.annotation.RequiresApi;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,9 +33,9 @@ public final class ImageViewUtils {
     // 日志 TAG
     private static final String TAG = ImageViewUtils.class.getSimpleName();
 
-    // =================
+    // ==================
     // = 获取 ImageView =
-    // =================
+    // ==================
 
     /**
      * 获取 ImageView
@@ -98,6 +107,372 @@ public final class ImageViewUtils {
         }
         return null;
     }
+
+    // ========
+    // = 背景 =
+    // ========
+
+    /**
+     * 设置背景图片
+     * @param view       {@link View}
+     * @param background 背景图片
+     */
+    public static void setBackground(final View view, final Drawable background) {
+        if (view != null) {
+            try {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    view.setBackground(background);
+                } else {
+                    view.setBackgroundDrawable(background);
+                }
+            } catch (Exception e) {
+                LogPrintUtils.eTag(TAG, e, "setBackground");
+            }
+        }
+    }
+
+    /**
+     * 设置背景颜色
+     * @param view  {@link View}
+     * @param color 背景颜色
+     */
+    public static void setBackgroundColor(final View view, @ColorInt final int color) {
+        if (view != null) {
+            try {
+                view.setBackgroundColor(color);
+            } catch (Exception e) {
+                LogPrintUtils.eTag(TAG, e, "setBackgroundColor");
+            }
+        }
+    }
+
+    /**
+     * 设置背景资源
+     * @param view  {@link View}
+     * @param resId resource identifier
+     */
+    public static void setBackgroundResource(final View view, @DrawableRes final int resId) {
+        if (view != null) {
+            try {
+                view.setBackgroundResource(resId);
+            } catch (Exception e) {
+                LogPrintUtils.eTag(TAG, e, "setBackgroundResource");
+            }
+        }
+    }
+
+    /**
+     * 设置背景着色
+     * @param view {@link View}
+     * @param tint 着色颜色
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public static void setBackgroundTintList(final View view, final ColorStateList tint) {
+        if (view != null) {
+            try {
+                view.setBackgroundTintList(tint);
+            } catch (Exception e) {
+                LogPrintUtils.eTag(TAG, e, "setBackgroundTintList");
+            }
+        }
+    }
+
+    /**
+     * 设置背景着色模式
+     * @param view     {@link View}
+     * @param tintMode 着色模式 {@link PorterDuff.Mode}
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public static void setBackgroundTintMode(final View view, final PorterDuff.Mode tintMode) {
+        if (view != null) {
+            try {
+                view.setBackgroundTintMode(tintMode);
+            } catch (Exception e) {
+                LogPrintUtils.eTag(TAG, e, "setBackgroundTintMode");
+            }
+        }
+    }
+
+    // ========
+    // = 前景 =
+    // ========
+
+    /**
+     * 设置前景图片
+     * @param view       {@link View}
+     * @param foreground 前景图片
+     */
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public static void setForeground(final View view, final Drawable foreground) {
+        if (view != null) {
+            try {
+                view.setForeground(foreground);
+            } catch (Exception e) {
+                LogPrintUtils.eTag(TAG, e, "setForeground");
+            }
+        }
+    }
+
+    /**
+     * 设置前景重心
+     * @param view    {@link View}
+     * @param gravity 重心
+     */
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public static void setForegroundGravity(final View view, final int gravity) {
+        if (view != null) {
+            try {
+                view.setForegroundGravity(gravity);
+            } catch (Exception e) {
+                LogPrintUtils.eTag(TAG, e, "setForegroundGravity");
+            }
+        }
+    }
+
+    /**
+     * 设置前景着色
+     * @param view {@link View}
+     * @param tint 着色颜色
+     */
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public static void setForegroundTintList(final View view, final ColorStateList tint) {
+        if (view != null) {
+            try {
+                view.setForegroundTintList(tint);
+            } catch (Exception e) {
+                LogPrintUtils.eTag(TAG, e, "setForegroundTintList");
+            }
+        }
+    }
+
+    /**
+     * 设置前景着色模式
+     * @param view     {@link View}
+     * @param tintMode 着色模式 {@link PorterDuff.Mode}
+     */
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public static void setForegroundTintMode(final View view, final PorterDuff.Mode tintMode) {
+        if (view != null) {
+            try {
+                view.setForegroundTintMode(tintMode);
+            } catch (Exception e) {
+                LogPrintUtils.eTag(TAG, e, "setForegroundTintMode");
+            }
+        }
+    }
+
+    // =============
+    // = ImageView =
+    // =============
+
+    /**
+     * 设置 ImageView Bitmap
+     * @param view   {@link View}
+     * @param bitmap {@link Bitmap}
+     */
+    public static void setImageBitmap(final View view, final Bitmap bitmap) {
+        setImageBitmap(getImageView(view), bitmap);
+    }
+
+    /**
+     * 设置 ImageView Bitmap
+     * @param imageView {@link ImageView}
+     * @param bitmap    {@link Bitmap}
+     * @param <T>       泛型
+     * @return {@link ImageView}
+     */
+    public static <T extends ImageView> T setImageBitmap(final T imageView, final Bitmap bitmap) {
+        if (imageView != null) {
+            try {
+                imageView.setImageBitmap(bitmap);
+            } catch (Exception e) {
+                LogPrintUtils.eTag(TAG, e, "setImageBitmap");
+            }
+        }
+        return imageView;
+    }
+
+    // =
+
+    /**
+     * 设置 ImageView Drawable
+     * @param view     {@link View}
+     * @param drawable {@link Bitmap}
+     */
+    public static void setImageDrawable(final View view, final Drawable drawable) {
+        setImageDrawable(getImageView(view), drawable);
+    }
+
+    /**
+     * 设置 ImageView Drawable
+     * @param imageView {@link ImageView}
+     * @param drawable  {@link Drawable}
+     * @param <T>       泛型
+     * @return {@link ImageView}
+     */
+    public static <T extends ImageView> T setImageDrawable(final T imageView, final Drawable drawable) {
+        if (imageView != null) {
+            try {
+                imageView.setImageDrawable(drawable);
+            } catch (Exception e) {
+                LogPrintUtils.eTag(TAG, e, "setImageDrawable");
+            }
+        }
+        return imageView;
+    }
+
+    // =
+
+    /**
+     * 设置 ImageView 资源
+     * @param view  {@link View}
+     * @param resId resource identifier
+     */
+    public static void setImageResource(final View view, @DrawableRes final int resId) {
+        setImageResource(getImageView(view), resId);
+    }
+
+    /**
+     * 设置 ImageView 资源
+     * @param imageView {@link ImageView}
+     * @param resId     resource identifier
+     * @param <T>       泛型
+     * @return {@link ImageView}
+     */
+    public static <T extends ImageView> T setImageResource(final T imageView, @DrawableRes final int resId) {
+        if (imageView != null) {
+            try {
+                imageView.setImageResource(resId);
+            } catch (Exception e) {
+                LogPrintUtils.eTag(TAG, e, "setImageResource");
+            }
+        }
+        return imageView;
+    }
+
+    // =
+
+    /**
+     * 设置 ImageView Matrix
+     * @param view   {@link View}
+     * @param matrix {@link Matrix}
+     */
+    public static void setImageMatrix(final View view, final Matrix matrix) {
+        setImageMatrix(getImageView(view), matrix);
+    }
+
+    /**
+     * 设置 ImageView Matrix
+     * @param imageView {@link ImageView}
+     * @param matrix    {@link Matrix}
+     * @param <T>       泛型
+     * @return {@link ImageView}
+     */
+    public static <T extends ImageView> T setImageMatrix(final T imageView, final Matrix matrix) {
+        if (imageView != null) {
+            try {
+                imageView.setImageMatrix(matrix);
+            } catch (Exception e) {
+                LogPrintUtils.eTag(TAG, e, "setImageMatrix");
+            }
+        }
+        return imageView;
+    }
+
+    // =
+
+    /**
+     * 设置 ImageView 着色
+     * @param view {@link View}
+     * @param tint 着色颜色
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public static void setImageTintList(final View view, final ColorStateList tint) {
+        setImageTintList(getImageView(view), tint);
+    }
+
+    /**
+     * 设置 ImageView 着色
+     * @param imageView {@link ImageView}
+     * @param tint      着色颜色
+     * @param <T>       泛型
+     * @return {@link ImageView}
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public static <T extends ImageView> T setImageTintList(final T imageView, final ColorStateList tint) {
+        if (imageView != null) {
+            try {
+                imageView.setImageTintList(tint);
+            } catch (Exception e) {
+                LogPrintUtils.eTag(TAG, e, "setImageTintList");
+            }
+        }
+        return imageView;
+    }
+
+    // =
+
+    /**
+     * 设置 ImageView 着色模式
+     * @param view     {@link View}
+     * @param tintMode 着色模式 {@link PorterDuff.Mode}
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public static void setImageTintMode(final View view, final PorterDuff.Mode tintMode) {
+        setImageTintMode(getImageView(view), tintMode);
+    }
+
+    /**
+     * 设置 ImageView 着色模式
+     * @param imageView {@link ImageView}
+     * @param tintMode  着色模式 {@link PorterDuff.Mode}
+     * @param <T>       泛型
+     * @return {@link ImageView}
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public static <T extends ImageView> T setImageTintMode(final T imageView, final PorterDuff.Mode tintMode) {
+        if (imageView != null) {
+            try {
+                imageView.setImageTintMode(tintMode);
+            } catch (Exception e) {
+                LogPrintUtils.eTag(TAG, e, "setImageTintMode");
+            }
+        }
+        return imageView;
+    }
+
+    // =
+
+    /**
+     * 设置 ImageView 缩放类型
+     * @param view     {@link View}
+     * @param scaleType 缩放类型 {@link ImageView.ScaleType}
+     */
+    public static void setScaleType(final View view, final ImageView.ScaleType scaleType) {
+        setScaleType(getImageView(view), scaleType);
+    }
+
+    /**
+     * 设置 ImageView 着色模式
+     * @param imageView {@link ImageView}
+     * @param scaleType 缩放类型 {@link ImageView.ScaleType}
+     * @param <T>       泛型
+     * @return {@link ImageView}
+     */
+    public static <T extends ImageView> T setScaleType(final T imageView, final ImageView.ScaleType scaleType) {
+        if (imageView != null) {
+            try {
+                imageView.setScaleType(scaleType);
+            } catch (Exception e) {
+                LogPrintUtils.eTag(TAG, e, "setScaleType");
+            }
+        }
+        return imageView;
+    }
+
+    // ========
+    // = 获取 =
+    // ========
 
     // =
 
