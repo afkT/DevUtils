d8cfeecb3a3113bf409fbe860e97f7b230442163
Ttt <13798405957@163.com>
2019-07-25 02:34:08 +0800

    1.增加 BitmapUtils 图片圆角处理方法(非圆形)
    Former-commit-id: 5ae551ea221105542245b86d02a3c53c43d50b52

--- DevLibUtils/src/main/java/dev/utils/app/image/temp/BitmapUtils.java
+++ DevLibUtils/src/main/java/dev/utils/app/image/temp/BitmapUtils.java
@@ -2,7 +2,13 @@ package dev.utils.app.image.temp;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.media.ExifInterface;
 
 import java.io.ByteArrayOutputStream;
@@ -13,6 +19,10 @@ import dev.utils.LogPrintUtils;
 /**
  * detail: Bitmap 工具类
  * @author Ttt
+ * <pre>
+ *     Android PorterDuffXfermode 的正确使用方式
+ *     @see <a href="http://www.jcodecraeer.com/a/anzhuokaifa/androidkaifa/2017/0731/8318.html"/>
+ * </pre>
  */
 public final class BitmapUtils {
 
@@ -94,7 +104,7 @@ public final class BitmapUtils {
      * @return {@link Bitmap}
      */
     public static Bitmap copy(final Bitmap bitmap, final boolean isMutable) {
-        if (bitmap == null) return null;
+        if (isEmpty(bitmap)) return null;
         return bitmap.copy(bitmap.getConfig(), isMutable);
     }
 
@@ -106,7 +116,7 @@ public final class BitmapUtils {
      * @return Alpha 位图
      */
     public static Bitmap extractAlpha(final Bitmap bitmap) {
-        if (bitmap == null) return null;
+        if (isEmpty(bitmap)) return null;
         return bitmap.extractAlpha();
     }
 
@@ -160,7 +170,7 @@ public final class BitmapUtils {
      * @return 旋转后的图片
      */
     public static Bitmap rotate(final Bitmap bitmap, final float degrees) {
-        if (bitmap == null) return null;
+        if (isEmpty(bitmap)) return null;
         Matrix matrix = new Matrix();
         matrix.postRotate(degrees);
         return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, false);
@@ -175,7 +185,7 @@ public final class BitmapUtils {
      * @return 旋转后的图片
      */
     public static Bitmap rotate(final Bitmap bitmap, final float degrees, final float px, final float py) {
-        if (bitmap == null) return null;
+        if (isEmpty(bitmap)) return null;
         Matrix matrix = new Matrix();
         matrix.postRotate(degrees, px, py);
         return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, false);
@@ -235,7 +245,7 @@ public final class BitmapUtils {
      * @return 翻转后的图片
      */
     public static Bitmap reverse(final Bitmap bitmap, final boolean horizontal) {
-        if (bitmap == null) return null;
+        if (isEmpty(bitmap)) return null;
         Matrix matrix = new Matrix();
         if (horizontal) {
             matrix.preScale(-1, 1);
@@ -256,8 +266,7 @@ public final class BitmapUtils {
      * @return 缩放后的图片
      */
     public static Bitmap zoom(final Bitmap bitmap, final int newSize) {
-        if (isEmpty(bitmap)) return null;
-        return Bitmap.createScaledBitmap(bitmap, newSize, newSize, true);
+        return zoom(bitmap, newSize, newSize);
     }
 
     /**
@@ -330,6 +339,49 @@ public final class BitmapUtils {
         Matrix matrix = new Matrix();
         matrix.setSkew(kx, ky, px, py);
         return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    }
+
+    // ========
+    // = 圆角 =
+    // ========
+
+    /**
+     * 圆角图片(非圆形)
+     * <pre>
+     *     以宽高中最小值设置为圆角尺寸, 如果宽高一致, 则处理为圆形图片
+     * </pre>
+     * @param bitmap 待操作源图片
+     * @return 圆角处理后的图片
+     */
+    public static Bitmap roundCorner(final Bitmap bitmap) {
+        if (isEmpty(bitmap)) return null;
+        return roundCorner(bitmap, Math.min(bitmap.getWidth(), bitmap.getHeight()));
+    }
+
+    /**
+     * 圆角图片(非圆形)
+     * @param bitmap 待操作源图片
+     * @param pixels 圆角大小
+     * @return 圆角处理后的图片
+     */
+    public static Bitmap roundCorner(final Bitmap bitmap, final float pixels) {
+        if (isEmpty(bitmap)) return null;
+        // 创建一个同源图片一样大小的矩形, 用于把源图片绘制到这个矩形上
+        Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        RectF rectF = new RectF(rect); // 创建一个精度更高的矩形, 用于画出圆角效果
+
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(0xff424242); // 设置画笔的颜色为不透明的灰色
+
+        Bitmap newBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(newBitmap);
+        canvas.drawARGB(0, 0, 0, 0);
+        canvas.drawRoundRect(rectF, pixels, pixels, paint);
+        // 绘制底圆后, 进行合并(交集处理)
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return newBitmap;
     }
 
     // ======================
