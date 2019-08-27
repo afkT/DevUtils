361e46667b5d498960439ad126aa66e2dcb7e2f3
Ttt <13798405957@163.com>
2019-08-27 17:22:03 +0800

    1.升级 Gradle 版本 2.增加 DevAssist module
    Former-commit-id: 6c53f723ccb2f1fe263ffbabbb5b755d2a0b8097

---
+++ lib/DevAssist/src/main/java/dev/assist/DevAssist.java
@@ -0,0 +1,44 @@
+package dev.assist;
+
+/**
+ * detail: 开发辅助类
+ * @author Ttt
+ * <pre>
+ *     GitHub
+ *     @see <a href="https://github.com/afkT/DevUtils"/>
+ *     DevApp Api
+ *     @see <a href="https://github.com/afkT/DevUtils/blob/master/lib/DevApp/README.md"/>
+ *     DevJava Api
+ *     @see <a href="https://github.com/afkT/DevUtils/blob/master/lib/DevJava/README.md"/>
+ *     DevAssist Api
+ *     @see <a href="https://github.com/afkT/DevUtils/blob/master/lib/DevAssist/README.md"/>
+ * </pre>
+ */
+public final class DevAssist {
+
+    private DevAssist() {
+    }
+
+    // 日志 TAG
+    private static final String TAG = DevAssist.class.getSimpleName();
+
+    // ==============
+    // = 工具类版本 =
+    // ==============
+
+    /**
+     * 获取 DevAssist 工具类版本
+     * @return DevAssist versionName
+     */
+    public static String getDevAssistUtilsVersion() {
+        return BuildConfig.VERSION_NAME;
+    }
+
+    /**
+     * 获取 DevAssist 工具类版本号
+     * @return DevAssist versionCode
+     */
+    public static int getDevAssistUtilsVersionCode() {
+        return BuildConfig.VERSION_CODE;
+    }
+}
