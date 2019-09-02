63920803fb7905f5ce0ee54e458040af5699947b
Ttt <13798405957@163.com>
2019-09-03 02:40:27 +0800

    1.添加 DevAssist 部分 Adapter 统一操作接口方法
    Former-commit-id: 51850cd17026cb952371cdfdbda1976dc72044c8

---
+++ lib/DevAssist/src/main/java/dev/assist/adapter/IAdapterNotify.java
@@ -0,0 +1,13 @@
+package dev.assist.adapter;
+
+/**
+ * detail: Adapter 通知接口
+ * @author Ttt
+ */
+public interface IAdapterNotify {
+
+    /**
+     * Adapter 通知数据改变
+     */
+    void adapterNotifyDataSetChanged();
+}
