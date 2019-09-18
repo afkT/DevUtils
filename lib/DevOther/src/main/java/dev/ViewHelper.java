3a4d5671266473ef4c34a3fa262d6aa85237d680
Ttt <13798405957@163.com>
2019-09-18 16:25:32 +0800

    1.增加 DevOther 模块 ViewHelper 类, 方便链式调用设置 View
    Former-commit-id: 20584bc5a4b72028dd97b99c974075f7e7b3d49e

---
+++ lib/DevOther/src/main/java/dev/ViewHelper.java
@@ -0,0 +1,25 @@
+package dev;
+
+/**
+ * detail: View 链式调用快捷设置 Helper 类
+ * @author Ttt
+ */
+public final class ViewHelper {
+
+    // ViewHelper
+    private static final ViewHelper HELPER = new ViewHelper();
+
+    /**
+     * 获取单例 ViewHelper
+     * @return {@link ViewHelper}
+     */
+    public static ViewHelper get() {
+        return HELPER;
+    }
+
+    // ========
+    // = Text =
+    // ========
+
+
+}
