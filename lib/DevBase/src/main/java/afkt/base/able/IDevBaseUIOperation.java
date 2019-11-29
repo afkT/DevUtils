fc590fbab530b2090edbf7d2d5937d9abab3a3e0
Ttt <13798405957@163.com>
2019-11-29 11:22:06 +0800

    1.移动 DevProject module DevBase 到 DevUtils 项目中
    Former-commit-id: 021da5a487bb1bd5a94f1d5a963392fe0809e141

---
+++ lib/DevBase/src/main/java/afkt/base/able/IDevBaseUIOperation.java
@@ -0,0 +1,105 @@
+package afkt.base.able;
+
+import android.app.Dialog;
+import android.support.v4.app.DialogFragment;
+import android.widget.PopupWindow;
+
+/**
+ * detail: 基类 UI 交互相关操作方法
+ * @author Ttt
+ */
+public interface IDevBaseUIOperation {
+
+    // =========
+    // = Toast =
+    // =========
+
+    /**
+     * 显示 Toast
+     * @param text Toast 提示文本
+     * @param objs 格式化参数
+     */
+    void showToast(String text, Object... objs);
+
+    /**
+     * 显示 Toast
+     * @param resId R.string.id
+     * @param objs  格式化参数
+     */
+    void showToast(int resId, Object... objs);
+
+    // ===============
+    // = PopupWindow =
+    // ===============
+
+    /**
+     * 获取 PopupWindow
+     * @return {@link PopupWindow}
+     */
+    PopupWindow getPopupWindow();
+
+    /**
+     * 设置 PopupWindow
+     * @param popupWindow {@link PopupWindow}
+     * @return {@link PopupWindow}
+     */
+    <T extends PopupWindow> T setPopupWindow(T popupWindow);
+
+    /**
+     * 设置 PopupWindow
+     * @param isClose     是否关闭之前的 PopupWindow
+     * @param popupWindow {@link PopupWindow}
+     * @return {@link PopupWindow}
+     */
+    <T extends PopupWindow> T setPopupWindow(boolean isClose, T popupWindow);
+
+    // ==========
+    // = Dialog =
+    // ==========
+
+    /**
+     * 获取 Dialog
+     * @return {@link Dialog}
+     */
+    Dialog getDialog();
+
+    /**
+     * 设置 Dialog
+     * @param dialog {@link Dialog}
+     * @return {@link Dialog}
+     */
+    <T extends Dialog> T setDialog(T dialog);
+
+    /**
+     * 设置 Dialog
+     * @param isClose 是否关闭之前的 Dialog
+     * @param dialog  {@link Dialog}
+     * @return {@link Dialog}
+     */
+    <T extends Dialog> T setDialog(boolean isClose, T dialog);
+
+    // ==================
+    // = DialogFragment =
+    // ==================
+
+    /**
+     * 获取 DialogFragment
+     * @return {@link DialogFragment}
+     */
+    DialogFragment getDialogFragment();
+
+    /**
+     * 设置 DialogFragment
+     * @param dialog {@link DialogFragment}
+     * @return {@link DialogFragment}
+     */
+    <T extends DialogFragment> T setDialogFragment(T dialog);
+
+    /**
+     * 设置 DialogFragment
+     * @param isClose 是否关闭之前的 DialogFragment
+     * @param dialog  {@link DialogFragment}
+     * @return {@link DialogFragment}
+     */
+    <T extends DialogFragment> T setDialogFragment(boolean isClose, T dialog);
+}
---
+++ lib/DevBase/src/main/java/afkt/base/able/IDevBaseViewOperation.java
@@ -0,0 +1,32 @@
+package afkt.base.able;
+
+import android.view.View;
+
+/**
+ * detail: 基类 View 相关方法
+ * @author Ttt
+ */
+public interface IDevBaseViewOperation {
+
+    /**
+     * 获取 Content View
+     * @return Content View
+     */
+    View getContentView();
+
+    // ================
+    // = Content View =
+    // ================
+
+    /**
+     * 获取 Content Layout id
+     * @return Content Layout Id
+     */
+    int contentId();
+
+    /**
+     * 获取 Content Layout View
+     * @return Content Layout View
+     */
+    View contentView();
+}
