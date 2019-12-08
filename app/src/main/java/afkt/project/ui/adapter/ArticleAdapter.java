981d55bf2db5b113377ecbe8413a7ace821123d1
Ttt <13798405957@163.com>
2019-12-08 23:42:12 +0800

    1.增加文章 Adapter 显示处理
    Former-commit-id: b11a44b5e98e7f50675579e6cf68477e5a2cb383

--- app/src/main/java/afkt/project/ui/adapter/ArticleAdapter.java
+++ app/src/main/java/afkt/project/ui/adapter/ArticleAdapter.java
@@ -1,5 +1,10 @@
 package afkt.project.ui.adapter;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.text.TextUtils;
+import android.view.View;
+
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -10,6 +15,8 @@ import java.util.ArrayList;
 import afkt.project.R;
 import afkt.project.model.bean.ArticleBean;
 import dev.other.GlideUtils;
+import dev.utils.app.AppUtils;
+import dev.utils.app.ListenerUtils;
 import dev.utils.app.ResourceUtils;
 import dev.utils.common.DateUtils;
 import dev.utils.common.DevCommonUtils;
@@ -38,8 +45,20 @@ public class ArticleAdapter extends BaseQuickAdapter<ArticleBean.DataBean.DatasB
         // 标题
         helper.setText(R.id.vid_aa_title_tv, item.title);
         // 时间
-        helper.setText(R.id.vid_aa_time_tv, DevCommonUtils.toCheckValue(item.niceDate, item.niceShareDate));
+        helper.setText(R.id.vid_aa_time_tv, DevCommonUtils.toCheckValue(item.niceShareDate, item.niceDate));
         // 随机图片
         GlideUtils.with().displayImage("https://picsum.photos/2" + DateUtils.convertTime(position), helper.getView(R.id.vid_aa_pic_igview), requestOptions);
+        // 绑定点击事件
+        ListenerUtils.setOnClicks(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String link = item.link;
+                if (!TextUtils.isEmpty(link)) {
+                    Uri uri = Uri.parse(link);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    AppUtils.startActivity(intent);
+                }
+            }
+        }, helper.getView(R.id.vid_aa_cardview));
     }
 }
