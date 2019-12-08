5deff0bf7a258a04677d5388c853b1b3abfae479
Ttt <13798405957@163.com>
2019-12-08 23:32:45 +0800

    1.增加文章适配器展示处理
    Former-commit-id: a1dd21267601469c898eaac213e9fe0036bba190

--- app/src/main/java/afkt/project/ui/adapter/ArticleAdapter.java
+++ app/src/main/java/afkt/project/ui/adapter/ArticleAdapter.java
@@ -11,7 +11,8 @@ import afkt.project.R;
 import afkt.project.model.bean.ArticleBean;
 import dev.other.GlideUtils;
 import dev.utils.app.ResourceUtils;
-import dev.utils.app.ViewUtils;
+import dev.utils.common.DateUtils;
+import dev.utils.common.DevCommonUtils;
 
 /**
  * detail: 文章 Adapter
@@ -23,7 +24,7 @@ public class ArticleAdapter extends BaseQuickAdapter<ArticleBean.DataBean.DatasB
     private RequestOptions requestOptions;
 
     public ArticleAdapter() {
-        super(R.layout.adapter_item_edits, new ArrayList<>());
+        super(R.layout.adapter_article, new ArrayList<>());
         // 获取默认 RequestOptions
         requestOptions = GlideUtils.defaultOptions();
         // 设置圆角, 使用 RoundedCorners 图片不会闪烁
@@ -34,7 +35,11 @@ public class ArticleAdapter extends BaseQuickAdapter<ArticleBean.DataBean.DatasB
     protected void convert(BaseViewHolder helper, ArticleBean.DataBean.DatasBean item) {
         // 当前索引
         int position = helper.getLayoutPosition();
-        // 判断是否显示边距
-        ViewUtils.setVisibility(position == 0, helper.getView(R.id.vid_aie_line));
+        // 标题
+        helper.setText(R.id.vid_aa_title_tv, item.title);
+        // 时间
+        helper.setText(R.id.vid_aa_time_tv, DevCommonUtils.toCheckValue(item.niceDate, item.niceShareDate));
+        // 随机图片
+        GlideUtils.with().displayImage("https://picsum.photos/2" + DateUtils.convertTime(position), helper.getView(R.id.vid_aa_pic_igview), requestOptions);
     }
 }
