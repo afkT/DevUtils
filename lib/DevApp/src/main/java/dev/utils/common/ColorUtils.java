9d4e74a910a1af469b9d55a0a895654a764aa4dd
Ttt <13798405957@163.com>
2019-12-16 15:22:01 +0800

    1.增加 ColorUtils 颜色信息实体类(内部类)，以及 Color 解析器
    Former-commit-id: c3808a6e95707e6b61c82f57c8bd5d21f12e1e26

--- lib/DevApp/src/main/java/dev/utils/common/ColorUtils.java
+++ lib/DevApp/src/main/java/dev/utils/common/ColorUtils.java
@@ -89,6 +89,11 @@ public final class ColorUtils {
      * 0D、26、40、59、73、8C、A6、BF、D9、F2
      */
 
+    static {
+        // 设置 Color 解析器
+        setParser(new ColorInfo.ColorParser());
+    }
+
     /**
      * 获取十六进制透明度字符串
      * @param alpha 0-255
@@ -691,5 +696,217 @@ public final class ColorUtils {
         sColorNameMaps.put("navy", 0xFF000080);
         sColorNameMaps.put("olive", 0xFF808000);
         sColorNameMaps.put("teal", 0xFF008080);
+    }
+
+    // ============
+    // = 颜色信息 =
+    // ============
+
+    /**
+     * 设置 Color 解析器
+     * @param parser {@link ColorInfo.Parser}
+     */
+    public static void setParser(final ColorInfo.Parser parser) {
+        ColorInfo.sParser = parser;
+    }
+
+    /**
+     * detail: 颜色信息
+     * @author Ttt
+     */
+    public static class ColorInfo {
+
+        // key
+        private String key;
+        // value ( 如: #000000 )
+        private String value;
+        // value 解析后的值 ( 如: #000 => #000000 )
+        private String valueParser;
+        // ARGB/RGB color
+        private long valueColor;
+        // A、R、G、B
+        private int alpha = 255, red = 0, green = 0, blue = 0;
+
+        /**
+         * 构造函数
+         * @param key   Key
+         * @param value Value ( 如: #000000 )
+         */
+        public ColorInfo(final String key, final String value) {
+            this.key = key;
+            this.value = value;
+            priConvert();
+        }
+
+        /**
+         * 构造函数
+         * @param key        Key
+         * @param valueColor ARGB/RGB color
+         */
+        public ColorInfo(final String key, final int valueColor) {
+            this(key, ColorUtils.intToArgbString(valueColor));
+        }
+
+        /**
+         * 获取 Key
+         * @return key String
+         */
+        public String getKey() {
+            return key;
+        }
+
+        /**
+         * 获取 Value
+         * @return value String
+         */
+        public String getValue() {
+            return value;
+        }
+
+        /**
+         * 获取 Value 解析后的值 ( 如: #000 => #000000 )
+         * @return value 解析后的值 ( 如: #000 => #000000 )
+         */
+        public String getValueParser() {
+            return valueParser;
+        }
+
+        /**
+         * 获取 ARGB/RGB color
+         * @return ARGB/RGB color
+         */
+        public long getValueColor() {
+            return valueColor;
+        }
+
+        /**
+         * 返回颜色中的透明度值 ( 返回十进制 )
+         * @return alpha 值
+         */
+        public int getAlpha() {
+            return alpha;
+        }
+
+        /**
+         * 返回颜色中红色的色值 ( 返回十进制 )
+         * @return red 值
+         */
+        public int getRed() {
+            return red;
+        }
+
+        /**
+         * 返回颜色中绿色的色值 ( 返回十进制 )
+         * @return green 值
+         */
+        public int getGreen() {
+            return green;
+        }
+
+        /**
+         * 返回颜色中蓝色的色值 ( 返回十进制 )
+         * @return blue 值
+         */
+        public int getBlue() {
+            return blue;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            builder.append("key : " + key);
+            builder.append("\nvalue : " + value);
+            builder.append("\nvalueParser : " + valueParser);
+            builder.append("\nalpha : " + alpha);
+            builder.append("\nred : " + red);
+            builder.append("\ngreen : " + green);
+            builder.append("\nblue : " + blue);
+            builder.append("\nintToRgbString : " + ColorUtils.intToRgbString((int) valueColor));
+            builder.append("\nintToArgbString : " + ColorUtils.intToArgbString((int) valueColor));
+            return builder.toString();
+        }
+
+        // ============
+        // = 内部处理 =
+        // ============
+
+        /**
+         * 内部转换处理
+         */
+        private void priConvert() {
+            String temp = value;
+            if (sParser != null) {
+                temp = sParser.handleColor(value);
+                // 保存解析后的值
+                valueParser = temp;
+            }
+            if (temp == null) return;
+            // 转换 long 颜色值
+            valueColor = ColorUtils.parseColor(temp);
+            // 解析失败则不处理
+            if (valueColor == -1) return;
+
+            int[] argb = ColorUtils.getARGB((int) valueColor);
+            // 获取 ARGB
+            alpha = argb[0];
+            red = argb[1];
+            green = argb[2];
+            blue = argb[3];
+        }
+
+        // ==============
+        // = 解析器相关 =
+        // ==============
+
+        /**
+         * detail: Color 解析器
+         * @author Ttt
+         */
+        public interface Parser {
+
+            /**
+             * 处理 color
+             * @param value 如: #000000
+             * @return 处理后的 value
+             */
+            String handleColor(String value);
+        }
+
+        /**
+         * detail: Color 解析器
+         * @author Ttt
+         */
+        public static class ColorParser implements Parser {
+            @Override
+            public String handleColor(String value) {
+                if (value == null) return null;
+                String color = StringUtils.toClearSpace(value);
+                char[] chars = color.toCharArray();
+                int length = chars.length;
+                if (length != 0 && chars[0] == '#') {
+                    if (length == 4) {
+                        String colorsub = color.substring(1);
+                        // #000 => #000000
+                        return color + colorsub;
+                    } else if (length == 5) {
+                        String colorsub = color.substring(3);
+                        // #0011 => #00111111
+                        return color + colorsub + colorsub;
+                    }
+                }
+                return color;
+            }
+        }
+
+        // 内部解析器
+        private static Parser sParser;
+
+        /**
+         * 设置 Color 解析器
+         * @param parser {@link Parser}
+         */
+        public static void setParser(final Parser parser) {
+            ColorInfo.sParser = parser;
+        }
     }
 }
