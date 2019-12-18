283b27f1ae8efbd90ee1d571546b3a9b7cb9dd40
Ttt <13798405957@163.com>
2019-12-18 22:17:31 +0800

    1.更新 ContentResolverUtils#getMediaUri 实现方式代码，方便复用
    Former-commit-id: 4c999e28d1f0b01c510d30502fa9dde8e93f85c9

--- lib/DevApp/src/main/java/dev/utils/app/ContentResolverUtils.java
+++ lib/DevApp/src/main/java/dev/utils/app/ContentResolverUtils.java
@@ -12,7 +12,9 @@ import android.text.TextUtils;
 import java.io.File;
 
 import dev.utils.LogPrintUtils;
+import dev.utils.common.ArrayUtils;
 import dev.utils.common.CloseUtils;
+import dev.utils.common.ConvertUtils;
 import dev.utils.common.FileUtils;
 
 /**
@@ -63,7 +65,7 @@ public final class ContentResolverUtils {
             try {
                 // 添加到相册
                 MediaStore.Images.Media.insertImage(ResourceUtils.getContentResolver(),
-                    file.getAbsolutePath(), TextUtils.isEmpty(fileName) ? file.getName() : fileName, null);
+                        file.getAbsolutePath(), TextUtils.isEmpty(fileName) ? file.getName() : fileName, null);
                 // 通知图库扫描更新
                 if (isNotify) notifyMediaStore(file);
                 return true;
@@ -150,7 +152,7 @@ public final class ContentResolverUtils {
      * @param projection    查询的字段
      * @param selection     查询的条件
      * @param selectionArgs 查询条件的参数
-     * @param sortOrder     排序
+     * @param sortOrder     排序方式
      * @return {@link Cursor}
      */
     public static Cursor query(final Uri uri, final String[] projection, final String selection,
@@ -202,37 +204,174 @@ public final class ContentResolverUtils {
      * @return 指定文件 {@link Uri}
      */
     public static Uri getMediaUri(final Uri uri, final String filePath) {
-        if (uri == null || TextUtils.isEmpty(filePath)) return null;
-        String[] projection; // 查询的字段
-        String selection = MediaStore.Files.FileColumns.DATA + "=?"; // 查询的条件
-        String[] selectionArgs = new String[]{filePath}; // 查询条件的参数
-        // 版本适配
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            projection = new String[]{
-                MediaStore.Files.FileColumns._ID,
-                MediaStore.Files.FileColumns.VOLUME_NAME
-            };
-        } else {
-            projection = new String[]{
-                MediaStore.Files.FileColumns._ID,
-            };
-        }
-        Cursor cursor = query(uri, projection, selection, selectionArgs, null);
-        try {
-            if (cursor != null && cursor.moveToFirst()) {
-                long rowId = cursor.getLong(cursor.getColumnIndex(MediaStore.Files.FileColumns._ID));
-                String volumeName = VOLUME_EXTERNAL;
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                    volumeName = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.VOLUME_NAME));
-                }
+        String[] result = mediaQuery(uri, filePath, MEDIA_QUERY_URI);
+        if (ArrayUtils.isLength(result, 2)) {
+            try {
                 // 返回外部存储 ( 公开目录 ) SDCard 文件地址获取对应的 Uri content://
-                return MediaStore.Files.getContentUri(volumeName, rowId);
+                return MediaStore.Files.getContentUri(result[1], ConvertUtils.toLong(result[0]));
+            } catch (Exception e) {
+                LogPrintUtils.eTag(TAG, e, "getMediaUri - " + filePath);
+            }
+        }
+        return null;
+    }
+
+    // =
+
+    /**
+     * 通过 File 获取 Media 信息
+     * @param file 文件
+     * @return Media 信息
+     */
+    public static String[] mediaQuery(final File file, final MediaQuery mediaQuery) {
+        return mediaQuery(FILES_URI, file, mediaQuery);
+    }
+
+    /**
+     * 通过 File 获取 Media 信息
+     * @param uri  MediaStore.media-type.Media.EXTERNAL_CONTENT_URI
+     * @param file 文件
+     * @return Media 信息
+     */
+    public static String[] mediaQuery(final Uri uri, final File file, final MediaQuery mediaQuery) {
+        return mediaQuery(uri, FileUtils.getAbsolutePath(file), mediaQuery);
+    }
+
+    /**
+     * 通过 File Path 获取 Media Uri
+     * @param filePath 文件路径
+     * @return Media 信息
+     */
+    public static String[] mediaQuery(final String filePath, final MediaQuery mediaQuery) {
+        return mediaQuery(FILES_URI, filePath, mediaQuery);
+    }
+
+    /**
+     * 通过 File Path 获取 Media Uri
+     * <pre>
+     *     只能用于查询 Media ( SDK_INT >= Q 使用, SDK_INT < Q 则直接使用 {@link Uri#fromFile(File)})
+     *     通过外部存储 ( 公开目录 ) SDCard 文件地址获取对应的 Uri content://
+     * </pre>
+     * @param uri        MediaStore.media-type.Media.EXTERNAL_CONTENT_URI
+     * @param filePath   文件路径
+     * @param mediaQuery 多媒体查询抽象类
+     * @return Media 信息
+     */
+    public static String[] mediaQuery(final Uri uri, final String filePath, final MediaQuery mediaQuery) {
+        if (uri == null || TextUtils.isEmpty(filePath) && mediaQuery != null) return null;
+        Cursor cursor = null;
+        try {
+            cursor = query(uri,
+                    mediaQuery.getProjection(filePath),
+                    mediaQuery.getSelection(filePath),
+                    mediaQuery.getSelectionArgs(filePath),
+                    mediaQuery.getSortOrder(filePath)
+            );
+            if (cursor != null && cursor.moveToFirst()) {
+                return mediaQuery.getResult(filePath, cursor);
             }
         } catch (Exception e) {
-            LogPrintUtils.eTag(TAG, e, "getMediaUri - " + filePath);
+            LogPrintUtils.eTag(TAG, e, "mediaQuery - " + filePath);
         } finally {
             CloseUtils.closeIOQuietly(cursor);
         }
         return null;
+    }
+
+    // ===============
+    // = Media Query =
+    // ===============
+
+    // 多媒体查询获取 Uri 处理
+    private static final MediaQueryUri MEDIA_QUERY_URI = new MediaQueryUri();
+
+    /**
+     * detail: 多媒体查询抽象类
+     * @author Ttt
+     */
+    public static abstract class MediaQuery {
+
+        /**
+         * 获取查询结果
+         * @param filePath 文件路径
+         * @param cursor   Cursor
+         * @return 查询结果
+         */
+        public abstract String[] getResult(String filePath, Cursor cursor);
+
+        // ============
+        // = 查询条件 =
+        // ============
+
+        /**
+         * 获取查询的字段
+         * @param filePath 文件路径
+         * @return 查询的字段
+         */
+        public abstract String[] getProjection(String filePath);
+
+        /**
+         * 获取查询的条件
+         * @param filePath 文件路径
+         * @return 查询的条件
+         */
+        public abstract String getSelection(String filePath);
+
+        /**
+         * 获取查询条件的参数
+         * @param filePath 文件路径
+         * @return 查询条件的参数
+         */
+        public abstract String[] getSelectionArgs(String filePath);
+
+        /**
+         * 获取排序方式
+         * @param filePath 文件路径
+         * @return 排序方式
+         */
+        public String getSortOrder(String filePath) {
+            return null;
+        }
+    }
+
+    /**
+     * detail: 多媒体查询获取 Uri 处理
+     * @author Ttt
+     */
+    private static class MediaQueryUri extends MediaQuery {
+        @Override
+        public String[] getResult(String filePath, Cursor cursor) {
+            String[] result = new String[2];
+            long rowId = cursor.getLong(cursor.getColumnIndex(MediaStore.Files.FileColumns._ID));
+            String volumeName = VOLUME_EXTERNAL;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                volumeName = cursor.getString(cursor.getColumnIndex(MediaStore.Files.FileColumns.VOLUME_NAME));
+            }
+            result[0] = String.valueOf(rowId);
+            result[1] = volumeName;
+            return result;
+        }
+
+        @Override
+        public String[] getProjection(String filePath) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                return new String[]{
+                        MediaStore.Files.FileColumns._ID,
+                        MediaStore.Files.FileColumns.VOLUME_NAME
+                };
+            } else {
+                return new String[]{MediaStore.Files.FileColumns._ID};
+            }
+        }
+
+        @Override
+        public String getSelection(String filePath) {
+            return MediaStore.Files.FileColumns.DATA + "=?";
+        }
+
+        @Override
+        public String[] getSelectionArgs(String filePath) {
+            return new String[]{filePath};
+        }
     }
 }
