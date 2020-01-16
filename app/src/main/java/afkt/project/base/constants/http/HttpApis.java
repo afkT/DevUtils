0484af9fc61a8457c32823e4b63a2cb63c41bbb6
Ttt <13798405957@163.com>
2020-01-16 16:49:27 +0800

    1.增加注解使用，以及 APT 生成代码
    Former-commit-id: b7cf16edf1010cb98c8ed23d3bfa768d3cb0d7a1

--- app/src/main/java/afkt/project/base/constants/http/HttpApis.java
+++ app/src/main/java/afkt/project/base/constants/http/HttpApis.java
@@ -1,5 +1,8 @@
 package afkt.project.base.constants.http;
 
+import dev.environment.annotation.Environment;
+import dev.environment.annotation.Module;
+
 /**
  * detail: Http 请求接口地址
  * @author Ttt
@@ -12,4 +15,14 @@ public final class HttpApis {
     public static final String BASE_URL = "https://www.wanandroid.com/";
     // 文章列表
     public static final String ARTICLE_LIST = BASE_URL + "article/list/%s/json";
+
+    @Module(alias = "服务器接口")
+    private class BaseService {
+
+        @Environment(value = "https://www.wanandroid.com/article", isRelease = true, alias = "文章模块")
+        private String Article;
+
+        @Environment(value = "https://www.baidu.com", alias = "临时测试")
+        private String Temp;
+    }
 }
