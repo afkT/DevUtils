402276c7b2b318a7f0b1dd765879c8f510ca9ff9
Ttt <13798405957@163.com>
2020-01-28 00:20:02 +0800

    1.提交 DevEnvironment Module
    Former-commit-id: c633272639895419cf637318fe994862e21ff0eb

---
+++ lib/Environment/DevEnvironment/src/main/java/dev/environment/DevEnvironmentActivity.java
@@ -0,0 +1,10 @@
+package dev.environment;
+
+import android.app.Activity;
+
+/**
+ * detail: DevEnvironment 切换 Activity
+ * @author Ttt
+ */
+public class DevEnvironmentActivity extends Activity {
+}
