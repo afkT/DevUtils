667c126ebb4d453a34dc9d15c5193b8952a6ec11
Ttt <13798405957@163.com>
2020-05-08 15:42:30 +0800

    1.新增实现角标预览代码
    Former-commit-id: b80c1e4aa1768afd86169d80ecc6463fe411da61
    Former-commit-id: c997cc26229c482f5054564b30ee8abdf75c9ff9

--- app/src/main/java/afkt/project/ui/activity/CornerLabelActivity.java
+++ app/src/main/java/afkt/project/ui/activity/CornerLabelActivity.java
@@ -1,12 +1,16 @@
 package afkt.project.ui.activity;
 
+import android.view.Gravity;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import afkt.project.R;
 import afkt.project.base.app.BaseToolbarActivity;
 import butterknife.BindView;
 import butterknife.OnClick;
-import dev.widget.ui.FlowLikeView;
+import dev.utils.app.SizeUtils;
+import dev.utils.common.RandomUtils;
+import dev.widget.ui.CornerLabelView;
 
 /**
  * detail: 自定义角标 View
@@ -14,53 +18,112 @@ import dev.widget.ui.FlowLikeView;
  */
 public class CornerLabelActivity extends BaseToolbarActivity {
 
-    @BindView(R.id.vid_afl_flowlike)
-    FlowLikeView vid_afl_flowlike;
+    @BindView(R.id.vid_acl_labelview)
+    CornerLabelView labelView;
 
     @Override
     public int getLayoutId() {
         return R.layout.activity_corner_label;
     }
 
-    @Override
-    public void initValues() {
-        super.initValues();
-
-
-//        app:dev_animDuration="2000"
-//        app:dev_iconHeight="30.0dp"
-//        app:dev_iconWidth="30.0dp"
-
-//        // 设置动画时间
-//        vid_afl_flowlike.setAnimDuration(2000);
-//        // 设置图标宽度
-//        vid_afl_flowlike.setIconWidth(SizeUtils.dipConvertPx(30));
-//        // 设置图标高度
-//        vid_afl_flowlike.setIconHeight(SizeUtils.dipConvertPx(30));
-//
-//        // 设置漂浮图标
-//        List<Drawable> lists = new ArrayList<>();
-//        lists.add(ResourceUtils.getDrawable(R.drawable.icon_live_brow_1));
-//        lists.add(ResourceUtils.getDrawable(R.drawable.icon_live_brow_2));
-//        lists.add(ResourceUtils.getDrawable(R.drawable.icon_live_brow_3));
-//        lists.add(ResourceUtils.getDrawable(R.drawable.icon_live_brow_4));
-//        lists.add(ResourceUtils.getDrawable(R.drawable.icon_live_brow_5));
-//        vid_afl_flowlike.setDrawables(lists);
-
-        // 设置漂浮图标
-        vid_afl_flowlike.setDrawablesById(R.drawable.icon_live_brow_1, R.drawable.icon_live_brow_2,
-            R.drawable.icon_live_brow_3, R.drawable.icon_live_brow_4, R.drawable.icon_live_brow_5);
-    }
-
-    @OnClick({R.id.vid_afl_flowlike})
+    @OnClick({R.id.btn_color, R.id.btn_left, R.id.btn_top, R.id.btn_triangle,
+            R.id.btn_text1_minus, R.id.btn_text1_plus, R.id.btn_height1_minus, R.id.btn_height1_plus,
+            R.id.btn_text2_minus, R.id.btn_text2_plus, R.id.btn_height2_minus, R.id.btn_height2_plus})
     @Override
     public void onClick(View v) {
         super.onClick(v);
         switch (v.getId()) {
-            case R.id.vid_afl_flowlike:
-                vid_afl_flowlike.like();
-                vid_afl_flowlike.like(); // 演示效果
+            case R.id.btn_color:
+                labelView.setFillColor(0xff000000 | RandomUtils.getRandom(0, 0xffffff));
+                break;
+            case R.id.btn_left:
+                if (mIsLeft) {
+                    labelView.right();
+                } else {
+                    labelView.left();
+                }
+                mIsLeft = !mIsLeft;
+                FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) labelView.getLayoutParams();
+                lp.gravity = (mIsLeft ? Gravity.LEFT : Gravity.RIGHT) | (mIsTop ? Gravity.TOP : Gravity.BOTTOM);
+                labelView.setLayoutParams(lp);
+                break;
+            case R.id.btn_top:
+                if (mIsTop) {
+                    labelView.bottom();
+                } else {
+                    labelView.top();
+                }
+                mIsTop = !mIsTop;
+                FrameLayout.LayoutParams lp2 = (FrameLayout.LayoutParams) labelView.getLayoutParams();
+                lp2.gravity = (mIsLeft ? Gravity.LEFT : Gravity.RIGHT) | (mIsTop ? Gravity.TOP : Gravity.BOTTOM);
+                labelView.setLayoutParams(lp2);
+                break;
+            case R.id.btn_triangle:
+                mIsTriangle = !mIsTriangle;
+                labelView.triangle(mIsTriangle);
+                break;
+            case R.id.btn_text1_minus:
+                mText1Index = (mText1Index - 1 + TEXTS.length) % TEXTS.length;
+                labelView.setText1(TEXTS[mText1Index]);
+                break;
+            case R.id.btn_text1_plus:
+                mText1Index = (mText1Index + 1) % TEXTS.length;
+                labelView.setText1(TEXTS[mText1Index]);
+                break;
+            case R.id.btn_height1_minus:
+                if (mText1Height < 8) break;
+                mText1Height -= 2;
+                convertPx = SizeUtils.spConvertPx(mText1Height);
+                labelView.setTextHeight1(convertPx);
+                labelView.setPaddingTop(convertPx);
+                labelView.setPaddingCenter(convertPx / 3);
+                labelView.setPaddingBottom(convertPx / 3);
+                break;
+            case R.id.btn_height1_plus:
+                if (mText1Height > 30) break;
+                mText1Height += 2;
+                convertPx = SizeUtils.spConvertPx(mText1Height);
+                labelView.setTextHeight1(convertPx);
+                labelView.setPaddingTop(convertPx);
+                labelView.setPaddingCenter(convertPx / 3);
+                labelView.setPaddingBottom(convertPx / 3);
+                break;
+            case R.id.btn_text2_minus:
+                mText2Index = (mText2Index + 5 - 1) % 5;
+                labelView.setText2("1234567890".substring(0, mText2Index));
+                break;
+            case R.id.btn_text2_plus:
+                mText2Index = (mText2Index + 5 + 1) % 5;
+                labelView.setText2("1234567890".substring(0, mText2Index));
+                break;
+            case R.id.btn_height2_minus:
+                if (mText2Height < 4) break;
+                mText2Height -= 2;
+                convertPx = SizeUtils.spConvertPx(mText2Height);
+                labelView.setTextHeight2(convertPx);
+                break;
+            case R.id.btn_height2_plus:
+                if (mText2Height > 20) break;
+                mText2Height += 2;
+                convertPx = SizeUtils.spConvertPx(mText2Height);
+                labelView.setTextHeight2(convertPx);
                 break;
         }
     }
+
+    // =
+
+    static final String[] TEXTS = {"滿減", "赠品", "满送", "包邮", "拼图", "新人", "砍价", "预售", "众筹"};
+
+    float   convertPx;
+
+    int     mText1Index  = 3;
+    float   mText1Height = 12;
+
+    int     mText2Index  = 3;
+    float   mText2Height = 8;
+
+    boolean mIsLeft      = true;
+    boolean mIsTop       = true;
+    boolean mIsTriangle  = true;
 }
