587d4e6ca83385f97e32dfcd7afe0a5eefb0b91b
Ttt <13798405957@163.com>
2020-06-15 12:06:10 +0800

    1.新增部分 GreenDao 实体类
    Former-commit-id: 1f6eeb59ef07d2e3eca6a8c8e82ef458254f971b
    Former-commit-id: 786ba75605a2642904122587c05dd510e4c5f0da

---
+++ app/src/main/java/afkt/project/db/NotePicture.java
@@ -0,0 +1,8 @@
+package afkt.project.db;
+
+/**
+ * detail: 便签图片资源
+ * @author Ttt
+ */
+public class NotePicture {
+}
---
+++ app/src/main/java/afkt/project/db/NoteType.java
@@ -0,0 +1,9 @@
+package afkt.project.db;
+
+/**
+ * detail: 便签类型
+ * @author Ttt
+ */
+public enum NoteType {
+    TEXT, PICTURE, ALL
+}
