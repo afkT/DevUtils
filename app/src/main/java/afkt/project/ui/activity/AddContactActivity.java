9e4786c29842a2375a384e8fe2cc700dc527055a
Ttt <13798405957@163.com>
2020-08-02 04:54:08 +0800

    1.完成添加联系人细节逻辑处理
    Former-commit-id: 95a9ceedce4ec82297d47b0914d8a7ed49165e77
    Former-commit-id: 8db3dd4c08bb792c903a79436e7a2e0b6e8ba417

--- app/src/main/java/afkt/project/ui/activity/AddContactActivity.java
+++ app/src/main/java/afkt/project/ui/activity/AddContactActivity.java
@@ -1,5 +1,6 @@
 package afkt.project.ui.activity;
 
+import android.Manifest;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.DialogInterface;
@@ -11,6 +12,7 @@ import android.provider.ContactsContract.Data;
 import android.provider.ContactsContract.RawContacts;
 import android.view.View;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import afkt.project.R;
@@ -19,12 +21,18 @@ import afkt.project.ui.widget.BaseEditText;
 import afkt.project.ui.widget.BaseTextView;
 import butterknife.BindView;
 import butterknife.OnClick;
+import dev.utils.app.ActivityUtils;
 import dev.utils.app.DialogUtils;
 import dev.utils.app.EditTextUtils;
 import dev.utils.app.KeyBoardUtils;
+import dev.utils.app.TextViewUtils;
+import dev.utils.app.ViewUtils;
+import dev.utils.app.permission.PermissionUtils;
 import dev.utils.app.toast.ToastTintUtils;
+import dev.utils.app.toast.ToastUtils;
 import dev.utils.common.ConvertUtils;
 import dev.utils.common.StringUtils;
+import dev.utils.common.thread.DevThreadManager;
 import dev.utils.common.validator.ValiToPhoneUtils;
 import dev.utils.common.validator.ValidatorUtils;
 
@@ -49,7 +57,6 @@ public class AddContactActivity extends BaseToolbarActivity {
     // 递增数
     AtomicInteger index = new AtomicInteger();
 
-
     @Override
     public int getLayoutId() {
         return R.layout.activity_add_contact;
@@ -70,50 +77,164 @@ public class AddContactActivity extends BaseToolbarActivity {
                     ToastTintUtils.warning("运行中");
                     return;
                 }
-                // 获取手机号码开头、结尾
-                String start = EditTextUtils.getText(vid_aac_start_edit);
-                String end = EditTextUtils.getText(vid_aac_end_edit);
-                // 判断是否符合条件
-                String temp = start + end;
-                if (!ValidatorUtils.isNumber(temp)) {
-                    ToastTintUtils.error("请输入数字");
-                    return;
-                }
-                int length = temp.length();
-                if (length >= 11) {
-                    ToastTintUtils.error("开头与结尾需少于11位");
-                    return;
-                }
-                int diff = 11 - length;
-                // 待生成号码总数
-                count = ConvertUtils.toInt("1" + StringUtils.forString(diff, "0"));
-                // 临时号码
-                String tempNumber = start + StringUtils.forString(diff, "0") + end;
-                String tempNumber2 = start + StringUtils.forString(diff, "9") + end;
-                // 判断是否手机号
-                if (!ValiToPhoneUtils.isPhone(tempNumber)) {
-                    ToastTintUtils.error("请输入正确的手机号");
-                    return;
-                }
-                // 进行提示
-                StringBuilder builder = new StringBuilder();
-                builder.append("将会创建 " + count + " 条联系人数据\n");
-                builder.append(tempNumber + " - " + tempNumber2);
-                DialogUtils.createAlertDialog(mContext, "创建提示", builder.toString(),
-                        "取消", "创建", new DialogUtils.DialogListener() {
+                PermissionUtils.permission(Manifest.permission.WRITE_CONTACTS)
+                        .callBack(new PermissionUtils.PermissionCallBack() {
+                            /**
+                             * 授权通过权限回调
+                             */
                             @Override
-                            public void onLeftButton(DialogInterface dialog) {
-                                dialog.dismiss();
+                            public void onGranted() {
+                                // 联系人创建校验
+                                createCheck();
                             }
 
+                            /**
+                             * 授权未通过权限回调
+                             * @param grantedList  申请通过的权限
+                             * @param deniedList   申请未通过的权限
+                             * @param notFoundList 查询不到的权限 ( 包含未注册 )
+                             */
                             @Override
-                            public void onRightButton(DialogInterface dialog) {
-                                dialog.dismiss();
-                                runing = true;
-                                KeyBoardUtils.closeKeyboard();
+                            public void onDenied(List<String> grantedList, List<String> deniedList, List<String> notFoundList) {
+                                ToastUtils.showLong("请开启联系人写入权限.");
                             }
-                        }).show();
+                        }).request(this);
                 break;
+        }
+    }
+
+    /**
+     * 联系人创建校验
+     */
+    private void createCheck() {
+        // 获取手机号码开头、结尾
+        String start = EditTextUtils.getText(vid_aac_start_edit);
+        String end = EditTextUtils.getText(vid_aac_end_edit);
+        // 判断是否符合条件
+        String temp = start + end;
+        if (!ValidatorUtils.isNumber(temp)) {
+            ToastTintUtils.error("请输入数字");
+            return;
+        }
+        int length = temp.length();
+        if (length >= 11) {
+            ToastTintUtils.error("开头与结尾需少于11位");
+            return;
+        }
+        int diff = 11 - length;
+        // 待生成号码总数
+        int middle = ConvertUtils.toInt("1" + StringUtils.forString(diff, "0"));
+        // 临时号码
+        String tempNumber = start + StringUtils.forString(diff, "0") + end;
+        String tempNumber2 = start + StringUtils.forString(diff, "9") + end;
+        // 判断是否手机号
+        if (!ValiToPhoneUtils.isPhone(tempNumber)) {
+            ToastTintUtils.error("请输入正确的手机号");
+            return;
+        }
+        // 进行提示
+        StringBuilder builder = new StringBuilder();
+        builder.append("将会创建 " + middle + " 条联系人数据\n");
+        builder.append(tempNumber + " - " + tempNumber2);
+        DialogUtils.createAlertDialog(mContext, "创建提示", builder.toString(),
+                "取消", "创建", new DialogUtils.DialogListener() {
+                    @Override
+                    public void onLeftButton(DialogInterface dialog) {
+                        dialog.dismiss();
+                    }
+
+                    @Override
+                    public void onRightButton(DialogInterface dialog) {
+                        dialog.dismiss();
+                        // 创建联系人
+                        createContact(start, end, middle);
+                    }
+                }).show();
+    }
+
+    /**
+     * 创建联系人
+     * @param start 开头
+     * @param end   结尾
+     * @param count 创建总数
+     */
+    private void createContact(String start, String end, int count) {
+        ViewUtils.setVisibility(true, vid_aac_tips_tv);
+        KeyBoardUtils.closeKeyboard();
+        this.count = count;
+        this.runing = true;
+        this.index.set(0);
+        // 线程数
+        int threadCount = 5;
+        // 平均数
+        int average = count / 5;
+        // 间隔数
+        int[] intervals = new int[threadCount];
+        for (int i = 0; i < threadCount; i++) {
+            intervals[i] = (i + 1) * average;
+        }
+        for (int i = 0; i < threadCount; i++) {
+            if (i == 0) {
+                createThread(start, end, 0, intervals[i], threadCount);
+            } else {
+                createThread(start, end, intervals[i - 1], intervals[i], threadCount);
+            }
+        }
+    }
+
+    /**
+     * 创建线程
+     * @param start       开头
+     * @param end         结尾
+     * @param first       起点
+     * @param interval    间隔值 ( size )
+     * @param threadCount 线程数量
+     */
+    private void createThread(String start, String end, int first, int interval, int threadCount) {
+        DevThreadManager.getInstance(threadCount).execute(new Runnable() {
+            @Override
+            public void run() {
+                if (first == 0) {
+                    int length = String.valueOf(interval).length();
+                    for (int i = first; i < interval; i++) {
+                        int length1 = String.valueOf(i).length();
+                        if (length1 == length) { // 长度相同则不需要补 0
+                            String phoneNumber = start + i + end;
+                            addContact(phoneNumber, phoneNumber);
+                        } else {
+                            String zero = StringUtils.forString(length - length1, "0");
+                            String phoneNumber = start + zero + i + end;
+                            addContact(phoneNumber, phoneNumber);
+                        }
+                        changeTips();
+                        // 如果页面销毁了则不处理
+                        if (ActivityUtils.isFinishing(mActivity)) return;
+                    }
+                } else {
+                    for (int i = first; i < interval; i++) {
+                        String phoneNumber = start + i + end;
+                        addContact(phoneNumber, phoneNumber);
+                        changeTips();
+                        // 如果页面销毁了则不处理
+                        if (ActivityUtils.isFinishing(mActivity)) return;
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     * 改变提示
+     */
+    private void changeTips() {
+        int value = index.getAndIncrement();
+        if (count == value + 1) {
+            String tips = count + " 条数据, 创建成功";
+            TextViewUtils.setText(vid_aac_tips_tv, tips);
+            ToastTintUtils.success(tips);
+            runing = false;
+        } else {
+            TextViewUtils.setText(vid_aac_tips_tv, "需创建 " + count + " 条数据, 已创建 " + (value + 1) + " 条");
         }
     }
 
