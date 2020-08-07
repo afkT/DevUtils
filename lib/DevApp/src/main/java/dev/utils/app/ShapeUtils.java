a533f7be66dfb911279ae699d15e167d9d73c9a0
Ttt <13798405957@163.com>
2020-08-07 14:49:28 +0800

    1.更新 gradle apply from
    Former-commit-id: 12a49f1527c489d09c44ac0f0dabc8b2d819d69d
    Former-commit-id: a796eff24264fe211264846ddf5438e2c3ecedaa

--- lib/DevApp/src/main/java/dev/utils/app/ShapeUtils.java
+++ lib/DevApp/src/main/java/dev/utils/app/ShapeUtils.java
@@ -21,52 +21,52 @@ import androidx.annotation.RequiresApi;
  *     @see <a href="https://www.cnblogs.com/lang-yu/p/6112052.html"/>
  * </pre>
  * <pre>
- *      <shape
- *          android:shape=[rectangle | oval | line | ring]  矩形 ( 默认 ) 、椭圆形、直线形、环形
- *          android:useLevel=[true | false]                 使用 LevelListDrawable 对象需设置为 true, 设置为 true 无渐变, false 有渐变色
- *          // 以下 4 个属性只有当类型为环形时才有效
- *          android:innerRadius="dimension"                 内环半径
- *          android:innerRadiusRatio="float"                内环半径相对于环的宽度比例, 比如环的宽度为 50, 比例为 2.5, 那么内环半径为 20
- *          android:thickness="dimension"                   环厚度
- *          android:thicknessRatio="float"                  环厚度相对于环的宽度比例
- *          />
- *      <solid
- *          android:color="color"                           背景填充颜色
- *          />
- *      <stroke
- *          android:width="dimension"                       描边宽度
- *          android:color="color"                           描边颜色
- *          android:dashWidth="dimension"                   描边虚线宽度, 值为 0 时, 表示为实线, 值大于 0 则为虚线
- *          android:dashGap="dimension"                     描边为虚线时, 虚线之间的间隔 即「 - - - - 」
- *          />
- *      <corners
- *          android:radius="dimension"                      圆角值
- *          android:topLeftRadius="dimension"               左上圆角值
- *          android:topRightRadius="dimension"              右上圆角值
- *          android:bottomLeftRadius="dimension"            左下圆角值
- *          android:bottomRightRadius="dimension"           右下圆角值
- *          />
- *      <gradient
- *          android:startColor="color"                      起始颜色
- *          android:centerColor="color"                     渐变中间颜色, 即开始颜色与结束颜色之间的颜色
- *          android:endColor="color"                        结束颜色
- *          android:angle="integer"                         渐变角度 ( 当 angle = 0 时, 渐变色是从左向右, 然后逆时针方向转, 当 angle = 90 时为从下往上, angle 必须为 45 的整数倍 )
- *          android:type=[linear | radial | sweep]          渐变类型 linear ( 线性渐变 ) 、radial ( 放射性渐变 ) 、sweep ( 扫描线式渐变 )
- *          android:centerX="float"                         渐变中心 X 点坐标的相对位置, 范围: 0 ~ 1
- *          android:centerY="float"                         渐变中心 Y 点坐标的相对位置, 范围: 0 ~ 1
- *          android:gradientRadius="float"                  渐变色半径, 当 android:type="radial" 时才使用, 单独使用 android:type="radial" 会报错
- *          android:useLevel=[true | false]                 使用 LevelListDrawable 对象需设置为 true, 设置为 true 无渐变, false 有渐变色
- *          />
- *      <padding
- *          android:left="dimension"                        左内边距
- *          android:top="dimension"                         上内边距
- *          android:right="dimension"                       右内边距
- *          android:bottom="dimension"                      下内边距
- *          />
- *      <size
- *          android:width="dimension"                       shape drawable 宽
- *          android:height="dimension"                      shape drawable 高
- *          />
+ *     <shape
+ *         android:shape=[rectangle | oval | line | ring]  矩形 ( 默认 ) 、椭圆形、直线形、环形
+ *         android:useLevel=[true | false]                 使用 LevelListDrawable 对象需设置为 true, 设置为 true 无渐变, false 有渐变色
+ *         // 以下 4 个属性只有当类型为环形时才有效
+ *         android:innerRadius="dimension"                 内环半径
+ *         android:innerRadiusRatio="float"                内环半径相对于环的宽度比例, 比如环的宽度为 50, 比例为 2.5, 那么内环半径为 20
+ *         android:thickness="dimension"                   环厚度
+ *         android:thicknessRatio="float"                  环厚度相对于环的宽度比例
+ *         />
+ *     <solid
+ *         android:color="color"                           背景填充颜色
+ *         />
+ *     <stroke
+ *         android:width="dimension"                       描边宽度
+ *         android:color="color"                           描边颜色
+ *         android:dashWidth="dimension"                   描边虚线宽度, 值为 0 时, 表示为实线, 值大于 0 则为虚线
+ *         android:dashGap="dimension"                     描边为虚线时, 虚线之间的间隔 即「 - - - - 」
+ *         />
+ *     <corners
+ *         android:radius="dimension"                      圆角值
+ *         android:topLeftRadius="dimension"               左上圆角值
+ *         android:topRightRadius="dimension"              右上圆角值
+ *         android:bottomLeftRadius="dimension"            左下圆角值
+ *         android:bottomRightRadius="dimension"           右下圆角值
+ *         />
+ *     <gradient
+ *         android:startColor="color"                      起始颜色
+ *         android:centerColor="color"                     渐变中间颜色, 即开始颜色与结束颜色之间的颜色
+ *         android:endColor="color"                        结束颜色
+ *         android:angle="integer"                         渐变角度 ( 当 angle = 0 时, 渐变色是从左向右, 然后逆时针方向转, 当 angle = 90 时为从下往上, angle 必须为 45 的整数倍 )
+ *         android:type=[linear | radial | sweep]          渐变类型 linear ( 线性渐变 ) 、radial ( 放射性渐变 ) 、sweep ( 扫描线式渐变 )
+ *         android:centerX="float"                         渐变中心 X 点坐标的相对位置, 范围: 0 ~ 1
+ *         android:centerY="float"                         渐变中心 Y 点坐标的相对位置, 范围: 0 ~ 1
+ *         android:gradientRadius="float"                  渐变色半径, 当 android:type="radial" 时才使用, 单独使用 android:type="radial" 会报错
+ *         android:useLevel=[true | false]                 使用 LevelListDrawable 对象需设置为 true, 设置为 true 无渐变, false 有渐变色
+ *         />
+ *     <padding
+ *         android:left="dimension"                        左内边距
+ *         android:top="dimension"                         上内边距
+ *         android:right="dimension"                       右内边距
+ *         android:bottom="dimension"                      下内边距
+ *         />
+ *     <size
+ *         android:width="dimension"                       shape drawable 宽
+ *         android:height="dimension"                      shape drawable 高
+ *         />
  * </pre>
  */
 public final class ShapeUtils {
