7ba342b5a84ebb16ff613f983ff564adcdf05382
Ttt <13798405957@163.com>
2020-08-09 00:27:07 +0800

    1.更新 DevBase Module 部分 kotlin 代码
    Former-commit-id: 7161ed73f7a8fdfef1e541e1d6f63ebedb008e63
    Former-commit-id: 0beb0f33b87b739f5909d9995c338b53fafe86da

---
+++ lib/DevBase/src/main/java/dev/base/able/IDevBaseUIOperation.kt
@@ -0,0 +1,105 @@
+package dev.base.able
+
+import android.app.Dialog
+import android.widget.PopupWindow
+import androidx.fragment.app.DialogFragment
+
+/**
+ * detail: 基类 UI 交互相关操作方法
+ * @author Ttt
+ */
+interface IDevBaseUIOperation {
+
+    // =========
+    // = Toast =
+    // =========
+
+    /**
+     * 显示 Toast
+     * @param text Toast 提示文本
+     * @param objs 格式化参数
+     */
+    fun showToast(text: String?, vararg objs: Any?)
+
+    /**
+     * 显示 Toast
+     * @param resId R.string.id
+     * @param objs  格式化参数
+     */
+    fun showToast(resId: Int, vararg objs: Any?)
+
+    // ===============
+    // = PopupWindow =
+    // ===============
+
+    /**
+     * 获取 PopupWindow
+     * @return [PopupWindow]
+     */
+    val popupWindow: PopupWindow?
+
+    /**
+     * 设置 PopupWindow
+     * @param popupWindow [PopupWindow]
+     * @return [PopupWindow]
+     */
+    fun <T : PopupWindow?> setPopupWindow(popupWindow: T): T
+
+    /**
+     * 设置 PopupWindow
+     * @param isClose     是否关闭之前的 PopupWindow
+     * @param popupWindow [PopupWindow]
+     * @return [PopupWindow]
+     */
+    fun <T : PopupWindow?> setPopupWindow(isClose: Boolean, popupWindow: T): T
+
+    // ==========
+    // = Dialog =
+    // ==========
+
+    /**
+     * 获取 Dialog
+     * @return [Dialog]
+     */
+    val dialog: Dialog?
+
+    /**
+     * 设置 Dialog
+     * @param dialog [Dialog]
+     * @return [Dialog]
+     */
+    fun <T : Dialog?> setDialog(dialog: T): T
+
+    /**
+     * 设置 Dialog
+     * @param isClose 是否关闭之前的 Dialog
+     * @param dialog  [Dialog]
+     * @return [Dialog]
+     */
+    fun <T : Dialog?> setDialog(isClose: Boolean, dialog: T): T
+
+    // ==================
+    // = DialogFragment =
+    // ==================
+
+    /**
+     * 获取 DialogFragment
+     * @return [DialogFragment]
+     */
+    val dialogFragment: DialogFragment?
+
+    /**
+     * 设置 DialogFragment
+     * @param dialog [DialogFragment]
+     * @return [DialogFragment]
+     */
+    fun <T : DialogFragment?> setDialogFragment(dialog: T): T
+
+    /**
+     * 设置 DialogFragment
+     * @param isClose 是否关闭之前的 DialogFragment
+     * @param dialog  [DialogFragment]
+     * @return [DialogFragment]
+     */
+    fun <T : DialogFragment?> setDialogFragment(isClose: Boolean, dialog: T): T
+}
