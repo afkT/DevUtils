fe5030a7714369f2ee6b2dfd824050b87960fb90
Ttt <13798405957@163.com>
2020-08-21 14:35:40 +0800

    1.新增 DevBaseMVPViewBindingActivity、DevBaseMVPViewBindingFragment
    Former-commit-id: 6e959333e6262bb90a9c946df6d4cf908606a03c
    Former-commit-id: fdb1dbe2a2e3e8759523c5f20c4970411021dd36

---
+++ lib/DevBase/src/main/java/dev/base/expand/content/DevBaseContentMVPViewBindingActivity.java
@@ -0,0 +1,53 @@
+package dev.base.expand.content;
+
+import android.os.Bundle;
+
+import androidx.viewbinding.ViewBinding;
+
+import dev.base.expand.mvp.MVP;
+
+/**
+ * detail: Content MVP Activity ViewBinding 基类
+ * @author Ttt
+ */
+public abstract class DevBaseContentMVPViewBindingActivity<P extends MVP.Presenter, VB extends ViewBinding>
+        extends DevBaseContentViewBindingActivity<VB> implements MVP.IView {
+
+    // MVP Presenter
+    protected P mPresenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // 创建 MVP 模式的 Presenter
+        createPresenter();
+        // 初始化操作
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 取消 MVP 各个模块间的关联
+        if (mPresenter != null) mPresenter.detachView();
+    }
+
+    /**
+     * 创建 MVP 模式的 Presenter
+     */
+    private void createPresenter() {
+        // 初始化 Presenter
+        mPresenter = presenter();
+        // 绑定关联
+        if (mPresenter != null) mPresenter.attachView(this);
+    }
+
+    // ====================
+    // = 对外要求实现方法 =
+    // ====================
+
+    /**
+     * 初始化创建 Presenter
+     * @return {@link MVP.Presenter}
+     */
+    protected abstract P presenter();
+}
