7caab2a624fda4a0ae09b3d55da0562bf773916f
Ttt <13798405957@163.com>
2020-08-24 17:13:26 +0800

    1.更新 app module Base 相关代码
    Former-commit-id: c75e3bd2ec8c96b847f257dc0c45a89a2f636174
    Former-commit-id: 6f320ddab1e1c8d5f3c00ce6d0a6f8eec49edb48

--- app/src/main/java/afkt/project/framework/mvvm/ArticleMVVMActivity.java
+++ app/src/main/java/afkt/project/framework/mvvm/ArticleMVVMActivity.java
@@ -31,7 +31,7 @@ public class ArticleMVVMActivity extends BaseMVVMActivity<ActivityArticleMvvmBin
     WhorlView             whorlView;
 
     @Override
-    public int getLayoutId() {
+    public int layoutId() {
         return R.layout.activity_article_mvvm;
     }
 
@@ -43,7 +43,7 @@ public class ArticleMVVMActivity extends BaseMVVMActivity<ActivityArticleMvvmBin
         // 在一开始选型确定后, 才能专门为其设计基类
 
         // MVVM 只需要调用此句绑定
-        viewDataBinding = DataBindingUtil.setContentView(this, getLayoutId());
+        viewDataBinding = DataBindingUtil.bind(mLayoutView);
 
         // = 处理 ActionBar =
         setSupportActionBar(viewDataBinding.vidAamToolbar);
