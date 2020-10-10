9e4639334f80700ebddfdd51f08e8ea8d88c3bcf
Ttt <13798405957@163.com>
2020-10-10 08:02:07 +0800

    1.添加部分文件分片代码
    Former-commit-id: 48cf8a2e2d51c2ffb13d9f7270f18c70570e1cae
    Former-commit-id: 748a34bdc1d8f0ed0b716f52ce6b27880de1ed03

---
+++ lib/DevApp/src/main/java/dev/utils/common/file/FilePartAssist.java
@@ -0,0 +1,151 @@
+package dev.utils.common.file;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import dev.utils.common.CollectionUtils;
+import dev.utils.common.FileUtils;
+
+/**
+ * detail: 文件分片辅助类
+ * @author Ttt
+ */
+public final class FilePartAssist {
+
+    // 文件路径
+    private final File               file;
+    // 文件分片信息集合
+    private final List<FilePartItem> filePartItems = new ArrayList<>();
+
+    /**
+     * 构造函数
+     * @param file          文件
+     * @param filePartItems 文件分片信息集合
+     */
+    public FilePartAssist(final File file, final List<FilePartItem> filePartItems) {
+        this.file = file;
+        if (filePartItems != null) {
+            this.filePartItems.addAll(filePartItems);
+        }
+    }
+
+    /**
+     * 构造函数
+     * @param filePath  文件路径
+     * @param partCount 分片总数
+     */
+    public FilePartAssist(final String filePath, final int partCount) {
+        this(FileUtils.getFile(filePath), partCount);
+    }
+
+    /**
+     * 构造函数
+     * @param file      文件
+     * @param partCount 分片总数
+     */
+    public FilePartAssist(final File file, final int partCount) {
+        this.file = file;
+        if (file != null && partCount > 0) {
+            // 原始文件总字节
+            long fileByteLength = file.length();
+            // 分片总字节
+            long partByteLength = fileByteLength / partCount;
+            // 余数 ( 全部加到最后一个分片 )
+            long remainder = fileByteLength - partByteLength * partCount;
+            // 当前分片字节累加总数
+            int total = 0;
+            if (partCount > 1) { // 如果分片大于 1 个, 则处理前面的数量
+                for (int i = 0, len = partCount - 1; i < len; i++) {
+                    FilePartItem item = new FilePartItem(
+                            i, partCount, partByteLength, fileByteLength,
+                            total, total + partByteLength
+                    );
+                    total += partByteLength;
+                    filePartItems.add(item);
+                }
+            }
+            // 最后一个分片片段
+            FilePartItem item = new FilePartItem(
+                    partCount - 1, partCount, partByteLength, fileByteLength,
+                    total, total + partByteLength + remainder
+            );
+            filePartItems.add(item);
+        }
+    }
+
+    // ===============
+    // = 对外公开方法 =
+    // ===============
+
+    /**
+     * 获取文件
+     * @return {@link File}
+     */
+    public File getFile() {
+        return file;
+    }
+
+    /**
+     * 获取文件名
+     * @return 文件名
+     */
+    public String getFileName() {
+        return FileUtils.getFileName(file);
+    }
+
+    /**
+     * 获取文件分片信息集合
+     * @return {@link List<FilePartItem>}
+     */
+    public List<FilePartItem> getFilePartItems() {
+        return filePartItems;
+    }
+
+    /**
+     * 获取分片总数
+     * @return 分片总数
+     */
+    public int getPartCount() {
+        return filePartItems.size();
+    }
+
+    /**
+     * 是否只有一个分片文件
+     * @return {@code true} yes, {@code false} no
+     */
+    public boolean isOnlyOne() {
+        return getPartCount() == 1;
+    }
+
+    /**
+     * 获取分片文件名 ( 后缀索引拼接 )
+     * @param partIndex 分片索引
+     * @return 分片文件名
+     */
+    public String getPartName(final int partIndex) {
+        return FilePartUtils.getPartName(getFileName(), partIndex);
+    }
+
+    /**
+     * 获取指定索引文件分片信息
+     * @param partIndex 分片索引
+     * @return {@link FilePartItem}
+     */
+    public FilePartItem getFilePartItem(final int partIndex) {
+        return CollectionUtils.get(filePartItems, partIndex);
+    }
+
+    // ===========
+    // = 文件拆分 =
+    // ===========
+
+    /**
+     * 文件拆分
+     * @param partIndex 分片索引
+     * @return 指定位置数据
+     */
+    public byte[] fileSplit(final int partIndex) {
+        return FilePartUtils.fileSplit(file, getFilePartItem(partIndex));
+    }
+}
---
+++ lib/DevApp/src/main/java/dev/utils/common/file/FilePartUtils.java
@@ -0,0 +1,326 @@
+package dev.utils.common.file;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.util.Arrays;
+
+import dev.utils.JCLogUtils;
+import dev.utils.common.ArrayUtils;
+import dev.utils.common.CloseUtils;
+import dev.utils.common.FileUtils;
+
+/**
+ * detail: 文件分片工具类
+ * @author Ttt
+ */
+public final class FilePartUtils {
+
+    private FilePartUtils() {
+    }
+
+    // 日志 TAG
+    private static final String TAG         = FilePartUtils.class.getSimpleName();
+    // 文件后缀
+    public static final  String PART_SUFFIX = "_filePart_";
+    // 分片数量
+    public static final  int    PART_COUNT  = 10;
+    // 分片片段允许最小值 byte ( 1mb )
+    public static final  long   MIN_LENGTH  = 1048576L;
+
+    // ===============
+    // = 对外公开方法 =
+    // ===============
+
+    /**
+     * 获取分片文件名 ( 后缀索引拼接 )
+     * @param item     {@link FilePartItem}
+     * @param fileName 原始文件名
+     * @return 分片文件名
+     */
+    public static String getPartName(final FilePartItem item, final String fileName) {
+        return item != null ? item.getPartName(fileName) : null;
+    }
+
+    /**
+     * 获取分片文件名 ( 后缀索引拼接 )
+     * @param assist    {@link FilePartAssist}
+     * @param partIndex 分片索引
+     * @return 分片文件名
+     */
+    public static String getPartName(final FilePartAssist assist, final int partIndex) {
+        return assist != null ? assist.getPartName(partIndex) : null;
+    }
+
+    /**
+     * 获取分片文件名 ( 后缀索引拼接 )
+     * @param fileName  原始文件名
+     * @param partIndex 分片索引
+     * @return 分片文件名
+     */
+    public static String getPartName(final String fileName, final int partIndex) {
+        return String.format("%s%s%s", fileName, PART_SUFFIX, partIndex);
+    }
+
+    // =
+
+    /**
+     * 获取文件分片辅助类
+     * @param filePath 文件路径
+     * @return {@link FilePartAssist}
+     */
+    public static FilePartAssist getFilePartAssist(final String filePath) {
+        return getFilePartAssist(FileUtils.getFile(filePath), PART_COUNT, MIN_LENGTH);
+    }
+
+    /**
+     * 获取文件分片辅助类
+     * @param filePath  文件路径
+     * @param partCount 分片总数
+     * @param minLength 分片片段允许最小值 byte
+     * @return {@link FilePartAssist}
+     */
+    public static FilePartAssist getFilePartAssist(final String filePath,
+                                                   final int partCount, final long minLength) {
+        return getFilePartAssist(FileUtils.getFile(filePath), partCount, minLength);
+    }
+
+    /**
+     * 获取文件分片辅助类
+     * @param file 文件
+     * @return {@link FilePartAssist}
+     */
+    public static FilePartAssist getFilePartAssist(final File file) {
+        return getFilePartAssist(file, PART_COUNT, MIN_LENGTH);
+    }
+
+    /**
+     * 获取文件分片辅助类
+     * @param file      文件
+     * @param partCount 分片总数
+     * @param minLength 分片片段允许最小值 byte
+     * @return {@link FilePartAssist}
+     */
+    public static FilePartAssist getFilePartAssist(final File file,
+                                                   final int partCount, final long minLength) {
+        // 原始文件总字节
+        long fileByteLength = FileUtils.getFileLength(file);
+        // 分片总字节
+        long partByteLength = fileByteLength / partCount;
+        if (partByteLength >= minLength) {
+            return new FilePartAssist(file, partCount);
+        } else {
+            return new FilePartAssist(file, 1);
+        }
+    }
+
+    // =
+
+    /**
+     * 是否符合文件分片条件
+     * @param filePath 文件路径
+     * @return {@code true} yes, {@code false} no
+     */
+    public static boolean isFilePart(final String filePath) {
+        return isFilePart(FileUtils.getFile(filePath), PART_COUNT, MIN_LENGTH);
+    }
+
+    /**
+     * 是否符合文件分片条件
+     * @param filePath  文件路径
+     * @param partCount 分片总数
+     * @param minLength 分片片段允许最小值 byte
+     * @return {@code true} yes, {@code false} no
+     */
+    public static boolean isFilePart(final String filePath,
+                                     final int partCount, final long minLength) {
+        return isFilePart(FileUtils.getFile(filePath), partCount, minLength);
+    }
+
+    /**
+     * 是否符合文件分片条件
+     * @param file 文件
+     * @return {@code true} yes, {@code false} no
+     */
+    public static boolean isFilePart(final File file) {
+        return isFilePart(file, PART_COUNT, MIN_LENGTH);
+    }
+
+    /**
+     * 是否符合文件分片条件
+     * @param file      文件
+     * @param partCount 分片总数
+     * @param minLength 分片片段允许最小值 byte
+     * @return {@code true} yes, {@code false} no
+     */
+    public static boolean isFilePart(final File file,
+                                     final int partCount, final long minLength) {
+        // 原始文件总字节
+        long fileByteLength = FileUtils.getFileLength(file);
+        // 分片总字节
+        long partByteLength = fileByteLength / partCount;
+        return partByteLength >= minLength;
+    }
+
+    // ===========
+    // = 文件拆分 =
+    // ===========
+
+    /**
+     * 文件拆分
+     * @param filePath 文件路径
+     * @param start    分片字节开始索引
+     * @param end      分片字节结束索引
+     * @return 指定位置数据
+     */
+    public static byte[] fileSplit(final String filePath, final long start, final long end) {
+        return fileSplit(FileUtils.getFile(filePath), start, end);
+    }
+
+    /**
+     * 文件拆分
+     * @param file  文件
+     * @param start 分片字节开始索引
+     * @param end   分片字节结束索引
+     * @return 指定位置数据
+     */
+    public static byte[] fileSplit(final File file, final long start, final long end) {
+        if (file != null && start >= 0 && end > start) {
+            RandomAccessFile raf = null;
+            try {
+                raf = new RandomAccessFile(file, "r");
+                if (end > raf.length()) {
+                    return null;
+                }
+                raf.seek(start);
+                byte[] bytes = new byte[(int) (end - start)];
+                raf.read(bytes);
+                return bytes;
+            } catch (Exception e) {
+                JCLogUtils.eTag(TAG, e, "fileSplit");
+            } finally {
+                CloseUtils.closeIOQuietly(raf);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 文件拆分
+     * @param filePath 文件路径
+     * @param item     {@link FilePartItem}
+     * @return 指定位置数据
+     */
+    public static byte[] fileSplit(final String filePath, final FilePartItem item) {
+        return fileSplit(FileUtils.getFile(filePath), item);
+    }
+
+    /**
+     * 文件拆分
+     * @param file 文件
+     * @param item {@link FilePartItem}
+     * @return 指定位置数据
+     */
+    public static byte[] fileSplit(final File file, final FilePartItem item) {
+        if (file == null || item == null) return null;
+        return fileSplit(file, item.start, item.end);
+    }
+
+    /**
+     * 文件拆分
+     * @param filePath  文件路径
+     * @param assist    {@link FilePartAssist}
+     * @param partIndex 分片索引
+     * @return 指定位置数据
+     */
+    public static byte[] fileSplit(final String filePath,
+                                   final FilePartAssist assist, final int partIndex) {
+        return fileSplit(FileUtils.getFile(filePath), assist, partIndex);
+    }
+
+    /**
+     * 文件拆分
+     * @param file      文件
+     * @param assist    {@link FilePartAssist}
+     * @param partIndex 分片索引
+     * @return 指定位置数据
+     */
+    public static byte[] fileSplit(final File file,
+                                   final FilePartAssist assist, final int partIndex) {
+        if (file == null || assist == null) return null;
+        return fileSplit(file, assist.getFilePartItem(partIndex));
+    }
+
+    // =
+
+    /**
+     * 文件拆分并存储
+     * @param filePath       文件路径
+     * @param start          分片字节开始索引
+     * @param end            分片字节结束索引
+     * @param destFolderPath 目标文件夹地址
+     * @param partName       分片文件名
+     * @return 指定位置数据
+     */
+    public static boolean fileSplitSave(final String filePath, final long start, final long end,
+                                        final String destFolderPath, final String partName) {
+        return fileSplitSave(FileUtils.getFile(filePath), start, end, destFolderPath, partName);
+    }
+
+    /**
+     * 文件拆分并存储
+     * @param file           文件
+     * @param start          分片字节开始索引
+     * @param end            分片字节结束索引
+     * @param destFolderPath 目标文件夹地址
+     * @param partName       分片文件名
+     * @return 指定位置数据
+     */
+    public static boolean fileSplitSave(final File file, final long start, final long end,
+                                        final String destFolderPath, final String partName) {
+        if (file != null && start >= 0 && end > start) {
+            FileInputStream fis = null;
+            FileChannel inputChannel = null;
+            FileOutputStream fos = null;
+            FileChannel outputChannel = null;
+            try {
+                fis = new FileInputStream(file);
+                if (end > file.length()) return false;
+                inputChannel = fis.getChannel();
+                fos = new FileOutputStream(new File(destFolderPath, partName));
+                outputChannel = fos.getChannel();
+                inputChannel.transferTo(start, (int) (end - start), outputChannel);
+                return true;
+            } catch (Exception e) {
+                JCLogUtils.eTag(TAG, e, "fileSplitSave");
+            } finally {
+                CloseUtils.closeIOQuietly(outputChannel, fos, inputChannel, fis);
+            }
+        }
+        return false;
+    }
+
+    // =======
+    // = 删除 =
+    // =======
+
+    public static void main(String[] args) throws Exception {
+        String filePath = "D:\\测试文件夹\\c.mp4";
+        String folderPath = "D:\\测试文件夹\\file\\";
+        String partName = "c.mp4_filePart_0";
+
+//        FilePartAssist assist = getFilePartAssist(filePath, 10, MIN_LENGTH);
+//
+//        byte[] bytes = fileSplit(new File(filePath), 0, 158595 + 3);
+        boolean resultAA = fileSplitSave(new File(filePath), 0, FileUtils.readFileBytes(filePath).length, folderPath, getPartName("c.mp4", 0));
+
+        byte[] bytes = FileUtils.readFileBytes(filePath);
+        byte[] asd = ArrayUtils.subarray(bytes, 0, bytes.length);
+        String a = Arrays.toString(asd);
+        String b = Arrays.toString(FileUtils.readFileBytes(new File(folderPath, partName)));
+        boolean result = a.equals(b);
+        String q = "";
+    }
+}
