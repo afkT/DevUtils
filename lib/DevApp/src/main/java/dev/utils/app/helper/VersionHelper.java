52efc7f1db0d9835adfb1b14e9c16bd0aacb3887
Ttt <13798405957@163.com>
2020-10-20 19:23:25 +0800

    1.新增 Android 版本适配 Helper 类 VersionHelper
    Former-commit-id: b2a59fe182cef68c8c47d2bc167d63bb339dd0ce
    Former-commit-id: 472bc2aef5f77664c3b413d3a4a3c6bd8bce00e9

---
+++ lib/DevApp/src/main/java/dev/utils/app/helper/VersionHelper.java
@@ -0,0 +1,18 @@
+package dev.utils.app.helper;
+
+/**
+ * detail: Android 版本适配 Helper 类
+ * @author Ttt
+ * <pre>
+ *     Android 版本适配 Helper 类, 方便快捷使用
+ *     并简化需多工具类组合使用的功能
+ * </pre>
+ */
+public final class VersionHelper {
+
+    private VersionHelper() {
+    }
+
+    // 日志 TAG
+    private static final String TAG = VersionHelper.class.getSimpleName();
+}
