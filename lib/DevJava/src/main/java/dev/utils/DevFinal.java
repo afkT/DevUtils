3264b217201e22b5b8fd29ca9e8c308585b532e0
Ttt <13798405957@163.com>
2020-11-11 13:50:37 +0800

    1.同步更新 DevJava 代码
    Former-commit-id: f90d7fa37a56489ce6e850659e35e03d00d9baca
    Former-commit-id: 15f014221321cef4d32926fdff68529eae90d7f0

--- lib/DevJava/src/main/java/dev/utils/DevFinal.java
+++ lib/DevJava/src/main/java/dev/utils/DevFinal.java
@@ -135,9 +135,10 @@ public final class DevFinal {
     // ===========
 
     // 用于建立十六进制字符的输出的小写字符数组
-    public static final char[] HEX_DIGITS       = {
+    public static final char[] HEX_DIGITS = {
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
     };
+
     // 用于建立十六进制字符的输出的大写字符数组
     public static final char[] HEX_DIGITS_UPPER = {
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
--- lib/DevJava/src/main/java/dev/utils/common/DateUtils.java
+++ lib/DevJava/src/main/java/dev/utils/common/DateUtils.java
@@ -1105,20 +1105,23 @@ public final class DateUtils {
     // =============
 
     // 生肖数组
-    private static final String[] ZODIAC             = {
+    private static final String[] ZODIAC = {
             "猴", "鸡", "狗", "猪", "鼠", "牛", "虎", "兔", "龙", "蛇", "马", "羊"
     };
+
     // 星座截止天数
-    private static final int[]    CONSTELLATION_DAY  = {
+    private static final int[] CONSTELLATION_DAY = {
             20, 19, 21, 21, 21, 22, 23, 23, 23, 24, 23, 22
     };
+
     // 星座对应日期
     private static final String[] CONSTELLATION_DATE = {
             "01.20-02.18", "02.19-03.20", "03.21-04.19", "04.20-05.20", "05.21-06.21", "06.22-07.22",
             "07.23-08.22", "08.23-09.22", "09.23-10.23", "10.24-11.22", "11.23-12.21", "12.22-01.19"
     };
+
     // 星座数组
-    private static final String[] CONSTELLATION      = {
+    private static final String[] CONSTELLATION = {
             "水瓶座", "双鱼座", "白羊座", "金牛座", "双子座", "巨蟹座",
             "狮子座", "处女座", "天秤座", "天蝎座", "射手座", "摩羯座"
     };
