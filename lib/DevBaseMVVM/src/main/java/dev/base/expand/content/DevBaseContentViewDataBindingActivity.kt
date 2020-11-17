37bbf8a78780aed982954292040105f6cf44fa6a
Ttt <13798405957@163.com>
2020-11-17 11:50:53 +0800

    1.更新 DemoKtx 使用 DevBaseMVVM Content 代码
    Former-commit-id: e5a282a85bb9af9d05c07309f683917d5d1ddc79
    Former-commit-id: 1b47f91668fcec3f581d5577dec8ce7e8aa8d7d3

---
+++ lib/DevBaseMVVM/src/main/java/dev/base/expand/content/DevBaseContentViewDataBindingActivity.kt
@@ -0,0 +1,49 @@
+package dev.base.expand.content
+
+import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import dev.base.able.IDevBaseViewDataBinding
+
+/**
+ * detail: Content Activity ViewDataBinding 基类
+ * @author Ttt
+ */
+abstract class DevBaseContentViewDataBindingActivity<VDB : ViewDataBinding> :
+    DevBaseContentActivity(), IDevBaseViewDataBinding<VDB> {
+
+    lateinit var binding: VDB
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (isViewBinding()) {
+            if (isTryViewBindingCatch()) {
+                try {
+                    // ViewDataBinding 初始化处理
+                    binding = DataBindingUtil.bind<VDB>(getBindingView()!!)!!
+                } catch (e: Exception) {
+                    assist.printLog(e, "onCreate - viewDataBinding")
+                }
+            } else {
+                // ViewDataBinding 初始化处理
+                binding = DataBindingUtil.bind<VDB>(getBindingView()!!)!!
+            }
+            // 支持 LiveData 绑定 xml 数据改变 UI 自动会更新
+            binding.setLifecycleOwner(this)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (isDetachBinding()) binding?.unbind()
+    }
+
+    // ===========================
+    // = IDevBaseViewDataBinding =
+    // ===========================
+
+    final override fun getBindingView(): View? {
+        return layoutView
+    }
+}
