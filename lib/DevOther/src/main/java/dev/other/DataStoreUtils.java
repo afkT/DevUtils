95f9bc559a1b03f826b8d6f73ab9d32eaecb01e8
Ttt <13798405957@163.com>
2020-11-30 23:08:14 +0800

    1.更新部分代码
    Former-commit-id: 58a4d26b98866e70dae3a4a6935304d4dc70b966
    Former-commit-id: b7d5ea792e9c4d31c2621b7dcd838fed42513caa

---
+++ lib/DevOther/src/main/java/dev/other/DataStoreUtils.java
@@ -0,0 +1,20 @@
+package dev.other;
+
+/**
+ * detail: DataStore 工具类
+ * @author Ttt
+ * <pre>
+ *     [Google] 再见 SharedPreferences 拥抱 Jetpack DataStore
+ *     @see <a href="https://juejin.im/post/6881442312560803853"/>
+ *     [Google] 再见 SharedPreferences 拥抱 Jetpack DataStore ( 二 )
+ *     @see <a href="https://juejin.im/post/6888847647802097672"/>
+ * </pre>
+ */
+public final class DataStoreUtils {
+
+    private DataStoreUtils() {
+    }
+
+    // 日志 TAG
+    private static final String TAG = DataStoreUtils.class.getSimpleName();
+}
