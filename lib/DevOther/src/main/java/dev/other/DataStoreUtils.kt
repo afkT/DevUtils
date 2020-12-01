3c07259f5df131d0462beb126c740378efabd415
Ttt <13798405957@163.com>
2020-12-01 10:22:51 +0800

    1.更新 DataStoreUtils.kt
    Former-commit-id: db456084126684b960b1ffd78d6139241090db2e
    Former-commit-id: f4cca459d7f6a7f3858d86a8ee2f175ba775bf44

--- lib/DevOther/src/main/java/dev/other/DataStoreUtils.kt
+++ lib/DevOther/src/main/java/dev/other/DataStoreUtils.kt
@@ -1,20 +1,16 @@
-package dev.other;
+package dev.other
 
 /**
  * detail: DataStore 工具类
  * @author Ttt
- * <pre>
- *     [Google] 再见 SharedPreferences 拥抱 Jetpack DataStore
- *     @see <a href="https://juejin.im/post/6881442312560803853"/>
- *     [Google] 再见 SharedPreferences 拥抱 Jetpack DataStore ( 二 )
- *     @see <a href="https://juejin.im/post/6888847647802097672"/>
- * </pre>
+ * [Google] 再见 SharedPreferences 拥抱 Jetpack DataStore
+ * @see <a href="https://juejin.im/post/6881442312560803853"/>
+ * [Google] 再见 SharedPreferences 拥抱 Jetpack DataStore ( 二 )
+ * @see <a href="https://juejin.im/post/6888847647802097672"/>
  */
-public final class DataStoreUtils {
-
-    private DataStoreUtils() {
-    }
+object DataStoreUtils {
 
     // 日志 TAG
-    private static final String TAG = DataStoreUtils.class.getSimpleName();
+    private val TAG = DataStoreUtils::class.java.simpleName
+
 }
