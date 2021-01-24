3bfd2b66e4c0ef894993b998122e32f5fc529a71
Cjt <jtongttt@gmail.com>
2021-01-24 20:42:42 +0800

    1.更新部分代码
    Former-commit-id: 54fddc83529bd2f2e513f287eb0a285422497a13
    Former-commit-id: b7814ef5193f0253ebb61ef0be5c8fcbdedfcb51

--- lib/DevApp/src/main/java/dev/utils/DevFinal.java
+++ lib/DevApp/src/main/java/dev/utils/DevFinal.java
@@ -20,6 +20,16 @@ public final class DevFinal {
     // 空对象字符串
     public static final String NULL_STR        = "null";
 
+    // 编码格式
+    public static final String US_ASCII   = "US-ASCII";
+    public static final String ISO_8859_1 = "ISO-8859-1";
+    public static final String UTF_8      = "UTF-8";
+    public static final String UTF_16BE   = "UTF-16BE";
+    public static final String UTF_16LE   = "UTF-16LE";
+    public static final String UTF_16     = "UTF-16";
+    public static final String GBK        = "GBK";
+    public static final String GBK_2312   = "GBK-2312";
+
     // ===============
     // = 日期格式类型 =
     // ===============
