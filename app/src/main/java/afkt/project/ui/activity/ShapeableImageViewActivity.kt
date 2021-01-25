8d28b8bf043c2349e3d01f8bf27d0da8b54ebc58
Cjt <jtongttt@gmail.com>
2021-01-25 14:31:09 +0800

    1.转换部分代码
    Former-commit-id: f9db34b74901db3d052b4f35fbdf729afdf46de1
    Former-commit-id: a4ec796e9bdfcacfd3ebe828de0129236102e393

--- app/src/main/java/afkt/project/ui/activity/ShapeableImageViewActivity.kt
+++ app/src/main/java/afkt/project/ui/activity/ShapeableImageViewActivity.kt
@@ -1,14 +1,11 @@
-package afkt.project.ui.activity;
+package afkt.project.ui.activity
 
-import android.os.Bundle;
-
-import com.google.android.material.imageview.ShapeableImageView;
-
-import afkt.project.R;
-import afkt.project.base.app.BaseActivity;
-import afkt.project.databinding.ActivityShapeableImageViewBinding;
-import dev.utils.app.SizeUtils;
-import dev.utils.app.ViewUtils;
+import afkt.project.R
+import afkt.project.base.app.BaseActivity
+import afkt.project.databinding.ActivityShapeableImageViewBinding
+import com.google.android.material.imageview.ShapeableImageView
+import dev.utils.app.SizeUtils
+import dev.utils.app.ViewUtils
 
 /**
  * detail: Material ShapeableImageView
@@ -17,31 +14,22 @@ import dev.utils.app.ViewUtils;
  *     描边需设置 padding 大小为描边宽度一半, 否则显示不全
  * </pre>
  */
-public class ShapeableImageViewActivity
-        extends BaseActivity<ActivityShapeableImageViewBinding> {
+class ShapeableImageViewActivity : BaseActivity<ActivityShapeableImageViewBinding>() {
 
-    @Override
-    public int baseLayoutId() {
-        return R.layout.activity_shapeable_image_view;
-    }
+    override fun baseLayoutId(): Int = R.layout.activity_shapeable_image_view
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-    @Override
-    public void initValue() {
-        super.initValue();
-
-        int margin = SizeUtils.dipConvertPx(15);
-
-        for (int i = 0, len = binding.vidAsivLinear.getChildCount(); i < len; i++) {
-            ShapeableImageView imageView = ViewUtils.getChildAt(binding.vidAsivLinear, i);
-            // 随机图片
-//            GlideUtils.with().displayImage("https://picsum.photos/300", imageView);
-            ViewUtils.setMargin(imageView, 0, margin, 0, 0);
+    override fun initValue() {
+        super.initValue()
+        val margin = SizeUtils.dipConvertPx(15f)
+        for (i in 0..binding.vidAsivLinear.childCount) {
+            val imageView = ViewUtils.getChildAt<ShapeableImageView>(
+                binding.vidAsivLinear, i
+            )
+            ViewUtils.setMargin(imageView, 0, margin, 0, 0)
         }
+        ViewUtils.setMarginBottom(
+            ViewUtils.getChildAtLast<ShapeableImageView>(binding.vidAsivLinear),
+            margin, false
+        )
     }
 }
--- app/src/main/java/afkt/project/ui/activity/SignActivity.kt
+++ app/src/main/java/afkt/project/ui/activity/SignActivity.kt
@@ -1,47 +1,37 @@
-package afkt.project.ui.activity;
+package afkt.project.ui.activity
 
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.view.View;
-import android.widget.LinearLayout;
-
-import androidx.viewbinding.ViewBinding;
-
-import afkt.project.base.app.BaseActivity;
-import dev.widget.function.SignView;
+import afkt.project.base.app.BaseActivity
+import android.graphics.Color
+import android.graphics.Paint
+import android.view.View
+import android.widget.LinearLayout
+import androidx.viewbinding.ViewBinding
+import dev.widget.function.SignView
 
 /**
  * detail: 签名 View
  * @author Ttt
  */
-public class SignActivity
-        extends BaseActivity<ViewBinding> {
+class SignActivity : BaseActivity<ViewBinding>() {
 
-    @Override
-    public boolean isViewBinding() {
-        return false;
-    }
+    override fun isViewBinding(): Boolean = false
 
-    @Override
-    public int baseLayoutId() {
-        return 0;
-    }
+    override fun baseLayoutId(): Int = 0
 
-    @Override
-    public View baseLayoutView() {
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT
-        );
-        SignView signView = new SignView(this);
-        signView.setLayoutParams(layoutParams);
+    override fun baseLayoutView(): View {
+        val layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT
+        )
+        val signView = SignView(this)
+        signView.layoutParams = layoutParams
 
         // 设置画笔
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(30);
-        paint.setColor(Color.BLACK);
-        signView.setPaint(paint);
-        return signView;
+        val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 30f
+        paint.color = Color.BLACK
+        signView.paint = paint
+        return signView
     }
 }
