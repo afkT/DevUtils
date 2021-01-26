90159da5bd29cee20cc615b8f73e848fb1809362
Cjt <jtongttt@gmail.com>
2021-01-26 15:36:24 +0800

    1.新增 BottomSheet UI 效果实现
    Former-commit-id: 87dc41c8fc4359b42983405de06ce793c67d0008
    Former-commit-id: 89276832425a1eed9d69c496a321cd5c8d29fbbe

--- app/src/main/java/afkt/project/ui/activity/BottomSheetActivity.kt
+++ app/src/main/java/afkt/project/ui/activity/BottomSheetActivity.kt
@@ -2,15 +2,88 @@ package afkt.project.ui.activity
 
 import afkt.project.R
 import afkt.project.base.app.BaseActivity
-import androidx.viewbinding.ViewBinding
+import afkt.project.databinding.ActivityBottomSheetBinding
+import android.view.View
+import android.widget.LinearLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import dev.engine.log.DevLogEngine
+import dev.utils.app.ViewUtils
 
 /**
  * detail: Material BottomSheet
  * @author Ttt
  */
-class BottomSheetActivity : BaseActivity<ViewBinding>() {
-
-    override fun isViewBinding(): Boolean = false
+class BottomSheetActivity : BaseActivity<ActivityBottomSheetBinding>() {
 
     override fun baseLayoutId(): Int = R.layout.activity_bottom_sheet
+
+    lateinit var behavior: BottomSheetBehavior<LinearLayout>
+
+    override fun initValue() {
+        super.initValue()
+        behavior = BottomSheetBehavior.from(binding.vidAbsSheetLinear)
+    }
+
+    override fun initListener() {
+        super.initListener()
+
+        behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(
+                bottomSheet: View,
+                newState: Int
+            ) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                        // 折叠状态, bottom sheets 只在底部显示一部分布局
+                        // 显示高度可以通过 app:behavior_peekHeight 设置
+                        DevLogEngine.getEngine().dTag(TAG, "STATE_COLLAPSED")
+
+                        ViewUtils.setVisibility(false, binding.vidAbsBgView)
+                    }
+                    BottomSheetBehavior.STATE_DRAGGING -> {
+                        // 过渡状态, 此时用户正在向上或者向下拖动 bottom sheet
+                        DevLogEngine.getEngine().dTag(TAG, "STATE_DRAGGING")
+
+                        ViewUtils.setVisibility(true, binding.vidAbsBgView)
+                    }
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                        // 完全展开的状态
+                        DevLogEngine.getEngine().dTag(TAG, "STATE_EXPANDED")
+
+                        ViewUtils.setVisibility(true, binding.vidAbsBgView)
+                    }
+                    BottomSheetBehavior.STATE_HIDDEN -> {
+                        // 隐藏状态, 默认是 false 可通过 app:behavior_hideable 属性设置是否能隐藏
+                        DevLogEngine.getEngine().dTag(TAG, "STATE_HIDDEN")
+
+                        ViewUtils.setVisibility(false, binding.vidAbsBgView)
+                    }
+                    BottomSheetBehavior.STATE_SETTLING -> {
+                        // 视图从脱离手指自由滑动到最终停下的这一小段时间
+                        DevLogEngine.getEngine().dTag(TAG, "STATE_SETTLING")
+                    }
+                }
+            }
+
+            override fun onSlide(
+                bottomSheet: View,
+                slideOffset: Float
+            ) {
+            }
+        })
+
+//        // 手动设置状态
+//        // 展开
+//        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+//        // 折叠
+//        behavior.state = BottomSheetBehavior.STATE_COLLAPSED
+//        // 隐藏
+//        behavior.state = BottomSheetBehavior.STATE_HIDDEN
+
+//        // Bottom Sheet 是否允许隐藏
+//        // xml 设置
+//        app:behavior_hideable="false"
+//        // 代码设置
+//        behavior.isHideable = false
+    }
 }
