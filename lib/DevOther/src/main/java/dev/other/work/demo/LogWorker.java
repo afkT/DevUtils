fe16ea60f01125c952d4d3fcb97c4082f4e71a5e
Cjt <jtongttt@gmail.com>
2021-02-17 01:56:20 +0800

    1.提交部分代码
    Former-commit-id: 80939bbc7b5ac3feda243837eb1df0989d43142a
    Former-commit-id: 2843a943c60e66d412f71ff42080abb7be60b71d

--- lib/DevOther/src/main/java/dev/other/work/demo/LogWorker.java
+++ lib/DevOther/src/main/java/dev/other/work/demo/LogWorker.java
@@ -3,10 +3,13 @@ package dev.other.work.demo;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
+import androidx.work.Data;
+import androidx.work.OneTimeWorkRequest;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
 import dev.engine.log.DevLogEngine;
+import dev.utils.DevFinal;
 
 /**
  * detail: 打印日志 Worker ( 用于演示 )
@@ -39,12 +42,55 @@ public class LogWorker
 
         // 表示执行成功
         return Result.success();
+
+//        // 也可以输出数据
+//        return Result.success(
+//                createInputData("xx")
+//        );
     }
 
     /**
      * 执行的代码
      */
     private void code() {
-        DevLogEngine.getEngine().dTag(TAG, "LogWorker - code");
+        DevLogEngine.getEngine().dTag(TAG, "LogWorker - code() => " + getLog());
+    }
+
+    // =============
+    // = InputData =
+    // =============
+
+    /**
+     * 创建 Data 用于 Worker 传输
+     * @param log 打印日志内容
+     * @return {@link Data}
+     */
+    private static Data createInputData(final String log) {
+        Data.Builder builder = new Data.Builder();
+        builder.putString(DevFinal.CONTENT, log);
+        return builder.build();
+    }
+
+    /**
+     * 获取传递的数据
+     * @return Log Content
+     */
+    private String getLog() {
+        return getInputData().getString(DevFinal.CONTENT);
+    }
+
+    // ===================
+    // = 快捷创建 Request =
+    // ===================
+
+    /**
+     * 快捷创建 {@link OneTimeWorkRequest.Builder}
+     * @param log 传递待打印的日志内容
+     * @return {@link OneTimeWorkRequest.Builder}
+     */
+    public static OneTimeWorkRequest.Builder builder(final String log) {
+        return new OneTimeWorkRequest.Builder(LogWorker.class)
+                .setInputData(createInputData(log))
+                .addTag(TAG);
     }
 }
