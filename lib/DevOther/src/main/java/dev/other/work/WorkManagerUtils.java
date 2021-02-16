32d6e13dca2c9c46341ba083e53d6276aac3434b
Cjt <jtongttt@gmail.com>
2021-02-16 22:22:46 +0800

    1.移动包名位置
    Former-commit-id: 28073801249d5afe7a52ac718c9a2af1caf372d8
    Former-commit-id: cdca77987a292af2f2125e1abb02ed1d86fac6cf

---
+++ lib/DevOther/src/main/java/dev/other/work/WorkManagerUtils.java
@@ -0,0 +1,23 @@
+package dev.other.work;
+
+/**
+ * detail: WorkManager 工具类
+ * @author Ttt
+ * <pre>
+ *     使用 WorkManager 调度任务
+ *     @see <a href="https://developer.android.google.cn/topic/libraries/architecture/workmanager"/>
+ *     @see <a href="https://developer.android.google.cn/topic/libraries/architecture/workmanager/basics"/>
+ *     WorkManager
+ *     @see <a href="https://developer.android.google.cn/jetpack/androidx/releases/work"/>
+ * </pre>
+ */
+public final class WorkManagerUtils {
+
+    private WorkManagerUtils() {
+    }
+
+    // 日志 TAG
+    private static final String TAG = WorkManagerUtils.class.getSimpleName();
+
+
+}
