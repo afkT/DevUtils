a4fcf0f5bc3b5492474218808f95b7f5ca6826fa
Cjt <jtongttt@gmail.com>
2021-02-27 15:34:32 +0800

    1.更新部分代码
    Former-commit-id: f6d69c5816ec31a0dbb3a8c8765d90987b3c107d
    Former-commit-id: b1ff9ae08b6fa308c1f9d04dfd002840a2053804

--- lib/DevApp/src/main/java/dev/utils/common/BigDecimalUtils.java
+++ lib/DevApp/src/main/java/dev/utils/common/BigDecimalUtils.java
@@ -1171,6 +1171,52 @@ public final class BigDecimalUtils {
     // =========
 
     /**
+     * detail: 配置信息
+     * @author Ttt
+     */
+    public static final class Config {
+
+        // 小数点位数
+        private final int mScale;
+        // 舍入模式
+        private final int mRoundingMode;
+
+        public Config() {
+            this(10, BigDecimal.ROUND_DOWN);
+        }
+
+        /**
+         * 初始化小数点保留位数、舍入模式
+         * @param scale        小数点保留位数
+         * @param roundingMode 舍入模式
+         * @return {@link Operation}
+         */
+        public Config(
+                final int scale,
+                final int roundingMode
+        ) {
+            this.mScale = scale;
+            this.mRoundingMode = roundingMode;
+        }
+
+        /**
+         * 获取小数点保留位数
+         * @return 小数点保留位数
+         */
+        public int getScale() {
+            return mScale;
+        }
+
+        /**
+         * 获取舍入模式
+         * @return 舍入模式
+         */
+        public int getRoundingMode() {
+            return mRoundingMode;
+        }
+    }
+
+    /**
      * detail: BigDecimal 操作包装类
      * @author Ttt
      */
@@ -1240,27 +1286,28 @@ public final class BigDecimalUtils {
         }
 
         /**
-         * @param mConfig
-         * @return
+         * 设置配置信息
+         * @param config {@link Config}
+         * @return {@link Operation}
          */
-        public Operation setConfig(final Config mConfig) {
-            this.mConfig = mConfig;
+        public Operation setConfig(final Config config) {
+            this.mConfig = config;
             return this;
         }
 
-        // ===========
-        // = 获取方法 =
-        // ===========
-
-        // ===========
-        // = 内部方法 =
-        // ===========
+        /**
+         * 移除配置信息
+         * @return {@link Operation}
+         */
+        public Operation removeConfig() {
+            return setConfig(null);
+        }
 
         /**
          * 设置小数点保留位数、舍入模式
          * @return {@link Operation}
          */
-        public Operation set() {
+        public Operation setConfigScale() {
             if (mConfig != null && mValue != null) {
                 try {
                     mValue = mValue.setScale(
@@ -1268,56 +1315,14 @@ public final class BigDecimalUtils {
                             mConfig.getRoundingMode()
                     );
                 } catch (Exception e) {
-                    JCLogUtils.eTag(TAG, e, "set");
+                    JCLogUtils.eTag(TAG, e, "setConfigScale");
                 }
             }
             return this;
         }
-    }
 
-    /**
-     * detail: 配置信息
-     * @author Ttt
-     */
-    public static final class Config {
-
-        // 小数点位数
-        private final int mScale;
-        // 舍入模式
-        private final int mRoundingMode;
-
-        public Config() {
-            this(10, BigDecimal.ROUND_DOWN);
-        }
-
-        /**
-         * 初始化小数点保留位数、舍入模式
-         * @param scale        小数点保留位数
-         * @param roundingMode 舍入模式
-         * @return {@link Operation}
-         */
-        public Config(
-                final int scale,
-                final int roundingMode
-        ) {
-            this.mScale = scale;
-            this.mRoundingMode = roundingMode;
-        }
-
-        /**
-         * 获取小数点保留位数
-         * @return 小数点保留位数
-         */
-        public int getScale() {
-            return mScale;
-        }
-
-        /**
-         * 获取舍入模式
-         * @return 舍入模式
-         */
-        public int getRoundingMode() {
-            return mRoundingMode;
-        }
+        // ===========
+        // = 获取方法 =
+        // ===========
     }
 }
