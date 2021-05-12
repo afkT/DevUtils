d201fa03cd42f5fe368d74ca6a530ac01e7d3a0d
Cjt <jtongttt@gmail.com>
2021-05-12 13:57:46 +0800

    1.新增 AutoDispose
    Former-commit-id: d26e3a23a7e5fbad474e1fbc5a5b6dd8a55b5e78
    Former-commit-id: 42232535b20dc78effc4f4155712bea4559bf20d

--- app/src/main/java/afkt/project/framework/mvvm/ArticleRepository.kt
+++ app/src/main/java/afkt/project/framework/mvvm/ArticleRepository.kt
@@ -2,10 +2,11 @@ package afkt.project.framework.mvvm
 
 import afkt.project.function.http.RetrofitUtils
 import afkt.project.model.bean.ArticleBean
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
-import com.trello.lifecycle4.android.lifecycle.AndroidLifecycle
-import com.trello.rxlifecycle4.LifecycleProvider
+import autodispose2.androidx.lifecycle.AndroidLifecycleScopeProvider
+import autodispose2.autoDispose
 import ktx.dev.other.retrofit_rxjava.RxJavaManager
 import ktx.dev.other.retrofit_rxjava.subscriber.BaseBeanSubscriber
 
@@ -21,9 +22,14 @@ class ArticleRepository {
         owner: LifecycleOwner,
         article: MutableLiveData<ArticleBean?>
     ): MutableLiveData<ArticleBean?> {
-        val provider: LifecycleProvider<*> = AndroidLifecycle.createLifecycleProvider(owner)
+        // AutoDispose
+        val provider = AndroidLifecycleScopeProvider.from(owner, Lifecycle.Event.ON_DESTROY)
+
+//        // RxLifecycle
+//        val provider: LifecycleProvider<*> = AndroidLifecycle.createLifecycleProvider(owner)
+
         /**
-         * 通过 RxLifecycle 则无需手动进行 RxJava 管理
+         * 通过 RxLifecycle、AutoDispose 则无需手动进行 RxJava 管理
          * // 通过 tag 将请求添加到统一管理对象中
          * RxJavaManager.instance.add(TAG, disposable)
          * // 取消 TAG ( Activity ) 关联的请求
@@ -31,7 +37,10 @@ class ArticleRepository {
          */
         RetrofitUtils.instance.wanAndroidService().getArticleList(0)
             .compose(RxJavaManager.instance.io_main())
-            .compose(provider.bindToLifecycle<ArticleBean?>())
+            // AutoDispose
+            .autoDispose(provider)
+//            // RxLifecycle
+//            .compose(provider.bindToLifecycle<ArticleBean?>())
             .subscribeWith(object : BaseBeanSubscriber<ArticleBean>() {
                 override fun onSuccessResponse(data: ArticleBean) {
                     article.postValue(data)
