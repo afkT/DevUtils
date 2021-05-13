10451a3e3a0869cd5f0af3956afc5debce96f344
Cjt <jtongttt@gmail.com>
2021-05-13 16:54:16 +0800

    1.提交部分 Retrofit Coroutines 代码
    Former-commit-id: 2e4ada9c4c643a6df76c7c017501c4ea4277e480
    Former-commit-id: 7019daaa60d244c062cfcd13fb0c2ec50855c02d

--- lib/DevOther/src/main/java/ktx/dev/other/retrofit_coroutines/demo/RetrofitCoroutinesDemo.kt
+++ lib/DevOther/src/main/java/ktx/dev/other/retrofit_coroutines/demo/RetrofitCoroutinesDemo.kt
@@ -1,7 +1,9 @@
 package ktx.dev.other.retrofit_coroutines.demo
 
+import androidx.lifecycle.LiveData
+import dev.engine.log.DevLogEngine
 import ktx.dev.other.http.HttpLoggingInterceptor
-import ktx.dev.other.retrofit_coroutines.BaseResponse
+import ktx.dev.other.retrofit_coroutines.*
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -71,12 +73,70 @@ object RetrofitManager {
 // 日志 TAG
 const val TAG_L = "RetrofitCoroutinesDemo"
 
-fun getArticleList(page: Int) {
-
+suspend fun getArticleList(page: Int) {
+    /**
+     * 请求方法统一放到 Module Repository 中
+     * 并通过 [LiveData.postValue] 进行通知
+     */
+    val errorMethod = errorMethodCommon()
+    // 映射各种 JSON 实体类
+    executeRequest(block = {
+        var bean = RetrofitManager.apiService.getArticleList(page)
+        if (isSuccess(bean?.errorCode)) {
+            DevLogEngine.getEngine()?.dTag(
+                TAG_L, "请求成功 data: ${bean?.data?.datas?.size}"
+            )
+        } else {
+            errorMethod.invoke(bean?.errorCode, Throwable(bean?.errorMsg))
+        }
+    }, start = {
+        DevLogEngine.getEngine()?.dTag(
+            TAG_L, "开始请求"
+        )
+    }, complete = {
+        DevLogEngine.getEngine()?.dTag(
+            TAG_L, "请求完成"
+        )
+    }, error = errorMethod)
 }
 
-fun getArticleList2(page: Int) {
+suspend fun getArticleList2(page: Int) {
+    /**
+     * 请求方法统一放到 Module Repository 中
+     * 并通过 [LiveData.postValue] 进行通知
+     */
+    val errorMethod = errorMethodCommon()
+    // 映射统一标准 JSON 格式实体类 => BaseResponse
+    executeRequest(block = {
+        var result = RetrofitManager.apiService.getArticleList2(page).result()
+        if (result.success) {
+            DevLogEngine.getEngine()?.dTag(
+                TAG_L, "请求成功 data: ${result.data?.datas?.size}"
+            )
+        } else {
+            errorMethod.invoke(result.errorCode, result.error)
+        }
+    }, start = {
+        DevLogEngine.getEngine()?.dTag(
+            TAG_L, "开始请求"
+        )
+    }, complete = {
+        DevLogEngine.getEngine()?.dTag(
+            TAG_L, "请求完成"
+        )
+    }, error = errorMethod)
+}
 
+/**
+ * 通用错误回调方法 ( 方便复用 )
+ * @return (Throwable) -> Unit
+ */
+fun errorMethodCommon(): suspend (String?, Throwable) -> Unit {
+    return { code, error ->
+        DevLogEngine.getEngine()?.eTag(
+            TAG_L, error, "请求异常 code: ${code}, error: ${getErrorMessage(error)}"
+        )
+    }
 }
 
 // =========
@@ -94,13 +154,17 @@ class ArticleBean2 : ArticleBean.DataBean()
  * @author Ttt
  */
 class ArticleBean {
-    @JvmField
     var data: DataBean? = null
+
+    // 返回消息
+    var errorMsg: String? = null
+
+    // 返回结果状态 ( 内部定义 )
+    var errorCode: String? = null
 
     open class DataBean {
         var size = 0
 
-        @JvmField
         var datas: List<ListBean>? = null
 
         class ListBean {
--- lib/DevOther/src/main/java/ktx/dev/other/retrofit_coroutines/retrofit_http.kt
+++ lib/DevOther/src/main/java/ktx/dev/other/retrofit_coroutines/retrofit_http.kt
@@ -1,5 +1,11 @@
 package ktx.dev.other.retrofit_coroutines
 
+import android.text.TextUtils
+import dev.utils.app.toast.ToastUtils
+import java.net.ConnectException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+
 /**
  * detail: 请求响应统一解析类
  * @author Ttt
@@ -14,24 +20,25 @@ class BaseResponse<T> {
     var errorMsg: String? = null // message
 
     // 返回结果状态 ( 内部定义 )
-    var errorCode: Int = -1 // code
+    var errorCode: String? = null // code
 }
 
 /**
- * 请求结果
+ * detail: 请求结果包装类
+ * @author Ttt
  */
 class Result<T>(
     // 原始响应数据
-    var response: BaseResponse<T>,
+    val response: BaseResponse<T>,
     // 请求结果
-    var success: Boolean = false,
+    val success: Boolean = false,
     // 返回数据
-    var data: T? = null,
-) {
-
+    val data: T? = null,
+    // 返回结果状态 ( 内部定义 )
+    val errorCode: String? = null,
     // 异常情况
-    var error: Exception? = null
-}
+    val error: Exception
+)
 
 /**
  * 请求结果扩展函数
@@ -39,11 +46,10 @@ class Result<T>(
  * @return Result<T>
  */
 fun <T> BaseResponse<T>.result(): Result<T> {
-    return Result(this, true, data).apply {
-        if (!success) { // 请求失败
-            error = Exception(errorMsg)
-        }
-    }
+    return Result(
+        this, isSuccess(errorCode),
+        data, errorCode, Exception(errorMsg)
+    )
 }
 
 /**
@@ -57,28 +63,53 @@ suspend fun executeRequest(
     // 请求完成
     complete: suspend () -> Unit,
     // 请求失败
-    error: suspend (Throwable) -> Unit,
+    error: suspend (String?, Throwable) -> Unit,
 ) {
     start()
     try {
         block()
     } catch (e: Exception) {
-        error(e)
+        error(Int.MAX_VALUE.toString(), e)
     } finally {
         complete()
     }
 }
 
-interface Callback<T> {
+// ==========
+// = 快捷判断 =
+// ==========
 
-    fun onStart()
+/**
+ * 获取异常信息
+ * @param throwable [Throwable]
+ * @return error message
+ */
+fun getErrorMessage(throwable: Throwable?): String {
+    return if (throwable is SocketTimeoutException) {
+        "网络连接超时, 请检查您的网络状态, 稍后重试"
+    } else if (throwable is ConnectException) {
+        "网络连接异常, 请检查您的网络状态"
+    } else if (throwable is UnknownHostException) {
+        "网络异常, 请检查您的网络状态"
+    } else {
+        throwable?.message ?: "网络异常, 请检查您的网络状态"
+    }
+}
 
-    fun onSuccess(
-        data: T?,
-        result: Result<T>
-    )
-
-    fun onComplete()
-
-    fun onError(throwable: Throwable?)
+/**
+ * 通过 code 判断请求是否正确
+ * @param code Code
+ * @return `true` yes, `false` no
+ */
+fun isSuccess(code: String?): Boolean {
+    if (!TextUtils.isEmpty(code)) {
+        if ("0000" == code) { // 自行判断
+            return true
+        } else if ("0" == code) {
+            return true
+        } else if ("xxx" == code) {
+            ToastUtils.showShort("xxx")
+        }
+    }
+    return false
 }
