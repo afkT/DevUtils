27f61d38f32d3f24b25bbc8f132604f191c3c7ba
Cjt <jtongttt@gmail.com>
2021-05-20 14:01:07 +0800

    1.新增 Analytics Engine 接口 track 方法
    Former-commit-id: a0f3b1a408e4999cee53323c67390043a8ba5e57
    Former-commit-id: d7a0366b27d1842354cbb2781f88448f7aae6702

--- lib/DevAssist/src/main/java/dev/engine/analytics/IAnalyticsEngine.java
+++ lib/DevAssist/src/main/java/dev/engine/analytics/IAnalyticsEngine.java
@@ -4,5 +4,19 @@ package dev.engine.analytics;
  * detail: Analytics Engine 接口
  * @author Ttt
  */
-public interface IAnalyticsEngine {
+public interface IAnalyticsEngine<Item extends IAnalyticsEngine.EngineItem> {
+
+    /**
+     * detail: Analytics Item
+     * @author Ttt
+     */
+    class EngineItem {
+    }
+
+    /**
+     * 数据统计 ( 埋点 ) 方法
+     * @param item {@link EngineItem}
+     * @return {@code true} success, {@code false} fail
+     */
+    boolean track(Item item);
 }
