70b78ec8cd15f3929dae3912ccd6c665ed6e082c
Cjt <jtongttt@gmail.com>
2021-07-11 22:57:39 +0800

    1.更新 DevDataList 代码
    Former-commit-id: 464d556935073b7a0bca82c4130f4da597220f40
    Former-commit-id: df705723812951946de1db688b2d8fcde3131c83

--- lib/DevAssist/src/main/java/dev/adapter/DevDataList.java
+++ lib/DevAssist/src/main/java/dev/adapter/DevDataList.java
@@ -4,34 +4,52 @@ import android.app.Activity;
 import android.content.Context;
 import android.view.ViewGroup;
 
-import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 import dev.assist.DataAssist;
+import dev.assist.EditTextWatcherAssist;
+import dev.base.DevObject;
+import dev.base.DevPage;
 import dev.base.data.DataChanged;
 import dev.base.data.DataManager;
+import dev.base.expand.RequestState;
+import dev.base.multiselect.DevMultiSelectMap;
+import dev.callback.DevCallback;
+import dev.callback.DevItemClickCallback;
 
 /**
  * detail: DataManager List
  * @author Ttt
  * <pre>
- *     适用于不方便继承 {@link DevDataAdapter} 类, 但提供相同功能 DataManager
+ *     适用于不方便继承 {@link DevDataAdapter} 及扩展类, 但提供相同功能 DataManager
  * </pre>
  */
-public abstract class DevDataList<T, VH extends RecyclerView.ViewHolder>
-        extends RecyclerView.Adapter<VH>
+public abstract class DevDataList<T>
         implements DataManager<T>,
         DataChanged<T> {
 
     // 数据辅助类
-    protected DataAssist<T> mAssist = new DataAssist<>(this);
+    protected DataAssist<T>                mAssist = new DataAssist<>(this);
     // Context
-    protected Context       mContext;
+    protected Context                      mContext;
     // Activity
-    protected Activity      mActivity;
+    protected Activity                     mActivity;
+    // 通用 Object
+    protected DevObject<T>                 mObject = new DevObject<>();
+    // Page 实体类
+    protected DevPage<T>                   mPage;
+    // 通用回调
+    protected DevCallback<T>               mCallback;
+    // 通用 Item Click 回调
+    protected DevItemClickCallback<T>      mItemCallback;
+    // 请求状态
+    protected RequestState<T>              mState;
+    // EditText 输入监听辅助类
+    protected EditTextWatcherAssist<T>     mTextWatcherAssist;
+    // 多选辅助类
+    protected DevMultiSelectMap<String, T> mMultiSelectMap;
 
     public DevDataList() {
     }
@@ -43,6 +61,10 @@ public abstract class DevDataList<T, VH extends RecyclerView.ViewHolder>
     public DevDataList(Activity activity) {
         this.mActivity = activity;
     }
+
+    // ==================
+    // = DevDataAdapter =
+    // ==================
 
     // ===========
     // = get/set =
@@ -61,7 +83,7 @@ public abstract class DevDataList<T, VH extends RecyclerView.ViewHolder>
      * @param context {@link Context}
      * @return {@link DevDataList}
      */
-    public DevDataList<T, VH> setContext(final Context context) {
+    public DevDataList<T> setContext(final Context context) {
         this.mContext = context;
         return this;
     }
@@ -79,7 +101,7 @@ public abstract class DevDataList<T, VH extends RecyclerView.ViewHolder>
      * @param activity {@link Activity}
      * @return {@link DevDataList}
      */
-    public DevDataList<T, VH> setActivity(Activity activity) {
+    public DevDataList<T> setActivity(Activity activity) {
         this.mActivity = activity;
         return this;
     }
@@ -87,12 +109,12 @@ public abstract class DevDataList<T, VH extends RecyclerView.ViewHolder>
     /**
      * 通过 ViewGroup 设置 Context
      * <pre>
-     *     在 {@link #onCreateViewHolder(ViewGroup, int)} 中调用, 传入 ViewGroup
+     *     在 {@link androidx.recyclerview.widget.RecyclerView.Adapter#onCreateViewHolder(ViewGroup, int)} 中调用, 传入 ViewGroup
      * </pre>
      * @param parent {@link ViewGroup}
      * @return {@link DevDataList}
      */
-    public DevDataList<T, VH> parentContext(ViewGroup parent) {
+    public DevDataList<T> parentContext(ViewGroup parent) {
         if (parent != null && mContext == null) {
             this.mContext = parent.getContext();
         }
@@ -103,7 +125,6 @@ public abstract class DevDataList<T, VH extends RecyclerView.ViewHolder>
     // = RecyclerView.Adapter =
     // ========================
 
-    @Override
     public int getItemCount() {
         return getDataSize();
     }
@@ -349,17 +370,155 @@ public abstract class DevDataList<T, VH extends RecyclerView.ViewHolder>
         return mAssist.setDataList(collection, notify);
     }
 
-    // ==========
-    // = 通知方法 =
-    // ==========
+    // =====================
+    // = DevDataAdapterExt =
+    // =====================
 
-    @Override
-    public void notifyDataChanged() {
-        notifyDataSetChanged();
+    /**
+     * 获取通用 Object
+     * @return {@link DevObject}
+     */
+    public DevObject<T> getObject() {
+        return mObject;
     }
 
-    @Override
-    public void notifyElementChanged(T value) {
-        notifyDataSetChanged();
+    /**
+     * 设置通用 Object
+     * @param object {@link DevObject}
+     * @return {@link DevDataList}
+     */
+    public DevDataList<T> setObject(final DevObject<T> object) {
+        this.mObject = object;
+        return this;
+    }
+
+    /**
+     * 获取 Page 实体类
+     * @return {@link DevPage}
+     */
+    public DevPage<T> getPage() {
+        return mPage;
+    }
+
+    /**
+     * 设置 Page 实体类
+     * @param pageConfig 页数配置信息
+     * @return {@link DevDataList}
+     */
+    public DevDataList<T> setPage(final DevPage.PageConfig pageConfig) {
+        return setPage(new DevPage<>(pageConfig));
+    }
+
+    /**
+     * 设置 Page 实体类
+     * @param page     页数
+     * @param pageSize 每页请求条数
+     * @return {@link DevDataList}
+     */
+    public DevDataList<T> setPage(
+            final int page,
+            final int pageSize
+    ) {
+        return setPage(new DevPage<>(page, pageSize));
+    }
+
+    /**
+     * 设置 Page 实体类
+     * @param page Page 实体类
+     * @return {@link DevDataList}
+     */
+    public DevDataList<T> setPage(final DevPage<T> page) {
+        this.mPage = page;
+        return this;
+    }
+
+    /**
+     * 获取通用回调
+     * @return {@link DevCallback}
+     */
+    public DevCallback<T> getCallback() {
+        return mCallback;
+    }
+
+    /**
+     * 设置通用回调
+     * @param callback {@link DevCallback}
+     * @return {@link DevDataList}
+     */
+    public DevDataList<T> setCallback(final DevCallback<T> callback) {
+        this.mCallback = callback;
+        return this;
+    }
+
+    /**
+     * 获取通用 Item Click 回调
+     * @return {@link DevItemClickCallback}
+     */
+    public DevItemClickCallback<T> getItemCallback() {
+        return mItemCallback;
+    }
+
+    /**
+     * 设置通用 Item Click 回调
+     * @param itemCallback {@link DevItemClickCallback}
+     * @return {@link DevDataList}
+     */
+    public DevDataList<T> setItemCallback(final DevItemClickCallback<T> itemCallback) {
+        this.mItemCallback = itemCallback;
+        return this;
+    }
+
+    /**
+     * 请求状态实体类
+     * @return {@link RequestState}
+     */
+    public RequestState<T> getState() {
+        return mState;
+    }
+
+    /**
+     * 设置请求状态实体类
+     * @param state {@link RequestState}
+     * @return {@link DevDataList}
+     */
+    public DevDataList<T> setState(final RequestState<T> state) {
+        this.mState = state;
+        return this;
+    }
+
+    /**
+     * 获取 EditText 输入监听辅助类
+     * @return {@link EditTextWatcherAssist}
+     */
+    public EditTextWatcherAssist<T> getTextWatcherAssist() {
+        return mTextWatcherAssist;
+    }
+
+    /**
+     * 设置 EditText 输入监听辅助类
+     * @param textWatcherAssist {@link EditTextWatcherAssist}
+     * @return {@link DevDataList}
+     */
+    public DevDataList<T> setTextWatcherAssist(final EditTextWatcherAssist<T> textWatcherAssist) {
+        this.mTextWatcherAssist = textWatcherAssist;
+        return this;
+    }
+
+    /**
+     * 获取多选辅助类
+     * @return {@link DevMultiSelectMap}
+     */
+    public DevMultiSelectMap<String, T> getMultiSelectMap() {
+        return mMultiSelectMap;
+    }
+
+    /**
+     * 设置多选辅助类
+     * @param multiSelectMap {@link DevMultiSelectMap}
+     * @return {@link DevDataList}
+     */
+    public DevDataList<T> setMultiSelectMap(final DevMultiSelectMap<String, T> multiSelectMap) {
+        this.mMultiSelectMap = multiSelectMap;
+        return this;
     }
 }
