1d37c8cee12a7ec9ed697d5e8f99b9625ad2e92d
Cjt <jtongttt@gmail.com>
2021-09-06 14:51:27 +0800

    1.更新 DevUtils 部分代码
    Former-commit-id: 5f6fb8a1ad49848e93b0cb397c66a715d9aa594f
    Former-commit-id: 96c02c5b47049d3e1c3f3a5cc6f4eb98cf9f9aa8

--- lib/DevApp/src/main/java/dev/utils/common/ForUtils.java
+++ lib/DevApp/src/main/java/dev/utils/common/ForUtils.java
@@ -86,5 +86,186 @@ public final class ForUtils {
     // = 基础类型 =
     // ==========
 
+    // =======
+    // = Int =
+    // =======
 
+    /**
+     * detail: 循环消费者
+     * @author Ttt
+     */
+    public interface IntConsumer {
+
+        /**
+         * 循环消费方法
+         * @param index 索引
+         * @param value 对应索引值
+         */
+        void accept(
+                int index,
+                int value
+        );
+    }
+
+    /**
+     * 循环可变数组
+     * @param action 循环消费对象
+     * @param args   参数
+     * @return {@code true} success, {@code false} fail
+     */
+    public static boolean forInts(
+            final IntConsumer action,
+            final int... args
+    ) {
+        return forInts(action, false, args);
+    }
+
+    /**
+     * 循环可变数组
+     * @param action      循环消费对象
+     * @param checkLength 是否检查长度
+     * @param args        参数
+     * @return {@code true} success, {@code false} fail
+     */
+    public static boolean forInts(
+            final IntConsumer action,
+            final boolean checkLength,
+            final int... args
+    ) {
+        if (action != null && args != null) {
+            int len = args.length;
+            // 是否需要判断长度
+            if (checkLength && len == 0) {
+                return false;
+            }
+            for (int i = 0; i < len; i++) {
+                int value = args[i];
+                action.accept(i, value);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    // ==========
+    // = Double =
+    // ==========
+
+    /**
+     * detail: 循环消费者
+     * @author Ttt
+     */
+    public interface DoubleConsumer {
+
+        /**
+         * 循环消费方法
+         * @param index 索引
+         * @param value 对应索引值
+         */
+        void accept(
+                int index,
+                double value
+        );
+    }
+
+    /**
+     * 循环可变数组
+     * @param action 循环消费对象
+     * @param args   参数
+     * @return {@code true} success, {@code false} fail
+     */
+    public static boolean forDoubles(
+            final DoubleConsumer action,
+            final double... args
+    ) {
+        return forDoubles(action, false, args);
+    }
+
+    /**
+     * 循环可变数组
+     * @param action      循环消费对象
+     * @param checkLength 是否检查长度
+     * @param args        参数
+     * @return {@code true} success, {@code false} fail
+     */
+    public static boolean forDoubles(
+            final DoubleConsumer action,
+            final boolean checkLength,
+            final double... args
+    ) {
+        if (action != null && args != null) {
+            int len = args.length;
+            // 是否需要判断长度
+            if (checkLength && len == 0) {
+                return false;
+            }
+            for (int i = 0; i < len; i++) {
+                double value = args[i];
+                action.accept(i, value);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    // ===========
+    // = Boolean =
+    // ===========
+
+    /**
+     * detail: 循环消费者
+     * @author Ttt
+     */
+    public interface BooleanConsumer {
+
+        /**
+         * 循环消费方法
+         * @param index 索引
+         * @param value 对应索引值
+         */
+        void accept(
+                int index,
+                boolean value
+        );
+    }
+
+    /**
+     * 循环可变数组
+     * @param action 循环消费对象
+     * @param args   参数
+     * @return {@code true} success, {@code false} fail
+     */
+    public static boolean forBooleans(
+            final BooleanConsumer action,
+            final boolean... args
+    ) {
+        return forBooleans(action, false, args);
+    }
+
+    /**
+     * 循环可变数组
+     * @param action      循环消费对象
+     * @param checkLength 是否检查长度
+     * @param args        参数
+     * @return {@code true} success, {@code false} fail
+     */
+    public static boolean forBooleans(
+            final BooleanConsumer action,
+            final boolean checkLength,
+            final boolean... args
+    ) {
+        if (action != null && args != null) {
+            int len = args.length;
+            // 是否需要判断长度
+            if (checkLength && len == 0) {
+                return false;
+            }
+            for (int i = 0; i < len; i++) {
+                boolean value = args[i];
+                action.accept(i, value);
+            }
+            return true;
+        }
+        return false;
+    }
 }
