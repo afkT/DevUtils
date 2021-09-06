2ae4f4fed97d99763f15147fa6a68a96ca89e8d6
Cjt <jtongttt@gmail.com>
2021-09-06 14:20:21 +0800

    1.更新 DevUtils 部分代码
    Former-commit-id: 912e23118b43cc52adff22e02efdcc6174755029
    Former-commit-id: 2e011a271e69468b2b410b3065a94784eb2c26ee

--- lib/DevApp/src/main/java/dev/utils/common/ForUtils.java
+++ lib/DevApp/src/main/java/dev/utils/common/ForUtils.java
@@ -13,45 +13,61 @@ public final class ForUtils {
     private static final String TAG = ForUtils.class.getSimpleName();
 
     /**
-     * detail: 回调方法
+     * detail: 循环消费者
      * @author Ttt
      */
-    public interface Callback<T> {
+    public interface Consumer<T> {
 
-        void callback(
+        /**
+         * 循环消费方法
+         * @param index 索引
+         * @param value 对应索引值
+         */
+        void accept(
                 int index,
                 T value
         );
     }
 
+    // ==========
+    // = 可变数组 =
+    // ==========
+
+    // ==========
+    // = 对象类型 =
+    // ==========
+
     /**
      * 循环可变数组
-     * @param callback 回调方法
-     * @param args     参数
-     * @param <T>      泛型
+     * @param action 循环消费对象
+     * @param args   参数
+     * @param <T>    泛型
      * @return {@code true} success, {@code false} fail
      */
     public static <T> boolean forArgs(
-            final Callback<T> callback,
+            final Consumer<T> action,
             final T... args
     ) {
-        return forArgs(callback, false, args);
+        return forArgs(action, false, args);
     }
 
     /**
      * 循环可变数组
-     * @param callback    回调方法
+     * <pre>
+     *     基础类型需要传入包装类型, 如 int 传入为 Integer 为泛型类型
+     * </pre>
+     * @param action      循环消费对象
      * @param checkLength 是否检查长度
      * @param args        参数
      * @param <T>         泛型
      * @return {@code true} success, {@code false} fail
      */
     public static <T> boolean forArgs(
-            final Callback<T> callback,
+            final Consumer<T> action,
             final boolean checkLength,
             final T... args
     ) {
-        if (callback != null && args != null) {
+        if (action != null && args != null) {
             int len = args.length;
             // 是否需要判断长度
             if (checkLength && len == 0) {
@@ -59,10 +75,16 @@ public final class ForUtils {
             }
             for (int i = 0; i < len; i++) {
                 T value = args[i];
-                callback.callback(i, value);
+                action.accept(i, value);
             }
             return true;
         }
         return false;
     }
+
+    // ==========
+    // = 基础类型 =
+    // ==========
+
+
 }
