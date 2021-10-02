684545d382a8c1d995c95a68bc9cfb371d91ed77
Cjt <jtongttt@gmail.com>
2021-10-02 18:30:54 +0800

    1.更新部分 DevHttpCaptureCompiler 代码
    Former-commit-id: 0cc2090c7a3dfe2371c6ad5aebe3e76ec3181414
    Former-commit-id: a00ee2cf034ab3cf8b6526e291893c5c9284c0fa

--- lib/HttpCapture/DevHttpCaptureCompilerRelease/src/main/java/dev/DevHttpCaptureCompiler.java
+++ lib/HttpCapture/DevHttpCaptureCompilerRelease/src/main/java/dev/DevHttpCaptureCompiler.java
@@ -1,5 +1,9 @@
 package dev;
 
+import android.content.Context;
+
+import dev.capture.UrlFunctionGet;
+
 /**
  * detail: OKHttp 抓包工具库
  * @author Ttt
@@ -9,6 +13,57 @@ public final class DevHttpCaptureCompiler {
     private DevHttpCaptureCompiler() {
     }
 
-    // 日志 TAG
-    public static final String TAG = DevHttpCaptureCompiler.class.getSimpleName();
+    // =============
+    // = 对外提供方法 =
+    // =============
+
+    /**
+     * 结束所有 Activity
+     */
+    public static void finishAllActivity() {
+    }
+
+    // ==========
+    // = 跳转方法 =
+    // ==========
+
+    /**
+     * 跳转 DevHttpCaptureMainActivity Activity
+     * @param context {@link Context}
+     * @return {@code true} success, {@code false} fail
+     */
+    public static boolean start(final Context context) {
+        return false;
+    }
+
+    /**
+     * 跳转 DevHttpCaptureMainActivity Activity
+     * @param context    {@link Context}
+     * @param moduleName 模块名 ( 要求唯一性 )
+     * @return {@code true} success, {@code false} fail
+     */
+    public static boolean start(
+            final Context context,
+            final String moduleName
+    ) {
+        return false;
+    }
+
+    /**
+     * 添加接口所属功能注释
+     * @param moduleName 模块名 ( 要求唯一性 )
+     * @param function   接口所属功能注释获取
+     */
+    public static void putUrlFunction(
+            final String moduleName,
+            final UrlFunctionGet function
+    ) {
+    }
+
+    /**
+     * 移除接口所属功能注释
+     * @param moduleName 模块名 ( 要求唯一性 )
+     */
+    public static void removeUrlFunction(final String moduleName) {
+    }
 }
---
+++ lib/HttpCapture/DevHttpCaptureCompilerRelease/src/main/java/dev/capture/UrlFunctionGet.java
@@ -0,0 +1,23 @@
+package dev.capture;
+
+/**
+ * detail: 接口所属功能注释获取
+ * @author Ttt
+ */
+public interface UrlFunctionGet {
+
+    /**
+     * 接口所属功能注释获取
+     * @param moduleName    模块名 ( 要求唯一性 )
+     * @param url           原始请求链接
+     * @param convertUrlKey url 匹配规则 ( 拆分 ? 前为 key 进行匹配 )
+     * @param method        请求方法
+     * @return 接口所属功能注释
+     */
+    String toUrlFunction(
+            String moduleName,
+            String url,
+            String convertUrlKey,
+            String method
+    );
+}
