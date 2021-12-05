546007c0ff549f0b6f03c068848ba5859cfa2db0
Cjt <jtongttt@gmail.com>
2021-12-05 23:20:04 +0800

    1.格式化代码
    Former-commit-id: 09255e72bf6db0131a4655d7287c6fc0be16b67b
    Former-commit-id: 28cbac242d93fe3e97cabeb88f8bae504bd19273

---
+++ lib/DevApp/src/main/java/dev/utils/app/WindowUtils.java
@@ -0,0 +1,14 @@
+package dev.utils.app;
+
+/**
+ * detail: Window 工具类
+ * @author Ttt
+ */
+public final class WindowUtils {
+
+    private WindowUtils() {
+    }
+
+    // 日志 TAG
+    private static final String TAG = WindowUtils.class.getSimpleName();
+}
