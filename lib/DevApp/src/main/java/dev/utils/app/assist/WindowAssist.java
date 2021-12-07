effba8fcfa03ac39a639ed95d26bcaadf1520a6a
Cjt <jtongttt@gmail.com>
2021-12-07 17:16:42 +0800

    1.更新 Window 相关工具类
    Former-commit-id: 6235f48a497a0a7b3aa1d6a929193d080caaf428
    Former-commit-id: 2efd8792bed6f2a3ad685cbe0dfdcf75aa2c1de1

--- lib/DevApp/src/main/java/dev/utils/app/assist/WindowAssist.java
+++ lib/DevApp/src/main/java/dev/utils/app/assist/WindowAssist.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.fragment.app.DialogFragment;
 
@@ -64,6 +65,64 @@ public final class WindowAssist {
     public static Window getWindow(final android.app.DialogFragment dialog) {
         if (dialog == null) return null;
         return getWindow(dialog.getDialog());
+    }
+
+    // =======
+    // = get =
+    // =======
+
+    /**
+     * 获取 WindowAssist
+     * @param window {@link Context}
+     * @return {@link WindowAssist}
+     */
+    public static WindowAssist get(final Window window) {
+        return new WindowAssist(window);
+    }
+
+    /**
+     * 获取 WindowAssist
+     * @param context {@link Context}
+     * @return {@link WindowAssist}
+     */
+    public static WindowAssist get(final Context context) {
+        return new WindowAssist(context);
+    }
+
+    /**
+     * 获取 WindowAssist
+     * @param activity {@link Activity}
+     * @return {@link WindowAssist}
+     */
+    public static WindowAssist get(final Activity activity) {
+        return new WindowAssist(activity);
+    }
+
+    /**
+     * 获取 WindowAssist
+     * @param dialog {@link Dialog}
+     * @return {@link WindowAssist}
+     */
+    public static WindowAssist get(final Dialog dialog) {
+        return new WindowAssist(dialog);
+    }
+
+    /**
+     * 获取 WindowAssist
+     * @param dialog {@link DialogFragment}
+     * @return {@link WindowAssist}
+     */
+    public static WindowAssist get(final DialogFragment dialog) {
+        return new WindowAssist(dialog);
+    }
+
+    /**
+     * 获取 WindowAssist
+     * @param dialog {@link android.app.DialogFragment}
+     * @return {@link WindowAssist}
+     */
+    public static WindowAssist get(final android.app.DialogFragment dialog) {
+        return new WindowAssist(dialog);
     }
 
     // ===================
@@ -135,5 +194,129 @@ public final class WindowAssist {
      */
     public Window getWindow() {
         return mWindow;
+    }
+
+    /**
+     * 获取 Window LayoutParams
+     * @return Window LayoutParams
+     */
+    public WindowManager.LayoutParams getAttributes() {
+        return getAttributes(mWindow);
+    }
+
+    /**
+     * 设置 Window LayoutParams
+     * @param params WindowManager.LayoutParams
+     * @return {@code true} success, {@code false} fail
+     */
+    public boolean setAttributes(final WindowManager.LayoutParams params) {
+        return setAttributes(mWindow, params);
+    }
+
+    /**
+     * 清除 Window flags
+     * @param flags 待清除 flags
+     * @return {@code true} success, {@code false} fail
+     */
+    public boolean clearFlags(final int flags) {
+        return clearFlags(mWindow, flags);
+    }
+
+    /**
+     * 添加 Window flags
+     * @param flags 待添加 flags
+     * @return {@code true} success, {@code false} fail
+     */
+    public boolean addFlags(final int flags) {
+        return addFlags(mWindow, flags);
+    }
+
+    /**
+     * 设置 Window flags
+     * @param flags 待设置 flags
+     * @param mask  待设置 flags 位
+     * @return {@code true} success, {@code false} fail
+     */
+    public boolean setFlags(
+            final int flags,
+            final int mask
+    ) {
+        return setFlags(mWindow, flags, mask);
+    }
+
+    // ==============
+    // = Window 传参 =
+    // ==============
+
+    /**
+     * 获取 Window LayoutParams
+     * @param window {@link Window}
+     * @return Window LayoutParams
+     */
+    public WindowManager.LayoutParams getAttributes(final Window window) {
+        if (window == null) return null;
+        return window.getAttributes();
+    }
+
+    /**
+     * 设置 Window LayoutParams
+     * @param window {@link Window}
+     * @param params WindowManager.LayoutParams
+     * @return {@code true} success, {@code false} fail
+     */
+    public boolean setAttributes(
+            final Window window,
+            final WindowManager.LayoutParams params
+    ) {
+        if (window == null || params == null) return false;
+        window.setAttributes(params);
+        return true;
+    }
+
+    /**
+     * 清除 Window flags
+     * @param window {@link Window}
+     * @param flags  待清除 flags
+     * @return {@code true} success, {@code false} fail
+     */
+    public boolean clearFlags(
+            final Window window,
+            final int flags
+    ) {
+        if (window == null) return false;
+        window.clearFlags(flags);
+        return true;
+    }
+
+    /**
+     * 添加 Window flags
+     * @param window {@link Window}
+     * @param flags  待添加 flags
+     * @return {@code true} success, {@code false} fail
+     */
+    public boolean addFlags(
+            final Window window,
+            final int flags
+    ) {
+        if (window == null) return false;
+        window.addFlags(flags);
+        return true;
+    }
+
+    /**
+     * 设置 Window flags
+     * @param window {@link Window}
+     * @param flags  待设置 flags
+     * @param mask   待设置 flags 位
+     * @return {@code true} success, {@code false} fail
+     */
+    public boolean setFlags(
+            final Window window,
+            final int flags,
+            final int mask
+    ) {
+        if (window == null) return false;
+        window.setFlags(flags, mask);
+        return true;
     }
 }
