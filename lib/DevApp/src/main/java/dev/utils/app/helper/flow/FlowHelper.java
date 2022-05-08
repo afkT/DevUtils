ff7e54a3be4f2c5f4a8ba187700ac14091254a51
Cjt <jtongttt@gmail.com>
2022-05-08 19:27:20 +0800

    feat(DevApp): 新增 FlowHelper 类
    Former-commit-id: f454309771747b6a1b1fee0bbe7cd3be5095ade5
    Former-commit-id: 0589c6dabc02cbcd80ac7662fa17659646ecce66

---
+++ lib/DevApp/src/main/java/dev/utils/app/helper/flow/FlowHelper.java
@@ -0,0 +1,151 @@
+package dev.utils.app.helper.flow;
+
+import android.view.View;
+
+import dev.utils.app.HandlerUtils;
+import dev.utils.app.helper.dev.DevHelper;
+import dev.utils.app.helper.quick.QuickHelper;
+import dev.utils.app.helper.view.ViewHelper;
+
+/**
+ * detail: 流式 ( 链式 ) 连接 Helper 类
+ * @author Ttt
+ * <pre>
+ *     支持连接 {@link DevHelper}、{@link QuickHelper}、{@link ViewHelper} 等类使用
+ *     且不局限于上述类, 只需要返回 {@link FlowHelper}、{@link FlowHelper.Action} 即可使用
+ * </pre>
+ */
+public final class FlowHelper
+        implements IHelperByFlow<FlowHelper> {
+
+    private FlowHelper() {
+    }
+
+    // FlowHelper
+    private static final FlowHelper HELPER = new FlowHelper();
+
+    /**
+     * 获取单例 FlowHelper
+     * @return {@link FlowHelper}
+     */
+    public static FlowHelper get() {
+        return HELPER;
+    }
+
+    public interface Action {
+
+    }
+
+    // ===========
+    // = IHelper =
+    // ===========
+
+    /**
+     * 获取 DevHelper
+     * @return {@link DevHelper}
+     */
+    @Override
+    public DevHelper devHelper() {
+        return DevHelper.get();
+    }
+
+    /**
+     * 获取 QuickHelper
+     * @param target 目标 View
+     * @return {@link QuickHelper}
+     */
+    @Override
+    public QuickHelper quickHelper(View target) {
+        return QuickHelper.get(target);
+    }
+
+    /**
+     * 获取 ViewHelper
+     * @return {@link ViewHelper}
+     */
+    @Override
+    public ViewHelper viewHelper() {
+        return ViewHelper.get();
+    }
+
+    // ================
+    // = HandlerUtils =
+    // ================
+
+    /**
+     * 在主线程 Handler 中执行任务
+     * @param runnable 可执行的任务
+     * @return Helper
+     */
+    @Override
+    public FlowHelper postRunnable(Runnable runnable) {
+        HandlerUtils.postRunnable(runnable);
+        return this;
+    }
+
+    /**
+     * 在主线程 Handler 中执行延迟任务
+     * @param runnable    可执行的任务
+     * @param delayMillis 延迟时间
+     * @return Helper
+     */
+    @Override
+    public FlowHelper postRunnable(
+            Runnable runnable,
+            long delayMillis
+    ) {
+        HandlerUtils.postRunnable(runnable, delayMillis);
+        return this;
+    }
+
+    /**
+     * 在主线程 Handler 中执行延迟任务
+     * @param runnable    可执行的任务
+     * @param delayMillis 延迟时间
+     * @param number      轮询次数
+     * @param interval    轮询时间
+     * @return Helper
+     */
+    @Override
+    public FlowHelper postRunnable(
+            Runnable runnable,
+            long delayMillis,
+            int number,
+            int interval
+    ) {
+        HandlerUtils.postRunnable(runnable, delayMillis, number, interval);
+        return this;
+    }
+
+    /**
+     * 在主线程 Handler 中执行延迟任务
+     * @param runnable    可执行的任务
+     * @param delayMillis 延迟时间
+     * @param number      轮询次数
+     * @param interval    轮询时间
+     * @param listener    结束通知
+     * @return Helper
+     */
+    @Override
+    public FlowHelper postRunnable(
+            Runnable runnable,
+            long delayMillis,
+            int number,
+            int interval,
+            HandlerUtils.OnEndListener listener
+    ) {
+        HandlerUtils.postRunnable(runnable, delayMillis, number, interval, listener);
+        return this;
+    }
+
+    /**
+     * 在主线程 Handler 中清除任务
+     * @param runnable 需要清除的任务
+     * @return Helper
+     */
+    @Override
+    public FlowHelper removeRunnable(Runnable runnable) {
+        HandlerUtils.removeRunnable(runnable);
+        return this;
+    }
+}
---
+++ lib/DevApp/src/main/java/dev/utils/app/helper/flow/IHelperByFlow.java
@@ -0,0 +1,11 @@
+package dev.utils.app.helper.flow;
+
+import dev.utils.app.helper.IHelper;
+
+/**
+ * detail: FlowHelper 接口
+ * @author Ttt
+ */
+public interface IHelperByFlow<T>
+        extends IHelper<T> {
+}
