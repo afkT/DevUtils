dccc7f6aed0637d844a45bb500939a7713971f60
Cjt <jtongttt@gmail.com>
2022-05-24 15:21:47 +0800

    feat: 全面更新 fastjson 为 fastjson2 及对应变更代码
    Former-commit-id: a6e3e0ee72dee0a4367134482c3c30361875c2dc
    Former-commit-id: eb201fad4a1be08239f80800c9a61d313b9b4c2f

--- lib/DevEngine/src/main/java/dev/engine/json/FastjsonUtils.kt
+++ lib/DevEngine/src/main/java/dev/engine/json/FastjsonUtils.kt
@@ -1,11 +1,10 @@
 package dev.engine.json
 
-import com.alibaba.fastjson.JSON
-import com.alibaba.fastjson.JSONArray
-import com.alibaba.fastjson.JSONObject
-import com.alibaba.fastjson.parser.Feature
-import com.alibaba.fastjson.serializer.SerializerFeature
-import com.alibaba.fastjson.util.ParameterizedTypeImpl
+import com.alibaba.fastjson2.JSON
+import com.alibaba.fastjson2.JSONArray
+import com.alibaba.fastjson2.JSONObject
+import com.alibaba.fastjson2.JSONWriter
+import com.alibaba.fastjson2.util.ParameterizedTypeImpl
 import dev.utils.JCLogUtils
 import java.lang.reflect.GenericArrayType
 import java.lang.reflect.Type
@@ -140,12 +139,10 @@ internal object FastjsonUtils {
         if (json != null) {
             try {
                 // 保持 JSON 字符串次序
-                val obj = JSON.parse(json, Feature.OrderedField)
-                if (obj is JSONObject) {
-                    return JSONObject.toJSONString(obj, SerializerFeature.PrettyFormat)
-                } else if (obj is JSONArray) {
-                    return JSONArray.toJSONString(obj, SerializerFeature.PrettyFormat)
-                }
+                val obj = JSON.parse(json)
+                return JSON.toJSONString(
+                    obj, JSONWriter.Feature.PrettyFormat
+                )
             } catch (e: java.lang.Exception) {
                 JCLogUtils.eTag(TAG, e, "toJsonIndent")
             }
--- lib/DevOther/src/main/java/java/dev/other/FastjsonUtils.java
+++ lib/DevOther/src/main/java/java/dev/other/FastjsonUtils.java
@@ -1,11 +1,10 @@
 package java.dev.other;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.parser.Feature;
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.util.ParameterizedTypeImpl;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONWriter;
+import com.alibaba.fastjson2.util.ParameterizedTypeImpl;
 
 import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.Type;
@@ -150,12 +149,10 @@ public final class FastjsonUtils {
         if (json != null) {
             try {
                 // 保持 JSON 字符串次序
-                Object object = JSON.parse(json, Feature.OrderedField);
-                if (object instanceof JSONObject) {
-                    return JSONObject.toJSONString(object, SerializerFeature.PrettyFormat);
-                } else if (object instanceof JSONArray) {
-                    return JSONArray.toJSONString(object, SerializerFeature.PrettyFormat);
-                }
+                Object object = JSON.parse(json);
+                return JSON.toJSONString(
+                        object, JSONWriter.Feature.PrettyFormat
+                );
             } catch (Exception e) {
                 JCLogUtils.eTag(TAG, e, "toJsonIndent");
             }
