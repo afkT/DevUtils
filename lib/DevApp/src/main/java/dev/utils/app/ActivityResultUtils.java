15f614eca5e2f718d9d74de4ce022a947e0a5223
Cjt <jtongttt@gmail.com>
2022-05-26 14:16:56 +0800

    feat: 新增 ActivityResultUtils#register 方法
    Former-commit-id: 483040ac40f65c19ef021f3b0203c72d0695e1e9
    Former-commit-id: 00d663933bb9ea038e25fd6d100c61fb699f2214

--- lib/DevApp/src/main/java/dev/utils/app/ActivityResultUtils.java
+++ lib/DevApp/src/main/java/dev/utils/app/ActivityResultUtils.java
@@ -6,6 +6,7 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.ActivityResultRegistry;
 import androidx.activity.result.contract.ActivityResultContract;
 import androidx.core.app.ActivityOptionsCompat;
+import androidx.lifecycle.LifecycleOwner;
 
 import dev.utils.LogPrintUtils;
 import dev.utils.app.activity_result.DefaultActivityResult;
@@ -14,14 +15,16 @@ import dev.utils.app.activity_result.DefaultActivityResult;
  * detail: Activity Result 工具类
  * @author Ttt
  * <pre>
- *     可不封装, 只是为了拆分原始 onActivityResult 实现方式
- *     以及新的 ActivityResult API 实现方式
+ *     只是为了拆分原始 onActivityResult 实现方式以及新的 ActivityResult API 实现方式
+ *     可不封装 ( 内部捕获异常并返回操作是否成功 )
  *     <p></p>
  *     Activity Result API
  *     @see <a href="https://developer.android.google.cn/training/basics/intents/result"/>
  *     <p></p>
  *     关于 registerForActivityResult 创建的 ActivityResultLauncher 可以看
  *     {@link androidx.activity.result.ActivityResultRegistry#register}
+ *     正常在 Activity 中使用顺序是
+ *     registerForActivityResult().launch(input)
  * </pre>
  */
 public final class ActivityResultUtils {
@@ -125,7 +128,11 @@ public final class ActivityResultUtils {
      */
     public static <I> ActivityResultContract<I, ?> getContract(final ActivityResultLauncher<I> launcher) {
         if (launcher != null) {
-            return launcher.getContract();
+            try {
+                return launcher.getContract();
+            } catch (Exception e) {
+                LogPrintUtils.eTag(TAG, e, "getContract");
+            }
         }
         return null;
     }
@@ -136,7 +143,7 @@ public final class ActivityResultUtils {
 
     /**
      * 注册创建跳转回传值启动器并返回
-     * @param caller   ActivityResultCaller
+     * @param caller   ActivityResultCaller ( 只要属于继承 Fragment、FragmentActivity 传入 this 即可 )
      * @param contract ActivityResultContract
      * @param callback ActivityResultCallback 回传回调
      * @param <I>      启动所需输入参数类型
@@ -152,7 +159,9 @@ public final class ActivityResultUtils {
             try {
                 return caller.registerForActivityResult(contract, callback);
             } catch (Exception e) {
-                LogPrintUtils.eTag(TAG, e, "registerForActivityResult");
+                LogPrintUtils.eTag(
+                        TAG, e, "ActivityResultCaller registerForActivityResult"
+                );
             }
         }
         return null;
@@ -160,7 +169,7 @@ public final class ActivityResultUtils {
 
     /**
      * 注册创建跳转回传值启动器并返回
-     * @param caller   ActivityResultCaller
+     * @param caller   ActivityResultCaller ( 只要属于继承 Fragment、FragmentActivity 传入 this 即可 )
      * @param contract ActivityResultContract
      * @param registry ActivityResultRegistry
      * @param callback ActivityResultCallback 回传回调
@@ -174,11 +183,82 @@ public final class ActivityResultUtils {
             final ActivityResultRegistry registry,
             final ActivityResultCallback<O> callback
     ) {
-        if (caller != null && contract != null && callback != null && registry != null) {
+        if (caller != null && contract != null && registry != null && callback != null) {
             try {
                 return caller.registerForActivityResult(contract, registry, callback);
             } catch (Exception e) {
-                LogPrintUtils.eTag(TAG, e, "registerForActivityResult");
+                LogPrintUtils.eTag(
+                        TAG, e, "ActivityResultCaller registerForActivityResult"
+                );
+            }
+        }
+        return null;
+    }
+
+    // ==========================
+    // = ActivityResultRegistry =
+    // ==========================
+
+    /**
+     * 注册创建跳转回传值启动器并返回
+     * <pre>
+     *     ActivityResultRegistry ( 可通过 ComponentActivity、Fragment getActivityResultRegistry() 获取 )
+     * </pre>
+     * @param registry       ActivityResultRegistry
+     * @param key            唯一值字符串
+     * @param lifecycleOwner 生命周期监听
+     * @param contract       ActivityResultContract
+     * @param callback       ActivityResultCallback 回传回调
+     * @param <I>            启动所需输入参数类型
+     * @param <O>            回传结果解析值类型
+     * @return ActivityResultLauncher
+     */
+    public static <I, O> ActivityResultLauncher<I> register(
+            final ActivityResultRegistry registry,
+            final String key,
+            final LifecycleOwner lifecycleOwner,
+            final ActivityResultContract<I, O> contract,
+            final ActivityResultCallback<O> callback
+    ) {
+        if (registry != null && key != null && lifecycleOwner != null
+                && contract != null && callback != null) {
+            try {
+                return registry.register(key, lifecycleOwner, contract, callback);
+            } catch (Exception e) {
+                LogPrintUtils.eTag(
+                        TAG, e, "ActivityResultRegistry register"
+                );
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 注册创建跳转回传值启动器并返回
+     * <pre>
+     *     ActivityResultRegistry ( 可通过 ComponentActivity、Fragment getActivityResultRegistry() 获取 )
+     * </pre>
+     * @param registry ActivityResultRegistry
+     * @param key      唯一值字符串
+     * @param contract ActivityResultContract
+     * @param callback ActivityResultCallback 回传回调
+     * @param <I>      启动所需输入参数类型
+     * @param <O>      回传结果解析值类型
+     * @return ActivityResultLauncher
+     */
+    public static <I, O> ActivityResultLauncher<I> register(
+            final ActivityResultRegistry registry,
+            final String key,
+            final ActivityResultContract<I, O> contract,
+            final ActivityResultCallback<O> callback
+    ) {
+        if (registry != null && key != null && contract != null && callback != null) {
+            try {
+                return registry.register(key, contract, callback);
+            } catch (Exception e) {
+                LogPrintUtils.eTag(
+                        TAG, e, "ActivityResultRegistry register"
+                );
             }
         }
         return null;
---
+++ lib/DevApp/src/main/java/dev/utils/app/activity_result/ActivityResultAssist.java
@@ -0,0 +1,190 @@
+package dev.utils.app.activity_result;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import dev.DevUtils;
+import dev.utils.DevFinal;
+import dev.utils.LogPrintUtils;
+import dev.utils.app.AppUtils;
+import dev.utils.common.DevCommonUtils;
+
+/**
+ * detail: Activity Result 封装辅助类
+ * @author Ttt
+ * <pre>
+ *     默认 Activity onActivityResult(int, int, Intent) 实现方式封装
+ * </pre>
+ */
+public final class ActivityResultAssist {
+
+    private ActivityResultAssist() {
+    }
+
+    // DefaultActivityResult 实例
+    private static volatile ActivityResultAssist sInstance;
+
+    /**
+     * 获取 DefaultActivityResult 实例
+     * @return {@link ActivityResultAssist}
+     */
+    public static ActivityResultAssist getInstance() {
+        if (sInstance == null) {
+            synchronized (ActivityResultAssist.class) {
+                if (sInstance == null) {
+                    sInstance = new ActivityResultAssist();
+                }
+            }
+        }
+        return sInstance;
+    }
+
+    // =============
+    // = 对外公开方法 =
+    // =============
+
+    /**
+     * Activity 跳转回传
+     * @param callback Activity 跳转回传回调
+     * @return {@code true} success, {@code false} fail
+     */
+    public boolean startActivityForResult(final ResultCallback callback) {
+        return ActivityResultAssist.ResultActivity.start(callback);
+    }
+
+    // ==========
+    // = 跳转回传 =
+    // ==========
+
+    // 跳转回传回调 Map
+    private static final Map<Integer, ResultCallback> sResultCallbackMaps = new HashMap<>();
+
+    /**
+     * detail: Activity 跳转回传回调
+     * @author Ttt
+     */
+    public interface ResultCallback {
+
+        /**
+         * 跳转 Activity 操作
+         * <pre>
+         *     跳转失败, 必须返回 false 内部会根据返回值关闭 ResultActivity
+         *     必须返回正确的值, 表示是否跳转成功
+         * </pre>
+         * @param activity {@link Activity}
+         * @return {@code true} success, {@code false} fail
+         */
+        boolean onStartActivityForResult(Activity activity);
+
+        /**
+         * 回传处理
+         * @param result     resultCode 是否等于 {@link Activity#RESULT_OK}
+         * @param resultCode resultCode
+         * @param intent     回传数据
+         */
+        void onActivityResult(
+                boolean result,
+                int resultCode,
+                Intent intent
+        );
+    }
+
+    // =============
+    // = 内部封装逻辑 =
+    // =============
+
+    /**
+     * detail: 回传结果处理 Activity
+     * @author Ttt
+     */
+    public static class ResultActivity
+            extends FragmentActivity {
+
+        // 日志 TAG
+        private static final String TAG = ResultActivity.class.getSimpleName();
+
+        // 传参 UUID Key
+        private static final String         EXTRA_UUID = DevFinal.STR.UUID;
+        // 跳转回传回调
+        private              ResultCallback mCallback;
+        // 跳转回传回调
+        private              Integer        mUUIDHash;
+
+        /**
+         * 跳转回传结果处理 Activity 内部方法
+         * @param callback Activity 跳转回传回调
+         * @return {@code true} success, {@code false} fail
+         */
+        protected static boolean start(final ResultCallback callback) {
+            int     uuid   = -1;
+            boolean result = false;
+            if (callback != null) {
+                uuid = DevCommonUtils.randomUUIDToHashCode();
+                while (sResultCallbackMaps.containsKey(uuid)) {
+                    uuid = DevCommonUtils.randomUUIDToHashCode();
+                }
+                sResultCallbackMaps.put(uuid, callback);
+                try {
+                    Intent intent = new Intent(DevUtils.getContext(), ResultActivity.class);
+                    intent.putExtra(EXTRA_UUID, uuid);
+                    result = AppUtils.startActivity(intent);
+                } catch (Exception e) {
+                    LogPrintUtils.eTag(TAG, e, "start");
+                }
+            }
+            if (!result && uuid != -1) {
+                sResultCallbackMaps.remove(uuid);
+            }
+            return result;
+        }
+
+        @Override
+        protected void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            boolean result = false; // 跳转结果
+            try {
+                mUUIDHash = getIntent().getIntExtra(EXTRA_UUID, -1);
+                mCallback = sResultCallbackMaps.get(mUUIDHash);
+                result    = mCallback.onStartActivityForResult(this);
+            } catch (Exception e) {
+                LogPrintUtils.eTag(TAG, e, "onCreate");
+            }
+            if (!result) {
+                if (mCallback != null) {
+                    mCallback.onActivityResult(false, Activity.RESULT_CANCELED, null);
+                }
+                finish();
+            }
+        }
+
+        @Override
+        protected void onActivityResult(
+                int requestCode,
+                int resultCode,
+                Intent intent
+        ) {
+            super.onActivityResult(requestCode, resultCode, intent);
+            if (mCallback != null) {
+                mCallback.onActivityResult(
+                        resultCode == Activity.RESULT_OK,
+                        resultCode, intent
+                );
+            }
+            finish();
+        }
+
+        @Override
+        protected void onDestroy() {
+            super.onDestroy();
+            // 移除操作
+            sResultCallbackMaps.remove(mUUIDHash);
+        }
+    }
+}
