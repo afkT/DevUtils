a73260a96ee8e7bf274e4728743cbcf4be7a7ce3
Cjt <jtongttt@gmail.com>
2022-06-15 15:43:40 +0800

    demo: 新增 DevRetrofit Demo 使用代码
    Former-commit-id: b7a7b575b63019eee70ca4b8dd5f1478f03c96ae
    Former-commit-id: a7227e5652e5193d01395e8d1439ac1b1d7a0ccc

--- application/DevUtilsApp/src/main/java/afkt/project/use_demo/DevRetrofitCoroutinesDemo.kt
+++ application/DevUtilsApp/src/main/java/afkt/project/use_demo/DevRetrofitCoroutinesDemo.kt
@@ -1,270 +1,282 @@
 package afkt.project.use_demo
 
-///**
-// * detail: 玩 Android API Service
-// * @author Ttt
-// */
-//interface APIService {
-//
-//    @GET("/article/list/{page}/json")
-//    suspend fun getArticleList(@Path("page") page: Int): ArticleBean?
-//
-//    @GET("/article/list/{page}/json")
-//    suspend fun getArticleList2(@Path("page") page: Int): BaseResponse<ArticleBean2?>
-//
-////    http://www.jcodecraeer.com/a/anzhuokaifa/androidkaifa/2017/0601/8027.html
-////    https://blog.csdn.net/qq77485042/article/details/105681664
-//
-//    @POST("xxx")
-//    suspend fun uploadImage(@Body body: RequestBody): UploadBean
-//
-//    @POST("xxx")
-//    suspend fun uploadImage2(@Body body: RequestBody): BaseResponse<List<String?>?>
-//}
-//
-///**
-// * detail: Retrofit 请求 Demo
-// * @author Ttt
-// */
-//object RetrofitManager {
-//
-//    private val retrofit: Retrofit
-//
-//    val apiService: APIService
-//
-//    init {
-//        // ====================
-//        // = OkHttpClient 配置 =
-//        // ====================
-//
-//        val builder = OkHttpClient.Builder()
-//
-////        // 使用 DevHttpCapture 库进行 Http 拦截回调 ( 抓包数据存储 )
-////        DevHttpCapture.addInterceptor(builder, "ModuleName")
-//
-//        // 使用 DevHttpCapture 库进行 Http 拦截回调 ( 不进行抓包数据存储 )
-//        builder.addInterceptor(CallbackInterceptor { captureInfo ->
-//            LogPrintUtils.jsonTag(
-//                TAG_L, DevEngine.getJSON().toJson(captureInfo)
-//            )
-//        })
-//
-//        // 全局的读取超时时间
-//        builder.readTimeout(60000L, TimeUnit.MILLISECONDS)
-//        // 全局的写入超时时间
-//        builder.writeTimeout(60000L, TimeUnit.MILLISECONDS)
-//        // 全局的连接超时时间
-//        builder.connectTimeout(60000L, TimeUnit.MILLISECONDS)
-//
-//        // ================
-//        // = Retrofit 配置 =
-//        // ================
-//
-//        retrofit = Retrofit.Builder()
-//            // Gson 解析
-//            .addConverterFactory(GsonConverterFactory.create())
-//            // OkHttpClient
-//            .client(builder.build())
-//            // 服务器地址
-//            .baseUrl("https://www.wanandroid.com/")
-//            .build()
-//
-//        apiService = retrofit.create(APIService::class.java)
-//    }
-//}
-//
-//// ==========
-//// = 请求方法 =
-//// ==========
-//
-//// 日志 TAG
-//const val TAG_L = "RetrofitCoroutinesDemo"
-//
-//suspend fun getArticleList(page: Int) {
-//    /**
-//     * 请求方法统一放到 Module Repository 中
-//     * 并通过 [LiveData.postValue] 进行通知
-//     */
-//    val errorMethod = errorMethodCommon()
-//    // 映射各种 JSON 实体类
-//    executeRequest(block = {
-//        val bean = RetrofitManager.apiService.getArticleList(page)
-//        if (isSuccess(bean?.errorCode)) {
-//            DevEngine.getLog()?.dTag(
-//                TAG_L, "请求成功 data: ${bean?.data?.datas?.size}"
-//            )
-//        } else {
-//            errorMethod.invoke(bean?.errorCode, Throwable(bean?.errorMsg))
-//        }
-//    }, start = {
-//        DevEngine.getLog()?.dTag(
-//            TAG_L, "开始请求"
-//        )
-//    }, complete = {
-//        DevEngine.getLog()?.dTag(
-//            TAG_L, "请求完成"
-//        )
-//    }, error = errorMethod)
-//}
-//
-//suspend fun getArticleList2(page: Int) {
-//    /**
-//     * 请求方法统一放到 Module Repository 中
-//     * 并通过 [LiveData.postValue] 进行通知
-//     */
-//    val errorMethod = errorMethodCommon()
-//    // 映射统一标准 JSON 格式实体类 => BaseResponse
-//    executeRequest(block = {
-//        val result = RetrofitManager.apiService.getArticleList2(page).result()
-//        if (result.success) {
-//            DevEngine.getLog()?.dTag(
-//                TAG_L, "请求成功 data: ${result.data?.datas?.size}"
-//            )
-//        } else {
-//            errorMethod.invoke(result.errorCode, result.error)
-//        }
-//    }, start = {
-//        DevEngine.getLog()?.dTag(
-//            TAG_L, "开始请求"
-//        )
-//    }, complete = {
-//        DevEngine.getLog()?.dTag(
-//            TAG_L, "请求完成"
-//        )
-//    }, error = errorMethod)
-//}
-//
-//suspend fun uploadImage(lists: List<File>) {
-//    val formDataPart = MultipartBody.Builder()
-//        .setType(MultipartBody.FORM).apply {
-//            lists.forEach {
-//                addFormDataPart(
-//                    "images", it.name,
-//                    it.asRequestBody("image/*".toMediaTypeOrNull())
-//                )
-//            }
-//        }
-//    val requestBody = formDataPart.build()
-//
-//    /**
-//     * 请求方法统一放到 Module Repository 中
-//     * 并通过 [LiveData.postValue] 进行通知
-//     */
-//    val errorMethod = errorMethodCommon()
-//    // 映射各种 JSON 实体类
-//    executeRequest(block = {
-//        val bean = RetrofitManager.apiService.uploadImage(requestBody)
-//        if (isSuccess(bean.code)) {
-//            DevEngine.getLog()?.dTag(
-//                TAG_L, "请求成功 data: ${bean.data!!.toTypedArray().contentToString()}"
-//            )
-//        } else {
-//            errorMethod.invoke(bean.code, Throwable(bean.msg))
-//        }
-//    }, start = {
-//        DevEngine.getLog()?.dTag(
-//            TAG_L, "开始请求"
-//        )
-//    }, complete = {
-//        DevEngine.getLog()?.dTag(
-//            TAG_L, "请求完成"
-//        )
-//    }, error = errorMethod)
-//}
-//
-//suspend fun uploadImage2(lists: List<File>) {
-//    val formDataPart = MultipartBody.Builder()
-//        .setType(MultipartBody.FORM).apply {
-//            lists.forEach {
-//                addFormDataPart(
-//                    "images", it.name,
-//                    it.asRequestBody("image/*".toMediaTypeOrNull())
-//                )
-//            }
-//        }
-//    val requestBody = formDataPart.build()
-//
-//    /**
-//     * 请求方法统一放到 Module Repository 中
-//     * 并通过 [LiveData.postValue] 进行通知
-//     */
-//    val errorMethod = errorMethodCommon()
-//    // 映射统一标准 JSON 格式实体类 => BaseResponse
-//    executeRequest(block = {
-//        val result = RetrofitManager.apiService.uploadImage2(requestBody).result()
-//        if (result.success) {
-//            DevEngine.getLog()?.dTag(
-//                TAG_L, "请求成功 data: ${result.data!!.toTypedArray().contentToString()}"
-//            )
-//        } else {
-//            errorMethod.invoke(result.errorCode, result.error)
-//        }
-//    }, start = {
-//        DevEngine.getLog()?.dTag(
-//            TAG_L, "开始请求"
-//        )
-//    }, complete = {
-//        DevEngine.getLog()?.dTag(
-//            TAG_L, "请求完成"
-//        )
-//    }, error = errorMethod)
-//}
-//
-///**
-// * 通用错误回调方法 ( 方便复用 )
-// * @return (Throwable) -> Unit
-// */
-//fun errorMethodCommon(): suspend (String?, Throwable) -> Unit {
-//    return { code, error ->
-//        DevEngine.getLog()?.eTag(
-//            TAG_L, error, "请求异常 code: ${code}, error: ${getErrorMessage(error)}"
-//        )
-//    }
-//}
-//
-//class UploadBean(
-//    val code: String?,
-//    val `data`: List<String?>?,
-//    val msg: String?,
-//    val success: Boolean
-//)
-//
-//// ========
-//// = 实体类 =
-//// ========
-//
-///**
-// * detail: 文章信息实体类
-// * @author Ttt
-// */
-//class ArticleBean2 : ArticleBean.DataBean()
-//
-///**
-// * detail: 文章信息实体类
-// * @author Ttt
-// */
-//class ArticleBean {
-//    var data: DataBean? = null
-//
-//    // 返回消息
-//    var errorMsg: String? = null
-//
-//    // 返回结果状态 ( 内部定义 )
-//    var errorCode: String? = null
-//
-//    open class DataBean {
-//        var size = 0
-//
-//        var datas: List<ListBean>? = null
-//
-//        class ListBean {
-//            var id = 0
-//            var link: String? = null
-//            var niceDate: String? = null
-//            var niceShareDate: String? = null
-//            var author: String? = null
-//            var title: String? = null
-//            var type = 0
-//        }
-//    }
-//}
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import dev.DevHttpCapture
+import dev.capture.CallbackInterceptor
+import dev.engine.DevEngine
+import dev.retrofit.*
+import dev.utils.LogPrintUtils
+import dev.utils.common.StringUtils
+import okhttp3.OkHttpClient
+import okhttp3.RequestBody
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+import java.util.*
+
+// 日志 TAG
+const val TAG_L = "DevRetrofitCoroutinesDemo"
+
+/**
+ * detail: 服务器接口 API Service
+ * @author Ttt
+ */
+interface APIService {
+
+    @GET("xxx")
+    suspend fun loadArticleList(@Path("page") page: Int): ArticleResponse // or BaseResponse<List<ArticleBean?>>
+
+    @POST("xxx")
+    suspend fun uploadImage(@Body body: RequestBody): UploadBean // or BaseResponse<List<String?>>()
+}
+
+/**
+ * detail: Retrofit API Service
+ * @author Ttt
+ */
+object RetrofitAPI {
+
+    private val retrofit: Retrofit
+
+    private val apiService: APIService
+
+    init {
+        // ====================
+        // = OkHttpClient 配置 =
+        // ====================
+
+        val builder = OkHttpClient.Builder()
+
+        // 使用 DevHttpCapture 库进行 Http 拦截回调 ( 抓包数据存储 )
+        DevHttpCapture.addInterceptor(builder, "ModuleName")
+
+        // 使用 DevHttpCapture 库进行 Http 拦截回调 ( 不进行抓包数据存储 )
+        builder.addInterceptor(CallbackInterceptor { captureInfo ->
+            LogPrintUtils.jsonTag(
+                TAG_L, DevEngine.getJSON().toJson(captureInfo)
+            )
+        })
+
+        // ================
+        // = Retrofit 配置 =
+        // ================
+
+        retrofit = Retrofit.Builder()
+            // Gson 解析
+            .addConverterFactory(GsonConverterFactory.create())
+            // OkHttpClient
+            .client(builder.build())
+            // 服务器地址
+            .baseUrl("https://baseUrl")
+            .build()
+
+        apiService = retrofit.create(APIService::class.java)
+    }
+
+    fun api(): APIService {
+        return apiService
+    }
+}
+
+// ========
+// = 实体类 =
+// ========
+
+/**
+ * detail: 统一响应实体类
+ * @author Ttt
+ */
+open class BaseResponse<T> : Base.Response<T> {
+
+    private var customData: T? = null
+    private var customCode: String? = null
+    private var customMessage: String? = null
+
+    // =================
+    // = Base.Response =
+    // =================
+
+    override fun getData(): T? {
+        return customData
+    }
+
+    override fun getCode(): String? {
+        return customCode
+    }
+
+    override fun getMessage(): String? {
+        return customMessage
+    }
+
+    override fun isSuccess(): Boolean {
+        return customCode?.let { code ->
+            // 自定义 code 为 200 表示请求成功 ( 后台定义 )
+            StringUtils.equals(code, "200")
+        } ?: false
+    }
+}
+
+// =============
+// = 非基础实体类 =
+// =============
+
+/**
+ * detail: 上传图片结果
+ * @author Ttt
+ */
+class UploadBean : BaseResponse<List<String?>>()
+
+/**
+ * detail: 首页数据响应类 ( 可不定义只是为了方便理解 )
+ * @author Ttt
+ * data 映射实体类为 List<ArticleBean>
+ */
+class ArticleResponse : BaseResponse<List<ArticleBean?>>()
+
+/**
+ * detail: 文章实体类
+ * @author Ttt
+ */
+data class ArticleBean(
+    val content: String?,
+    val cover: String?
+)
+
+// ===================
+// = DevRetrofit 使用 =
+// ===================
+
+/**
+ * 模拟在 Activity 下使用
+ * 总的请求方法分为以下两种
+ * execute Request
+ * execute Response Request
+ * 区别在于 Response 是直接使用内部封装的 Base.Result 以及 ResultCallback 进行回调通知等
+ * 不管什么扩展函数方式请求, 最终都是执行 request.kt 中的 finalExecute、finalExecuteResponse 方法
+ */
+class TestActivity : AppCompatActivity() {
+
+    // 封装 Notify.ResultCallback 简化代码
+    abstract class BaseResultCallback<T> : Notify.ResultCallback<T, BaseResponse<T>>()
+
+    // LiveData
+    private val _articleLiveData = MutableLiveData<ArticleResponse>()
+    val articleLiveData: LiveData<ArticleResponse> = _articleLiveData
+
+    private fun request() {
+        // 加载文章列表方式一
+        simpleLaunchExecuteRequest(
+            block = {
+                RetrofitAPI.api().loadArticleList(1)
+            }, object : Notify.Callback<ArticleResponse>() {
+                override fun onSuccess(
+                    uuid: UUID,
+                    data: ArticleResponse?
+                ) {
+                    // 请求成功
+                }
+
+                override fun onError(
+                    uuid: UUID,
+                    error: Throwable?
+                ) {
+                    // 请求异常
+                }
+            }
+        )
+        // 加载文章列表方式一 ( 使用封装 Callback )
+        simpleLaunchExecuteRequest(
+            block = {
+                RetrofitAPI.api().loadArticleList(1)
+            }, InnerCallback()
+        )
+
+        // 加载文章列表方式二
+        simpleLaunchExecuteResponseRequest(
+            block = {
+                RetrofitAPI.api().loadArticleList(1)
+            }, object : Notify.ResultCallback<List<ArticleBean?>, ArticleResponse>() {
+                override fun onSuccess(
+                    uuid: UUID,
+                    data: Base.Result<List<ArticleBean?>, ArticleResponse>
+                ) {
+
+                }
+            }
+        )
+
+        // 加载文章列表方式二 ( 使用 BaseResultCallback )
+        simpleLaunchExecuteResponseRequest(
+            block = {
+                RetrofitAPI.api().loadArticleList(1)
+            }, object : BaseResultCallback<List<ArticleBean?>>() {
+                override fun onSuccess(
+                    uuid: UUID,
+                    data: Base.Result<List<ArticleBean?>, BaseResponse<List<ArticleBean?>>>
+                ) {
+
+                }
+            }
+        )
+
+        // 加载文章列表方式三
+        liveDataLaunchExecuteRequest(
+            block = {
+                RetrofitAPI.api().loadArticleList(1)
+            },
+            liveData = _articleLiveData,
+            usePostValue = false // default true
+        )
+
+        // 加载文章列表方式四 ( 可自行添加额外流程等 )
+        launchExecuteRequest(
+            block = {
+                RetrofitAPI.api().loadArticleList(1)
+            },
+            start = {
+
+            },
+            success = {
+
+            },
+            error = {
+
+            },
+            finish = {
+
+            },
+            callback = InnerCallback()
+        )
+    }
+
+    private class InnerCallback : Notify.Callback<ArticleResponse>() {
+        override fun onSuccess(
+            uuid: UUID,
+            data: ArticleResponse?
+        ) {
+            // 请求成功
+        }
+
+        override fun onError(
+            uuid: UUID,
+            error: Throwable?
+        ) {
+            // 请求异常
+        }
+
+        override fun onStart(uuid: UUID) {
+            super.onStart(uuid)
+            // 开始请求
+        }
+
+        override fun onFinish(uuid: UUID) {
+            super.onFinish(uuid)
+            // 请求结束
+        }
+    }
+}
