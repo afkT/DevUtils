ef380db7909bcdf76f596ab318ccc27391e10bd7
afkT <jtongttt@gmail.com>
2023-06-29 07:37:22 +0800

    add(DevMVVM): 更新 Hi 快捷方法
    Former-commit-id: 8a1a79f1e0dcbae160c86ddcfe8cfd52a0bc45ef

--- lib/DevMVVM/src/main/java/dev/mvvm/utils/hi/Hi.kt
+++ lib/DevMVVM/src/main/java/dev/mvvm/utils/hi/Hi.kt
@@ -28,31 +28,31 @@ inline fun <T> T?.hiIfNotNull(
     }
     return this.hiIfNotNull(
         hiIf = hiIf,
-        hiIfNull = { }
+        hiIfNot = { }
     )
 }
 
 /**
- * 目标对象 T 不为 null 触发 [hiIf] 反之触发 [hiIfNull]
+ * 目标对象 T 不为 null 触发 [hiIf] 反之触发 [hiIfNot]
  * @param hiIf 非 null 执行方法体
- * @param hiIfNull 为 null 执行方法体
+ * @param hiIfNot 为 null 执行方法体
  * @return 目标对象 T
  */
 @JvmSynthetic
 @DevInlineOnly
 inline fun <T> T?.hiIfNotNull(
     hiIf: (T) -> Unit,
-    hiIfNull: () -> Unit
+    hiIfNot: () -> Unit
 ): T? {
     contract {
         callsInPlace(hiIf, InvocationKind.AT_MOST_ONCE)
-        callsInPlace(hiIfNull, InvocationKind.AT_MOST_ONCE)
+        callsInPlace(hiIfNot, InvocationKind.AT_MOST_ONCE)
     }
     if (this != null) {
         hiIf(this)
         return this
     }
-    hiIfNull()
+    hiIfNot()
     return this
 }
 
@@ -107,24 +107,117 @@ inline fun <reified R> Any?.hiIfNotNullAs(
 }
 
 /**
- * 目标对象 T 不为 null 触发 [hiIf] 反之触发 [hiIfNull]
+ * 目标对象 T 不为 null 触发 [hiIf] 反之触发 [hiIfNot]
  * @param hiIf 非 null 执行方法体
- * @param hiIfNull 为 null 执行方法体
+ * @param hiIfNot 为 null 执行方法体
  * @return 方法体返回所需对象
  */
 @JvmSynthetic
 @DevInlineOnly
 inline fun <T, R> T?.hiIfNotNullWith(
     hiIf: (T) -> R,
-    hiIfNull: () -> R
+    hiIfNot: () -> R
 ): R {
     contract {
         callsInPlace(hiIf, InvocationKind.AT_MOST_ONCE)
-        callsInPlace(hiIfNull, InvocationKind.AT_MOST_ONCE)
+        callsInPlace(hiIfNot, InvocationKind.AT_MOST_ONCE)
     }
     return if (this != null) {
-        hiIf.invoke(this)
+        hiIf(this)
     } else {
-        hiIfNull.invoke()
+        hiIfNot()
     }
+}
+
+// =
+
+/**
+ * 目标对象 T 不为 null 触发 [hiIf]
+ * @param default 默认返回值
+ * @param hiIf 非 null 执行方法体
+ * @return 方法体返回所需对象
+ */
+@JvmSynthetic
+@DevInlineOnly
+inline fun <T, R> T.hiIfMap(
+    default: R,
+    hiIf: (T) -> R
+): R {
+    contract {
+        callsInPlace(hiIf, InvocationKind.AT_MOST_ONCE)
+    }
+    return this.hiIfMap(
+        hiIf = hiIf,
+        hiIfNot = { default }
+    )
+}
+
+/**
+ * 目标对象 T 不为 null 触发 [hiIf] 反之触发 [hiIfNot]
+ * @param hiIf 非 null 执行方法体
+ * @param hiIfNot 为 null 执行方法体
+ * @return 方法体返回所需对象
+ */
+@JvmSynthetic
+@DevInlineOnly
+inline fun <T, R> T.hiIfMap(
+    hiIf: (T) -> R,
+    hiIfNot: (T) -> R
+): R {
+    contract {
+        callsInPlace(hiIf, InvocationKind.AT_MOST_ONCE)
+        callsInPlace(hiIfNot, InvocationKind.AT_MOST_ONCE)
+    }
+    if (this != null) {
+        return hiIf(this)
+    }
+    return hiIfNot(this)
+}
+
+/**
+ * 目标对象 T 执行 [hiIf] 方法体
+ * @param given 执行 [hiIf] 方法体条件
+ * @param default 默认返回值
+ * @param hiIf [given] 为 true 执行方法体
+ * @return 方法体返回所需对象
+ */
+@JvmSynthetic
+@DevInlineOnly
+inline fun <T, R> T.hiIfMap(
+    given: Boolean?,
+    default: R,
+    hiIf: (T) -> R
+): R {
+    contract {
+        callsInPlace(hiIf, InvocationKind.AT_MOST_ONCE)
+    }
+    return this.hiIfMap(
+        given = given,
+        hiIf = hiIf,
+        hiIfNot = { default }
+    )
+}
+
+/**
+ * 目标对象 T 执行 [hiIf]、[hiIfNot] 方法体
+ * @param given 执行 [hiIf]、[hiIfNot] 方法体条件
+ * @param hiIf [given] 为 true 执行方法体
+ * @param hiIfNot [given] 为 false 执行方法体
+ * @return 方法体返回所需对象
+ */
+@JvmSynthetic
+@DevInlineOnly
+inline fun <T, R> T.hiIfMap(
+    given: Boolean?,
+    hiIf: (T) -> R,
+    hiIfNot: (T) -> R
+): R {
+    contract {
+        callsInPlace(hiIf, InvocationKind.AT_MOST_ONCE)
+        callsInPlace(hiIfNot, InvocationKind.AT_MOST_ONCE)
+    }
+    if (given == true) {
+        return hiIf(this)
+    }
+    return hiIfNot(this)
 }
