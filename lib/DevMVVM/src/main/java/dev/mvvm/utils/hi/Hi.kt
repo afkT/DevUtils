f3b2a11522003247831dfa1319ee426531c2136e
afkT <jtongttt@gmail.com>
2023-06-29 06:12:51 +0800

    add(DevMVVM): 更新 Hi 快捷方法
    Former-commit-id: 4a292e3c8da04e86017cd2a6c15f4b4da82e3683

--- lib/DevMVVM/src/main/java/dev/mvvm/utils/hi/Hi.kt
+++ lib/DevMVVM/src/main/java/dev/mvvm/utils/hi/Hi.kt
@@ -14,6 +14,99 @@ import kotlin.contracts.contract
 // ==============
 
 /**
+ * 目标对象 T 不为 null 触发 [hiIf]
+ * @param hiIf 非 null 执行方法体
+ * @return 目标对象 T
+ */
+@JvmSynthetic
+@DevInlineOnly
+inline fun <T> T?.hiIfNotNull(
+    hiIf: (T) -> Unit
+): T? {
+    contract {
+        callsInPlace(hiIf, InvocationKind.AT_MOST_ONCE)
+    }
+    return this.hiIfNotNull(
+        hiIf = hiIf,
+        hiIfNull = { }
+    )
+}
+
+/**
+ * 目标对象 T 不为 null 触发 [hiIf] 反之触发 [hiIfNull]
+ * @param hiIf 非 null 执行方法体
+ * @param hiIfNull 为 null 执行方法体
+ * @return 目标对象 T
+ */
+@JvmSynthetic
+@DevInlineOnly
+inline fun <T> T?.hiIfNotNull(
+    hiIf: (T) -> Unit,
+    hiIfNull: () -> Unit
+): T? {
+    contract {
+        callsInPlace(hiIf, InvocationKind.AT_MOST_ONCE)
+        callsInPlace(hiIfNull, InvocationKind.AT_MOST_ONCE)
+    }
+    if (this != null) {
+        hiIf(this)
+        return this
+    }
+    hiIfNull()
+    return this
+}
+
+/**
+ * 目标对象转换 [R] 成功则触发 [hiIf]
+ * @param hiIf 成功转换 [R] 执行方法体
+ * @return 目标对象
+ */
+@JvmSynthetic
+@DevInlineOnly
+inline fun <reified R> Any?.hiIfNotNullAs(
+    hiIf: (R) -> Unit
+): Any? {
+    contract {
+        callsInPlace(hiIf, InvocationKind.AT_MOST_ONCE)
+    }
+    return hiIfNotNullAs(
+        hiIf = hiIf,
+        hiIfNot = { }
+    )
+}
+
+/**
+ * 目标对象转换 [R] 成功则触发 [hiIf] 反之触发 [hiIfNot]
+ *
+ * ```
+ * serializable.hiIfNotNullAs<User> { user ->
+ *  log(poster.name)
+ * }
+ * ```
+ *
+ * @param hiIf 成功转换 [R] 执行方法体
+ * @param hiIfNot 转换失败 [R] 执行方法体
+ * @return 目标对象
+ */
+@JvmSynthetic
+@DevInlineOnly
+inline fun <reified R> Any?.hiIfNotNullAs(
+    hiIf: (R) -> Unit,
+    hiIfNot: () -> Unit
+): Any? {
+    contract {
+        callsInPlace(hiIf, InvocationKind.AT_MOST_ONCE)
+        callsInPlace(hiIfNot, InvocationKind.AT_MOST_ONCE)
+    }
+    if (this != null && this is R) {
+        hiIf(this as R)
+        return this
+    }
+    hiIfNot()
+    return this
+}
+
+/**
  * 目标对象 T 不为 null 触发 [hiIf] 反之触发 [hiIfNull]
  * @param hiIf 非 null 执行方法体
  * @param hiIfNull 为 null 执行方法体
