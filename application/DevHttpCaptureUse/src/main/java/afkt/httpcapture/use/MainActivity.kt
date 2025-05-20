9bbf0b6f59b28843326973446806b2ac8b307354
陈嘉桐 <jtongttt@gmail.com>
2025-05-21 00:16:40 +0800

    feat(DevHttpCaptureUse): 完成 DevHttpCapture 示例项目部分实现代码

--- application/DevHttpCaptureUse/src/main/java/afkt/httpcapture/use/MainActivity.kt
+++ application/DevHttpCaptureUse/src/main/java/afkt/httpcapture/use/MainActivity.kt
@@ -2,20 +2,7 @@ package afkt.httpcapture.use
 
 import afkt.httpcapture.use.base.BaseActivity
 import afkt.httpcapture.use.databinding.ActivityMainBinding
-import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : BaseActivity<ActivityMainBinding, AppViewModel>(
-    R.layout.activity_main, BR.viewModel, simple_Agile = { act ->
-        if (act is MainActivity) {
-            act.apply {
-                viewModel.backPressedEvent.observe(this) {
-                    val fragment = supportFragmentManager.findFragmentById(
-                        binding.navContainer.id
-                    ) as NavHostFragment
-                    val navController = fragment.navController
-                    navController.popBackStack()
-                }
-            }
-        }
-    }
+    R.layout.activity_main
 )
--- application/DevHttpCaptureUse/src/main/java/afkt/httpcapture/use/MainFragment.kt
+++ application/DevHttpCaptureUse/src/main/java/afkt/httpcapture/use/MainFragment.kt
@@ -2,8 +2,7 @@ package afkt.httpcapture.use
 
 import afkt.httpcapture.use.base.BaseFragment
 import afkt.httpcapture.use.databinding.FragmentMainBinding
-import dev.simple.app.base.FragmentVMType
 
 class MainFragment : BaseFragment<FragmentMainBinding, AppViewModel>(
-    R.layout.fragment_main, BR.viewModel, FragmentVMType.ACTIVITY
+    R.layout.fragment_main, BR.viewModel
 )
