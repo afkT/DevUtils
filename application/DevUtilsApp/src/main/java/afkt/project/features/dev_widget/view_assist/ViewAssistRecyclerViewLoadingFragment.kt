f140fb6dab2a8c71b57b175af7833b857bd0dbe6
陈嘉桐 <jtongttt@gmail.com>
2025-08-18 17:37:15 +0800

    update(DevUtilsApp): 更新 DevUtils 项目部分示例代码

--- application/DevUtilsApp/src/main/java/afkt/project/features/dev_widget/view_assist/ViewAssistRecyclerViewLoadingFragment.kt
+++ application/DevUtilsApp/src/main/java/afkt/project/features/dev_widget/view_assist/ViewAssistRecyclerViewLoadingFragment.kt
@@ -1,34 +1,99 @@
 package afkt.project.features.dev_widget.view_assist
 
+import afkt.project.BR
 import afkt.project.R
 import afkt.project.app.AppFragment
 import afkt.project.app.AppViewModel
-import afkt.project.databinding.BaseViewRecyclerviewBinding
+import afkt.project.databinding.AdapterItemRecyclerLoadingBinding
+import afkt.project.databinding.FragmentOtherFunctionViewAssistRecyclerBinding
+import afkt.project.model.basic.AdapterModel
 import afkt.project.model.helper.RandomHelper
-import android.view.ViewGroup
-import androidx.recyclerview.widget.GridLayoutManager
-import dev.utils.app.helper.quick.QuickHelper
+import android.graphics.drawable.Drawable
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.databinding.ViewDataBinding
+import dev.base.DevSource
+import dev.engine.image.listener.DrawableListener
+import dev.expand.engine.image.display
+import dev.mvvm.utils.toSource
+import dev.widget.assist.ViewAssist
+import me.tatarka.bindingcollectionadapter2.BindingRecyclerViewAdapter
+import me.tatarka.bindingcollectionadapter2.ItemBinding
 
 /**
  * detail: ViewAssist RecyclerView ( loading )
  * @author Ttt
  */
-class ViewAssistRecyclerViewLoadingFragment : AppFragment<BaseViewRecyclerviewBinding, AppViewModel>(
-    R.layout.base_view_recyclerview, simple_Agile = {
-        if (it is ViewAssistRecyclerViewLoadingFragment) {
-            it.apply {
-                val parent = binding.vidRv.parent as? ViewGroup
-                // 根布局处理
-                QuickHelper.get(parent).setPadding(0)
+class ViewAssistRecyclerViewLoadingFragment : AppFragment<FragmentOtherFunctionViewAssistRecyclerBinding, ViewAssistRecyclerViewLoadingViewModel>(
+    R.layout.fragment_other_function_view_assist_recycler, BR.viewModel
+)
 
-                val lists = mutableListOf<String>()
-                for (i in 0..19) {
-                    lists.add(RandomHelper.randomImage1080x1920(1000))
+class ViewAssistRecyclerViewLoadingViewModel : AppViewModel() {
+
+    val adapter = RecyclerLoadingAdapter()
+
+    val adapterModel = RecyclerLoadingAdapterModel().apply {
+        val lists = mutableListOf<String>()
+        for (i in 0..19) {
+            lists.add(RandomHelper.randomImage1080x1920(500))
+        }
+        addAll(lists)
+    }
+}
+
+class RecyclerLoadingAdapterModel : AdapterModel<String>() {
+
+    // Item Binding
+    val itemBinding = ItemBinding.of<String>(
+        BR.itemValue, R.layout.adapter_item_recycler_loading
+    )
+}
+
+class RecyclerLoadingAdapter : BindingRecyclerViewAdapter<String>() {
+
+    override fun onBindBinding(
+        binding: ViewDataBinding,
+        variableId: Int,
+        layoutRes: Int,
+        position: Int,
+        item: String?
+    ) {
+        super.onBindBinding(binding, variableId, layoutRes, position, item)
+        if (binding is AdapterItemRecyclerLoadingBinding) {
+            binding.apply {
+                val viewAssist = ViewAssist.wrap(vidSkeleton)
+                ViewAssistUtils.registerRecyclerLoading(viewAssist) {
+                    loadImage(vidIv, viewAssist, item)
                 }
-                // 初始化布局管理器、适配器
-                binding.vidRv.layoutManager = GridLayoutManager(this.activity, 2)
-                RecyclerLoadingAdapter(lists).bindAdapter(binding.vidRv)
+                loadImage(vidIv, viewAssist, item)
             }
         }
     }
-)
+
+    private fun loadImage(
+        imageView: AppCompatImageView,
+        viewAssist: ViewAssist,
+        url: String?
+    ) {
+        viewAssist.showIng()
+        // 加载图片
+        imageView.display(
+            source = url?.toSource(),
+            listener = object : DrawableListener() {
+                override fun onStart(source: DevSource) {}
+                override fun onResponse(
+                    source: DevSource,
+                    value: Drawable
+                ) {
+                    viewAssist.showSuccess()
+                }
+
+                override fun onFailure(
+                    source: DevSource,
+                    throwable: Throwable
+                ) {
+                    viewAssist.showFailed()
+                }
+            }
+        )
+    }
+}
