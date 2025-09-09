70368894b84ad0333a23f2d0c30f91cfdc27d933
陈嘉桐 <jtongttt@gmail.com>
2025-09-10 00:00:36 +0800

    update: 更新部分示例代码

--- application/DevUtilsApp/src/main/java/afkt/project/MainActivity.kt
+++ application/DevUtilsApp/src/main/java/afkt/project/MainActivity.kt
@@ -5,8 +5,12 @@ import afkt.project.app.AppViewModel
 import afkt.project.app.appViewModel
 import afkt.project.databinding.MainActivityBinding
 import afkt.project.model.button.args
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.findNavController
 import dev.utils.DevFinal
+import dev.utils.app.assist.lifecycle.fragment.AbstractFragmentLifecycle
 
 class MainActivity : AppActivity<MainActivityBinding, AppViewModel>(
     R.layout.main_activity
@@ -26,15 +30,15 @@ class MainActivity : AppActivity<MainActivityBinding, AppViewModel>(
         }
     }
 
-//    // Fragment LifecycleCallbacks 抽象类
-//    override fun fragmentLifecycleImpl(): AbstractFragmentLifecycle? {
-//        return object : AbstractFragmentLifecycle() {
-//            override fun onFragmentResumed(
-//                fm: FragmentManager,
-//                fragment: Fragment
-//            ) {
-//                Log.d("QWOEKQWEQWEQ", "onFragmentResumed() -> $fragment")
-//            }
-//        }
-//    }
+    // Fragment LifecycleCallbacks 抽象类
+    override fun fragmentLifecycleImpl(): AbstractFragmentLifecycle? {
+        return object : AbstractFragmentLifecycle() {
+            override fun onFragmentResumed(
+                fm: FragmentManager,
+                fragment: Fragment
+            ) {
+                Log.d("QWERQAZZXCV", "【Lifecycle】onFragmentResumed() => $fragment")
+            }
+        }
+    }
 }
