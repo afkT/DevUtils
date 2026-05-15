b42bd360613647f1d8187eebee76df4aaf8f44a5
陈嘉桐 <jtongttt@gmail.com>
2026-05-15 11:10:41 +0800

    feat(bindingadapters): 添加多种条件可点击绑定适配器
    - 导入 equality 扩展函数以支持字符串和值比较
    - 添加 binding_clickable_IfNotEmpty 适配器，根据文本非空控制可点击性
    - 添加 binding_clickable_IfStringEquals 适配器，根据字符串相等（区分大小写）控制可点击性
    - 添加 binding_clickable_IfStringEqualsIgnoreCase 适配器，根据字符串相等（忽略大小写）控制可点击性
    - 添加 binding_clickable_IfNotNull 适配器，根据对象非空控制可点击性
    - 添加 binding_clickable_IfValueEquals 适配器，根据对象值相等控制可点击性
    - 所有新适配器内部委托现有的 bindingClickable 方法实现

--- lib/DevSimple/src/main/java/dev/simple/bindingadapters/view/ViewStateClickable.kt
+++ lib/DevSimple/src/main/java/dev/simple/bindingadapters/view/ViewStateClickable.kt
@@ -2,6 +2,9 @@ package dev.simple.bindingadapters.view
 
 import android.view.View
 import androidx.databinding.BindingAdapter
+import dev.simple.extensions.equality.stringEquals
+import dev.simple.extensions.equality.stringEqualsIgnoreCase
+import dev.simple.extensions.equality.valueEquals
 import dev.utils.app.ViewUtils
 
 // ==================================
@@ -17,4 +20,113 @@ import dev.utils.app.ViewUtils
 @BindingAdapter("binding_clickable")
 fun View.bindingClickable(clickable: Boolean) {
     ViewUtils.setClickable(clickable, this)
+}
+
+// ==============
+// = IfNotEmpty =
+// ==============
+
+/**
+ * 根据文本是否非空控制是否可点击。
+ * <pre>
+ *     对应布局属性 binding_clickable_IfNotEmpty；null 或空串为不可点击，否则可点击；内部委托同文件 [bindingClickable]。
+ * </pre>
+ *
+ * @param value 文本，非 null 且非空时可点击
+ */
+@BindingAdapter("binding_clickable_IfNotEmpty")
+fun View.bindingClickableIfNotEmpty(value: String?) {
+    bindingClickable(!value.isNullOrEmpty())
+}
+
+// ======================
+// = IfStringEquals (CS) =
+// ======================
+
+/**
+ * 根据两字符串是否相等（区分大小写）控制是否可点击。
+ * <pre>
+ *     对应布局属性 binding_clickable_IfStringEquals 与 binding_clickable_IfStringEquals_value2；
+ *     相等为可点击，否则不可点击；内部委托 [stringEquals] 与同文件 [bindingClickable]。
+ * </pre>
+ *
+ * @param value1 参与比较的字符串，可为 null
+ * @param value2 参与比较的字符串，可为 null
+ */
+@BindingAdapter(
+    "binding_clickable_IfStringEquals",
+    "binding_clickable_IfStringEquals_value2",
+)
+fun View.bindingClickableIfStringEquals(
+    value1: String?,
+    value2: String?
+) {
+    bindingClickable(value1.stringEquals(value2))
+}
+
+// ==============================
+// = IfStringEquals (IgnoreCase) =
+// ==============================
+
+/**
+ * 根据两字符串是否相等（忽略大小写）控制是否可点击。
+ * <pre>
+ *     对应布局属性 binding_clickable_IfStringEqualsIgnoreCase 与 binding_clickable_IfStringEqualsIgnoreCase_value2；
+ *     相等为可点击，否则不可点击；内部委托 [stringEqualsIgnoreCase] 与同文件 [bindingClickable]。
+ * </pre>
+ *
+ * @param value1 参与比较的字符串，可为 null
+ * @param value2 参与比较的字符串，可为 null
+ */
+@BindingAdapter(
+    "binding_clickable_IfStringEqualsIgnoreCase",
+    "binding_clickable_IfStringEqualsIgnoreCase_value2",
+)
+fun View.bindingClickableIfStringEqualsIgnoreCase(
+    value1: String?,
+    value2: String?
+) {
+    bindingClickable(value1.stringEqualsIgnoreCase(value2))
+}
+
+// =============
+// = IfNotNull =
+// =============
+
+/**
+ * 根据引用是否非 null 控制是否可点击。
+ * <pre>
+ *     对应布局属性 binding_clickable_IfNotNull；null 为不可点击，否则可点击；内部委托同文件 [bindingClickable]。
+ * </pre>
+ *
+ * @param value 任意对象，非 null 时可点击
+ */
+@BindingAdapter("binding_clickable_IfNotNull")
+fun View.bindingClickableIfNotNull(value: Any?) {
+    bindingClickable(value != null)
+}
+
+// =================
+// = IfValueEquals =
+// =================
+
+/**
+ * 根据两对象在工具类语义下是否相等控制是否可点击。
+ * <pre>
+ *     对应布局属性 binding_clickable_IfValueEquals 与 binding_clickable_IfValueEquals_value2；
+ *     相等为可点击，否则不可点击；内部委托 [valueEquals] 与同文件 [bindingClickable]。
+ * </pre>
+ *
+ * @param value1 参与比较的对象，可为 null
+ * @param value2 参与比较的对象，可为 null
+ */
+@BindingAdapter(
+    "binding_clickable_IfValueEquals",
+    "binding_clickable_IfValueEquals_value2",
+)
+fun View.bindingClickableIfValueEquals(
+    value1: Any?,
+    value2: Any?
+) {
+    bindingClickable(value1.valueEquals(value2))
 }
